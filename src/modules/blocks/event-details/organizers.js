8e432e8f0e001d0bbe5b79220503fa27098077d6
bordoni <bordoni.dev@gmail.com>
2018-02-20 01:15:09 -0300

    Include Search Icon on Organizer Meta Group

--- src/modules/blocks/event-details/organizers.js
+++ src/modules/blocks/event-details/organizers.js
@@ -21,6 +21,80 @@ import {
 	OrganizerForm,
 } from 'elements'
 
+function SearchDropdown( { ...props } ) {
+	const { focus } = props;
+
+	if ( ! focus  ) {
+		return null;
+	}
+
+	const icon = (
+		<Dashicon icon='search' />
+	)
+
+	const dropdownToggle = ( { onToggle, isOpen } ) => (
+		<IconButton
+			className='tribe-editor-button'
+			label={ __( 'Add existing Organizer' ) }
+			onClick={ onToggle }
+			icon={ icon }
+		/>
+	)
+
+	const dropdownContent = () => (
+		'Search organizer here!'
+	)
+
+	const content = (
+		<Dropdown
+			className="tribe-editor-organizer-dropdown"
+			position="bottom center"
+			contentClassName="tribe-editor-dropdown__dialog"
+			renderToggle={ dropdownToggle }
+			renderContent={ dropdownContent }
+		/>
+	)
+
+	return content
+}
+
+function CreateDropdown( { ...props } ) {
+	const { focus } = props;
+
+	if ( ! focus  ) {
+		return null;
+	}
+
+	const icon = (
+		<Dashicon icon='plus' />
+	)
+
+	const dropdownToggle = ( { onToggle, isOpen } ) => (
+		<IconButton
+			className='tribe-editor-button'
+			label={ __( 'Add existing Organizer' ) }
+			onClick={ onToggle }
+			icon={ icon }
+		/>
+	)
+
+	const dropdownContent = () => (
+		<OrganizerForm />
+	)
+
+	const content = (
+		<Dropdown
+			className="tribe-editor-organizer-dropdown"
+			position="bottom center"
+			contentClassName="tribe-editor-dropdown__dialog"
+			renderToggle={ dropdownToggle }
+			renderContent={ dropdownContent }
+		/>
+	)
+
+	return content
+}
+
 /**
  * Module Code
  */
@@ -29,39 +103,15 @@ class EventOrganizers extends Component {
 		super( ...arguments );
 	}
 
+
+
 	render() {
 		const { focus } = this.props;
 
-		if ( ! focus  ) {
-			return null;
-		}
-
-		const icon = (
-			<Dashicon icon='plus' />
-		)
-
-		const dropdownToggle = ( { onToggle, isOpen } ) => (
-			<IconButton
-				className='tribe-editor-button'
-				label={ __( 'Add Organizer' ) }
-				onClick={ onToggle }
-				icon={ icon }
-			/>
-		)
-
-		const dropdownContent = () => (
-			<OrganizerForm />
-		)
-
-		const content = (
-			<Dropdown
-				className="tribe-editor-organizer-dropdown"
-				position="bottom center"
-				contentClassName="tribe-editor-dropdown__dialog"
-				renderToggle={ dropdownToggle }
-				renderContent={ dropdownContent }
-			/>
-		)
+		const content = [
+			<SearchDropdown key='organizer-search-dropdown' focus={ focus } />,
+			<CreateDropdown key='organizer-create-dropdown' focus={ focus } />
+		]
 
 		return content
 	}
