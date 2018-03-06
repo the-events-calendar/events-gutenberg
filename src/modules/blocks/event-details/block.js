ba821c94ee4d1dd159c35093e3078e494339c49e
leahkoerper <leah.koerper@gmail.com>
2018-03-05 16:44:56 -0800

    Update block.js
    Update website field text

--- src/modules/blocks/event-details/block.js
+++ src/modules/blocks/event-details/block.js
@@ -85,7 +85,7 @@ class EventDetails extends Component {
 						<PlainText
 							id="tribe-event-url"
 							value={ attributes.eventUrl }
-							placeholder={ __( 'Write event website here…', 'the-events-calendar' ) }
+							placeholder={ __( 'Enter url here', 'the-events-calendar' ) }
 							onChange={ ( nextContent ) => setAttributes( { eventUrl: nextContent } ) }
 						/>
 					</div>
