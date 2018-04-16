a6661ccb725a5919d5a372bf1408d5a44cea9591
mitogh <mitogh@gmail.com>
2018-04-16 16:37:53 -0500

    Remove not required `map` call

--- src/modules/editor/utils/request.js
+++ src/modules/editor/utils/request.js
@@ -10,17 +10,12 @@ export function getResponseHeaders( xhr ) {
 		.split( '\u000d\u000a' )
 		// Create an array of key/value pais separated by: ; or space
 		.map( ( entry ) => entry.split( '\u003a\u0020' ) )
-		// Crate an array of objects with key / value pairs
-		.map( (values) => {
+		// Crate a single objects with key / value pairs
+		.reduce( (headers, values) => {
 			if ( values.length === 2 ) {
 				const [ key, value ] = values;
-				const header = {};
-				header[ key ] = value;
-				return header;
+				headers[ key ] = value;
 			}
-		})
-		// Merge arrays into a single object.
-		.reduce( (previousValue, currentValue) => {
-			return Object.assign({}, previousValue, currentValue);
-		}, {} );
+			return headers;
+		}, {})
 }
