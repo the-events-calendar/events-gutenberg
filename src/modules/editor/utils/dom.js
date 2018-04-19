e9343ec9cd8de75df86b068d0df99a209f70d0ad
mitogh <mitogh@gmail.com>
2018-04-19 12:23:09 -0500

    Apply lint rules

--- src/modules/editor/utils/dom.js
+++ src/modules/editor/utils/dom.js
@@ -1,9 +1,10 @@
 /**
- * Utility to search the parent of a node looking from the current node Up to the highest node on the DOM Tree
+ * Utility to search the parent of a node looking from the current node Up to the highest
+ * node on the DOM Tree
  *
- * @param node
- * @param callback A callback executed on every iteration, it should return a boolean to test against if is the correct node.
- * @returns {boolean}
+ * @param {DOMElement} node - The DOM node where the search starts
+ * @param {function} callback - Is executed on every iteration, it should return a boolean
+ * @returns {boolean} Returns tre if the callback returns true with any of the parents.
  */
 export function searchParent( node, callback ) {
 	let found = false;
