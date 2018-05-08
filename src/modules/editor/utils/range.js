122ad53a2594052562c8d96d4ce67fcfeb236178
mitogh <mitogh@gmail.com>
2018-05-08 10:53:07 -0500

    Add comments to clarify code

--- src/modules/editor/utils/range.js
+++ src/modules/editor/utils/range.js
@@ -3,6 +3,12 @@
  */
 import { trim, isEmpty } from 'lodash';
 
+/**
+ * Parse a string into a range type of string {a} - {b} where a and b are numbers
+ *
+ * @param {string} input The original string
+ * @returns {string} A formatted range string.
+ */
 export function parser( input ) {
 	const range = trim( input );
 
@@ -25,6 +31,12 @@ export function parser( input ) {
 	return a >= b ? `${ trim( b ) } - ${ trim( a ) }` : `${ trim( a ) } - ${ trim( b ) }`;
 }
 
+/**
+ * Remove any char that is not a: number, dash, dot or comma.
+ *
+ * @param {string} input The string from where to extract the chars
+ * @returns {string} A string with only valid chars
+ */
 export function parseChars( input ) {
 	return input
 		.split( ' ' )
@@ -36,6 +48,12 @@ export function parseChars( input ) {
 		.trim();
 }
 
+/**
+ * Extract only valid numbers from the string
+ *
+ * @param {string} chars The chars to be split into parts.
+ * @returns {array} An array with the parts
+ */
 export function extractParts( chars ) {
 	return chars
 	// Convert , into . so we can parse into numbers
@@ -52,6 +70,12 @@ export function extractParts( chars ) {
 		.slice( 0, 2 );
 }
 
+/**
+ * Test to see if an input range is free of cost
+ *
+ * @param {string} input Range input
+ * @returns {boolean} true if the event has 0 on all parts of the range, false otherwise
+ */
 export function isFree( input ) {
 	const parts = input.split( '-' );
 
