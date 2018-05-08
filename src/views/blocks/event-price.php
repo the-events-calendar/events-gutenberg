8f17c8030aec252df745b0037ef8c22345e268d7
mitogh <mitogh@gmail.com>
2018-05-08 10:43:24 -0500

    Remove not required hooks

--- src/views/blocks/event-price.php
+++ src/views/blocks/event-price.php
@@ -4,13 +4,10 @@ $event_id = $this->get( 'post_id' );
 $cost = tribe_get_formatted_cost();
 ?>
 <div class="tribe-events-single-section tribe-events-event-cost tribe-clearfix">
-	<?php do_action( 'tribe_events_single_event_cost_section_start' ); ?>
     <?php
     // Event Cost
     if ( ! empty( $cost ) ) : ?>
         <dt> <?php esc_html_e( 'Cost:', 'events-gutenberg' ) ?> </dt>
         <dd class="tribe-events-event-cost"> <?php esc_html_e( $cost ); ?> </dd>
     <?php endif ?>
-
-	<?php do_action( 'tribe_events_single_event_cost_section_end' ); ?>
 </div>
