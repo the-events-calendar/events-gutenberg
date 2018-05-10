629d55559f6bb2ae5c3c1ab9f2bf81e79d3831f8
mitogh <mitogh@gmail.com>
2018-05-10 14:41:21 -0500

    Fix lint

--- src/modules/elements/dashboard/element.js
+++ src/modules/elements/dashboard/element.js
@@ -134,7 +134,7 @@ export default class Dashboard extends Component {
 			return;
 		}
 		this.close();
-	}
+	};
 
 	/**
 	 * Listen for a click on the document to see if the element is part of the Dashboard
