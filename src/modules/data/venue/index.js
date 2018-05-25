449de8f7de131de57d2ba09542682df23c06e007
mitogh <mitogh@gmail.com>
2018-05-25 14:08:17 -0500

    Refactor store into his own directory

--- src/modules/data/venue/index.js
+++ src/modules/data/venue/index.js
@@ -1,7 +1,6 @@
 /**
  * External imports
  */
-import { isEmpty, get } from 'lodash';
 
 /**
  * Wordpress Imports
@@ -11,8 +10,14 @@ import { dispatch } from '@wordpress/data';
 const { data, apiRequest } = wp;
 const { registerStore } = data;
 
-const POST_TYPE = 'tribe_venue';
+/**
+ * Internal imports
+ */
+import * as reducers from './reducer';
+
+export const POST_TYPE = 'tribe_venue';
 export const STORE_NAME = 'tec.venue';
+
 const DEFAULT_STATE = {
 	id: 0,
 	details: {},
@@ -29,7 +34,7 @@ const DEFAULT_STATE = {
 const reducer = ( state = DEFAULT_STATE, action ) => {
 	switch ( action.type ) {
 		case 'SET_DETAILS': {
-			return setDetails( state, action.id, action.details );
+			return reducers.setDetails( state, action.id, action.details );
 		}
 		case 'SET_DRAFT_TITLE': {
 			return {
@@ -59,10 +64,10 @@ const reducer = ( state = DEFAULT_STATE, action ) => {
 			};
 		}
 		case 'CREATE_DRAFT': {
-			return createDraft( state, action.fields );
+			return reducers.createDraft(state, action.fields );
 		}
 		case 'EDIT_DRAFT': {
-			return editDraft( state, action.id, action.fields );
+			return reducers.editDraft( state, action.id, action.fields );
 		}
 		case 'REGISTER_VENUE': {
 			return {
@@ -81,88 +86,6 @@ const reducer = ( state = DEFAULT_STATE, action ) => {
 			return state;
 		}
 	}
-};
-
-const setDetails = ( prevState, id, details ) => {
-	const meta = get( details, 'meta', {} );
-	return {
-		...prevState,
-		id,
-		details,
-		address: getAddress( meta ),
-		coordinates: getCoordinates( meta ),
-		edit: false,
-		create: false,
-		loading: false,
-		submit: false,
-	};
-};
-
-const createDraft = ( state, fields ) => {
-	apiRequest( {
-		path: `/wp/v2/${ POST_TYPE }`,
-		method: 'POST',
-		data: fields,
-	} ).then( ( body ) => {
-		const { id } = body;
-		dispatch( STORE_NAME ).registerVenue( id );
-		dispatch( STORE_NAME ).setDetails( id, body );
-	} );
-
-	return {
-		...state,
-		loading: true,
-		submit: true,
-	};
-};
-
-const editDraft = ( state, id, fields ) => {
-	apiRequest( {
-		path: `/wp/v2/${ POST_TYPE }/${ id }`,
-		method: 'PUT',
-		data: fields,
-	} ).then( ( body ) => {
-		dispatch( STORE_NAME ).setDetails( body.id, body );
-	} );
-
-	return {
-		...state,
-		loading: true,
-		submit: true,
-	};
-};
-
-const getAddress = ( meta = {} ) => {
-	if ( isEmpty( meta ) ) {
-		return {};
-	}
-
-	const {
-		_VenueAddress,
-		_VenueCity,
-		_VenueProvince,
-		_VenueZip,
-		_VenueCountry,
-	} = meta;
-
-	return {
-		street: _VenueAddress,
-		city: _VenueCity,
-		province: _VenueProvince,
-		zip: _VenueZip,
-		country: _VenueCountry,
-	};
-};
-
-const getCoordinates = ( meta = {} ) => {
-	const { _VenueLat, _VenueLng } = meta;
-	const lat = parseFloat( _VenueLat );
-	const lng = parseFloat( _VenueLng );
-
-	return {
-		lat: isNaN( lat ) ? null : lat,
-		lng: isNaN( lng ) ? null : lat,
-	};
 };
 
 const actions = {
