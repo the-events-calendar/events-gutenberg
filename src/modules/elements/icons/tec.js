904573cb2c2491a37233347d2e9d533d308e5f6e
mitogh <mitogh@gmail.com>
2018-06-19 19:46:24 -0500

    Register Icons Element with Tec component

---
+++ src/modules/elements/icons/tec.js
@@ -0,0 +1,18 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import TECIcon from 'icons/tec.svg';
+import './style.pcss';
+
+export default () => {
+	return (
+		<div className="tribe-editor__icons__container tribe-editor__icons--tec">
+			<TECIcon />
+		</div>
+	);
+};
