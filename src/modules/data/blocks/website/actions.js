36a712840d410f1862425ed715d7b6b8ef72233e
mitogh <mitogh@gmail.com>
2018-06-26 16:45:43 -0500

    Add actions for website block

---
+++ src/modules/data/blocks/website/actions.js
@@ -0,0 +1,27 @@
+/**
+ * Internal dependencies
+ */
+import * as types from './types';
+import { DEFAULT_STATE } from './reducers';
+
+export const setWebsite = ( url ) => ( {
+	type: types.SET_WEBSITE_URL,
+	payload: {
+		url,
+	},
+} );
+
+export const setLabel = ( label ) => ( {
+	type: types.SET_WEBSITE_LABEL,
+	payload: {
+		label,
+	},
+} );
+
+export const setInitialState = ( attributes = {} ) => {
+	return ( dispatch ) => {
+		dispatch( setWebsite( attributes.url || DEFAULT_STATE.url ) );
+		dispatch( setLabel( attributes.urlLabel || DEFAULT_STATE.label ) );
+	};
+};
+
