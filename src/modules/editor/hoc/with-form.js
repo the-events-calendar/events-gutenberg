a892d2c962313f54951f7e3ff8fc35a7eb7bfa89
mitogh <mitogh@gmail.com>
2018-07-06 12:46:02 -0500

    Add HOC to wrap form logic

---
+++ src/modules/editor/hoc/with-form.js
@@ -0,0 +1,74 @@
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { compose, bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { actions, selectors } from 'data/forms';
+
+export default ( args ) => ( WrappedComponent ) => {
+	class WithForm extends Component {
+		static propTypes = {
+			registerForm: PropTypes.func,
+			postType: PropTypes.string,
+		};
+
+		constructor( props ) {
+			super( props );
+		}
+
+		componentDidMount() {
+			const name = args( this.props );
+			const { registerForm, postType } = this.props;
+			registerForm( name, postType );
+		}
+
+		render() {
+			return <WrappedComponent { ...this.props } { ...this.aditionalProps() } />;
+		}
+
+		aditionalProps() {
+			const {
+				createDraft,
+				sendForm,
+				setSubmit,
+				editEntry,
+			} = this.props;
+			const name = args( this.props );
+			return {
+				createDraft( fieldsObject ) {
+					createDraft( name, fieldsObject );
+				},
+				editEntry( fieldsObject ) {
+					editEntry( name, fieldsObject );
+				},
+				sendForm( fieldsObject, callback, details ) {
+					sendForm( name, fieldsObject, callback, details );
+				},
+				setSubmit() {
+					setSubmit( name );
+				},
+			};
+		}
+	}
+
+	const mapStateToProps = ( state, props ) => {
+		const name = args( props );
+		const modifiedProps = { name };
+		return {
+			edit: selectors.getFormEdit( state, modifiedProps ),
+			create: selectors.getFormCreate( state, modifiedProps ),
+			fields: selectors.getFormFields( state, modifiedProps ),
+			submit: selectors.getFormSubmit( state, modifiedProps ),
+		};
+	};
+
+	const mapDispatchToProps = ( dispatch ) => bindActionCreators( actions, dispatch );
+
+	return compose( connect( mapStateToProps, mapDispatchToProps ) )( WithForm );
+};
