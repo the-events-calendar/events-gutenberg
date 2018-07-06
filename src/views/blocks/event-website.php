c6888c6bd627ecfea1ab010826094ecd1bafde6c
Juanfra Aldasoro <juan@nicethemes.com>
2018-07-06 10:48:13 -0300

    Implement the event website block frontend view

--- src/views/blocks/event-website.php
+++ src/views/blocks/event-website.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Renders the event website block
+ *
+ * @version TBD
+ *
+ */
 $attributes = $this->attributes( array(
 	'urlLabel' => '',
 	'href' => tribe_get_event_website_url(),
@@ -12,7 +18,7 @@ $target = apply_filters( 'tribe_get_event_website_link_target', '_self' );
 ?>
 
 <a
-    class="tribe-block__website"
+    class="tribe-block__event-website"
     href="<?php echo esc_url( $attributes['href'] ); ?>"
     target="<?php echo esc_attr( $target ); ?>"
     <?php if ( '_blank' === $target  ) : ?> rel="noopener noreferrer" <?php endif; ?>
