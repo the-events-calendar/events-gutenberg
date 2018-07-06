fabed1763bf65d772dfdb3c89a1ff4fcde9db3fa
mitogh <mitogh@gmail.com>
2018-07-05 23:08:42 -0500

    Register details reducer

--- src/modules/data/reducers.js
+++ src/modules/data/reducers.js
@@ -9,10 +9,12 @@ import { combineReducers } from 'redux';
 import blocks from './blocks';
 import ui from './ui';
 import search from './search';
+import details from './details';
 
 export default combineReducers( {
 	blocks,
 	ui,
 	search,
+	details,
 } );
 
