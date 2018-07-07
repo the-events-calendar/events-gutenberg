cb21dcc0628332c76fce58af6d15353118687db2
mitogh <mitogh@gmail.com>
2018-07-06 19:18:10 -0500

    Register volatile types

--- src/modules/data/forms/types.js
+++ src/modules/data/forms/types.js
@@ -4,3 +4,6 @@ export const CREATE_FORM_DRAFT = 'CREATE_FORM_DRAFT';
 export const EDIT_FORM_ENTRY = 'EDIT_FORM_ENTRY';
 export const SUBMIT_FORM = 'SUBMIT_FORM';
 export const CLEAR_FORM = 'CLEAR_FORM';
+
+export const ADD_VOLATILE_ID = 'ADD_VOLATILE_ID';
+export const REMOVE_VOLATILE_ID = 'REMOVE_VOLATILE_ID';
