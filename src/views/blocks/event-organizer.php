d89bd2ff721f96b907b06d59ee5650b43409748c
Juanfra Aldasoro <juan@nicethemes.com>
2018-07-11 11:58:35 -0300

    Add `.tribe-block` to main wrapper Add classes to each tag as requested on the CR

--- src/views/blocks/event-organizer.php
+++ src/views/blocks/event-organizer.php
@@ -17,17 +17,17 @@ $website = tribe_get_organizer_website_link( $organizer_id );
 $email   = tribe_get_organizer_email( $organizer_id );
 
 ?>
-<div class="tribe-block__organizer__details tribe-clearfix">
+<div class="tribe-block tribe-block__organizer__details tribe-clearfix">
 	<div class="tribe-block__organizer__title">
 		<h3><?php echo tribe_get_organizer( $organizer_id ); ?></h3>
 	</div>
 	<?php if ( ! empty( $phone ) ) : ?>
-		<p><?php echo esc_html( $phone ); ?></p>
+		<p class="tribe-block__organizer__phone"><?php echo esc_html( $phone ); ?></p>
 	<?php endif; ?>
 	<?php if ( ! empty( $website ) ) : ?>
-		<p><?php echo $website; ?></p>
+		<p class="tribe-block__organizer__website"><?php echo $website; ?></p>
 	<?php endif; ?>
 	<?php if ( ! empty( $email ) ) : ?>
-		<p><?php echo esc_html( $email ); ?></p>
+		<p class="tribe-block__organizer__email"><?php echo esc_html( $email ); ?></p>
 	<?php endif; ?>
 </div>
