8c74a7533a04f45dc025436554a0cfb3f3d53749
Juanfra Aldasoro <juan@nicethemes.com>
2018-07-12 10:10:57 -0300

    Avoid generic classes.

--- src/views/blocks/event-website.php
+++ src/views/blocks/event-website.php
@@ -14,7 +14,6 @@ $target = apply_filters( 'tribe_get_event_website_link_target', '_self' );
 ?>
 <div class="tribe-block tribe-block__event-website">
     <a
-        class="blue bordered"
         href="<?php echo esc_url( $this->attr( 'href' ) ); ?>"
         target="<?php echo esc_attr( $target ); ?>"
         <?php if ( '_blank' === $target  ) : ?> rel="noopener noreferrer" <?php endif; ?>
