d1e5bd28ce8f3616d9fc00184912b0ff2a684ee1
mitogh <mitogh@gmail.com>
2018-07-12 18:42:53 -0500

    Remove not used dependency

--- src/modules/elements/terms-list/element.js
+++ src/modules/elements/terms-list/element.js
@@ -9,7 +9,7 @@ import { stringify } from 'querystringify';
  * WordPress dependencies
  */
 import { Component, compose } from '@wordpress/element';
-import { Placeholder, Spinner, withAPIData } from '@wordpress/components';
+import { Spinner, withAPIData } from '@wordpress/components';
 import { withSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
