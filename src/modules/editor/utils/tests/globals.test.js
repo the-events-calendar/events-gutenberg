3b058d7080750d7d82cd125556de849c8e07fda1
mitogh <mitogh@gmail.com>
2018-07-16 17:20:57 -0500

    Add tests for global.js utils

---
+++ src/modules/editor/utils/tests/globals.test.js
@@ -0,0 +1,18 @@
+/**
+ * Internal dependencies
+ */
+import {
+	global,
+	google,
+	mapsAPI,
+	settings,
+} from '../globals';
+
+describe( 'Tests for globals.js', () => {
+	test( 'Should match the default value for the globals values', () => {
+		expect( global ).toEqual( window );
+		expect( google ).toEqual( null );
+		expect( settings ).toEqual( {} );
+		expect( mapsAPI ).toEqual( {} );
+	} );
+} );
