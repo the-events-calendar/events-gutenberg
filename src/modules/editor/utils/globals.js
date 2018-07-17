71e5214549ebc8733db34385f1229090b8629616
mitogh <mitogh@gmail.com>
2018-07-16 21:55:19 -0500

    Use functions to access to a global variable instead

--- src/modules/editor/utils/globals.js
+++ src/modules/editor/utils/globals.js
@@ -1,14 +1,8 @@
-/**
- * Internal dependencies
- */
-import { get } from 'lodash';
-
-export const global = window || {};
-
-export const google = get( global, 'google', null );
-export const mapsAPI = get( global, 'tribe_blocks_editor_google_maps_api', {} );
-export const settings = get( global, 'tribe_blocks_editor_settings', {} );
-export const list = {
-	countries: get( global, 'tribe_data_countries', {} ),
-	us_states: get( global, 'tribe_data_us_states', {} ),
-};
+export const get = ( key ) => window[ key ];
+export const google = () => get( 'google' );
+export const mapsAPI = () => get( 'tribe_blocks_editor_google_maps_api' );
+export const settings = () => get( 'tribe_blocks_editor_settings' );
+export const list = () => ( {
+	countries: get( 'tribe_data_countries' ),
+	us_states: get( 'tribe_data_us_states' ),
+} );
