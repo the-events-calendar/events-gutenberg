f03bd02fe7b96a81e63de4255b8b59a1dbda5eee
mitogh <mitogh@gmail.com>
2018-07-16 21:58:00 -0500

    Update globals mock

--- src/modules/editor/utils/tests/globals.test.js
+++ src/modules/editor/utils/tests/globals.test.js
@@ -2,22 +2,39 @@
  * Internal dependencies
  */
 import {
-	global,
 	google,
 	mapsAPI,
 	settings,
 	list,
+	get,
 } from '../globals';
 
 describe( 'Tests for globals.js', () => {
+
+	beforeAll( () => {
+		window.tribe_blocks_editor_google_maps_api = {};
+		window.tribe_blocks_editor_settings = {};
+		window.tribe_data_countries = {};
+		window.tribe_data_us_states = {};
+	} );
+
 	test( 'Should match the default value for the globals values', () => {
-		expect( global ).toEqual( window );
-		expect( google ).toEqual( null );
-		expect( settings ).toEqual( {} );
-		expect( mapsAPI ).toEqual( {} );
-		expect( list ).toEqual( {
+		expect( get( 'random' ) ).toBe( undefined );
+		expect( get( 'google' ) ).toBe( undefined );
+		expect( google() ).toBe( undefined );
+		expect( get( 'tribe_blocks_editor_settings' ) ).toEqual( {} );
+		expect( settings() ).toEqual( {} );
+		expect( mapsAPI() ).toEqual( {} );
+		expect( list() ).toEqual( {
 			countries: {},
 			us_states: {},
 		} );
 	} );
+
+	afterAll( () => {
+		delete window.tribe_blocks_editor_google_maps_api;
+		delete window.tribe_blocks_editor_settings;
+		delete window.tribe_data_countries;
+		delete window.tribe_data_us_states;
+	});
 } );
