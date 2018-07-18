896be8196d52f52f3a0d1afcba7cf2a5567b3a3d
mitogh <mitogh@gmail.com>
2018-07-18 13:10:45 -0500

    Update tests to use "toMatchSnapshot"

--- src/modules/data/search/__tests__/actions.test.js
+++ src/modules/data/search/__tests__/actions.test.js
@@ -7,118 +7,50 @@ import thunk from 'redux-thunk';
 /**
  * Internal dependencies
  */
-import { actions, types } from 'data/search';
+import { actions } from 'data/search';
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
 
 describe( '[STORE] - Search actions', () => {
 	it( 'Should add a block', () => {
-		const expectedAction = {
-			type: types.ADD_BLOCK,
-			payload: {
-				id: 1,
-			},
-		};
-		expect( actions.addBlock( 1 ) ).toEqual( expectedAction );
+		expect( actions.addBlock( 1 ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should set the term', () => {
-		const expectedAction = {
-			type: types.SET_TERM,
-			payload: {
-				id: 1,
-				term: 'lorem',
-			},
-		};
-		expect( actions.setTerm( 1, 'lorem' ) ).toEqual( expectedAction );
+		expect( actions.setTerm( 1, 'lorem' ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should set the results', () => {
-		const expectedAction = {
-			type: types.SET_RESULTS,
-			payload: {
-				id: 1,
-				results: [ 2 ],
-			},
-		};
-		expect( actions.setResults( 1, [ 2 ] ) ).toEqual( expectedAction );
+		expect( actions.setResults( 1, [ 2 ] ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should add results', () => {
-		const expectedAction = {
-			type: types.ADD_RESULTS,
-			payload: {
-				id: 1,
-				results: [ 2 ],
-			},
-		};
-		expect( actions.addResults( 1, [ 2 ] ) ).toEqual( expectedAction );
+		expect( actions.addResults( 1, [ 2 ] ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should set total pages', () => {
-		const expectedAction = {
-			type: types.SET_TOTAL_PAGES,
-			payload: {
-				id: 1,
-				totalPages: 10,
-			},
-		};
-		expect( actions.setTotalPages( 1, 10 ) ).toEqual( expectedAction );
+		expect( actions.setTotalPages( 1, 10 ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should set the page', () => {
-		const expectedAction = {
-			type: types.SET_PAGE,
-			payload: {
-				id: 1,
-				page: 2,
-			},
-		};
-		expect( actions.setPage( 1, 2 ) ).toEqual( expectedAction );
+		expect( actions.setPage( 1, 2 ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should enable loading', () => {
-		const expectedAction = {
-			type: types.SET_SEARCH_LOADING,
-			payload: {
-				id: 1,
-				loading: true,
-			},
-		};
-		expect( actions.enableLoading( 1 ) ).toEqual( expectedAction );
+		expect( actions.enableLoading( 1 ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should disable loading', () => {
-		const expectedAction = {
-			type: types.SET_SEARCH_LOADING,
-			payload: {
-				id: 1,
-				loading: false,
-			},
-		};
-		expect( actions.disableLoading( 1 ) ).toEqual( expectedAction );
+		expect( actions.disableLoading( 1 ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should clear the block', () => {
-		const expectedAction = {
-			type: types.CLEAR_BLOCK,
-			payload: {
-				id: 1,
-			},
-		};
-		expect( actions.clearBlock( 1 ) ).toEqual( expectedAction );
+		expect( actions.clearBlock( 1 ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should set the post type', () => {
-		const expectedAction = {
-			type: types.SET_POST_TYPE,
-			payload: {
-				id: 1,
-				type: 'post',
-			},
-		};
-		expect( actions.setPostType( 1, 'post' ) ).toEqual( expectedAction );
+		expect( actions.setPostType( 1, 'post' ) ).toMatchSnapshot();
 	} );
 } );
 
@@ -126,22 +58,8 @@ describe( '[STORE] - Search thunk actions', () => {
 	it( 'Should register a block', () => {
 		const store = mockStore( {} );
 		store.dispatch( actions.registerBlock( 'test', 'post' ) );
-		const expectedActions = [
-			{
-				type: types.ADD_BLOCK,
-				payload: {
-					id: 'test',
-				},
-			},
-			{
-				type: types.SET_POST_TYPE,
-				payload: {
-					id: 'test',
-					type: 'post',
-				},
-			},
-		];
-		expect( store.getActions() ).toEqual( expectedActions );
+
+		expect( store.getActions() ).toMatchSnapshot();
 	} );
 } );
 
--- src/modules/data/ui/__tests__/reducer.test.js
+++ src/modules/data/ui/__tests__/reducer.test.js
@@ -6,37 +6,26 @@ import moment from 'moment';
 /**
  * Internal dependencies
  */
-import reducer, { DEFAULT_STATE } from 'data/ui/reducer';
-import { actions } from 'data/ui';
+import reducer, { actions } from 'data/ui';
 
 describe( '[STORE] - UI reducer', () => {
 	it( 'Should return the default state', () => {
-		const current = reducer( undefined, {} );
-		expect( current ).toEqual( DEFAULT_STATE );
+		expect( reducer( undefined, {} ) ).toMatchSnapshot();
 	} );
 
 	it( 'Should Set the Dashboard date time by open it', () => {
 		const current = reducer( {}, actions.openDashboard() );
-		const expected = {
-			dashboardDateTimeOpen: true,
-		};
-		expect( current ).toEqual( expected );
+		expect( current ).toMatchSnapshot();
 	} );
 
 	it( 'Should set the Dashboard date time by closing it', () => {
 		const current = reducer( {}, actions.closeDashboard() );
-		const expected = {
-			dashboardDateTimeOpen: false,
-		};
-		expect( current ).toEqual( expected );
+		expect( current ).toMatchSnapshot();
 	} );
 
 	it( 'Should Toggle Dashboard date time', () => {
 		const current = reducer( { dashboardDateTimeOpen: false }, actions.toggleDashboard() );
-		const expected = {
-			dashboardDateTimeOpen: true,
-		};
-		expect( current ).toEqual( expected );
+		expect( current ).toMatchSnapshot();
 	} );
 
 	it( 'Should Set visibility month', () => {
