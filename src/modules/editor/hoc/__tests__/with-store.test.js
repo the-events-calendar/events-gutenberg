0a634d0d25dcf33623e33afcc09db6c1b49767b4
mitogh <mitogh@gmail.com>
2018-07-22 13:43:59 -0500

    Add tests for the HOC with store

---
+++ src/modules/editor/hoc/__tests__/with-store.test.js
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import renderer from 'react-test-renderer';
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { withStore } from 'editor/hoc';
+
+jest.mock( 'data', () => ( {
+	getStore() {
+		return {};
+	},
+} ) );
+
+describe( 'HOC - With Store', () => {
+	it( 'Should add the store property', () => {
+		const Block = ( props ) => <div { ...props } />;
+		const Wrapper = withStore()( Block );
+		const component = renderer.create( <Wrapper /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+
+		const instance = component.root;
+		expect( instance ).not.toBe( null );
+		expect( instance.findByType( Block ).props ).toEqual( { store: {} } );
+	} );
+} );
+
