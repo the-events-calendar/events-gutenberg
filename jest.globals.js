9e48a703f8988ebab966bea5f9848071fd06bf38
Neal Fennimore <hi@neal.codes>
2018-07-27 09:49:04 -0700

    Add in localStorage mock

--- jest.globals.js
+++ jest.globals.js
@@ -9,3 +9,27 @@ global.$ = $;
 global.wp = {
 	element: React,
 };
+
+class LocalStorageMock {
+	constructor() {
+		this.store = {};
+	}
+
+	clear() {
+		this.store = {};
+	}
+
+	getItem( key ) {
+		return this.store[ key ];
+	}
+
+	setItem( key, value ) {
+		this.store[ key ] = value.toString();
+	}
+
+	removeItem( key ) {
+		delete this.store[ key ];
+	}
+}
+
+global.localStorage = new LocalStorageMock;
