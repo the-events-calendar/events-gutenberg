40f153d85948bc2eef31dbd9ea979673a2969526
mitogh <mitogh@gmail.com>
2018-07-30 12:39:01 -0500

    Add support for chrono library

--- src/modules/elements/date-input/element.js
+++ src/modules/elements/date-input/element.js
@@ -3,9 +3,24 @@
  */
 import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
-import { noop } from 'lodash';
+import chrono from 'chrono-node';
+import { noop, isUndefined, isFunction } from 'lodash';
 
+/**
+ * Internal dependencies
+ */
 import './style.pcss';
+import { toDate, toDateTime, toMoment } from 'editor/utils/moment';
+
+const nullableType = ( props, name ) => {
+	if ( isUndefined( props[ name ] ) ) {
+		return;
+	}
+
+	if ( ! isFunction( props[ name ] ) ) {
+		return new Error( `Invalid prop ${ name } should be undefined or a function` );
+	}
+};
 
 class DateInput extends Component {
 	static propTypes = {
@@ -13,6 +28,7 @@ class DateInput extends Component {
 		children: PropTypes.node,
 		placeholder: PropTypes.string,
 		onClickHandler: PropTypes.func,
+		setDateTime: nullableType,
 	};
 
 	static defaultProps = {
@@ -20,12 +36,29 @@ class DateInput extends Component {
 		children: null,
 		placeholder: 'Enter your date',
 		onClickHandler: noop,
+		setDateTime: noop,
 	};
 
 	constructor( props ) {
 		super( props );
 		this.inputRef = createRef();
 	}
+
+	sendDateTime = ( text, reference ) => {
+		const [ parsed ] = chrono.parse( text, reference );
+		if ( ! parsed ) {
+			return;
+		}
+
+		const { start, end } = parsed;
+		const from = start ? toDateTime( toMoment( start.date() ) ) : null;
+		const to = end ? toDate( toMoment( end.date() ) ) : null;
+		this.props.setDateTime( from, to );
+	};
+
+	onBlur = ( event ) => {
+		this.sendDateTime( event.target.value );
+	};
 
 	renderInput() {
 		const { placeholder } = this.props;
@@ -36,14 +69,24 @@ class DateInput extends Component {
 				className="tribe-editor__date-input"
 				ref={ this.inputRef }
 				placeholder={ placeholder }
+				onBlur={ this.onBlur }
 			/>
 		);
 	}
 
 	render() {
-		const { children, selected } = this.props;
+		const {
+			children,
+			selected,
+			onClickHandler,
+		} = this.props;
 		return (
-			<div>
+			<div
+				role="button"
+				tabIndex={ 0 }
+				onClick={ onClickHandler }
+				onKeyDown={ onClickHandler }
+			>
 				{ selected ? this.renderInput() : children }
 			</div>
 		);
