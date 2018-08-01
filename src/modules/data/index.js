85589254b7b980059182d798fa34a1ddae60b7a4
Paul <kim.paul.ms@gmail.com>
2018-08-02 02:49:21 +0800

    remove allday middleware and tests

--- src/modules/data/index.js
+++ src/modules/data/index.js
@@ -6,7 +6,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
-import { dates, multiDay, allDay, endTime, startTime } from 'data/blocks/middlewares';
+import { dates, multiDay, endTime, startTime } from 'data/blocks/middlewares';
 import { wpRequest } from 'data/request/middlewares';
 
 const composeEnhancers = composeWithDevTools( {
@@ -19,7 +19,6 @@ const middlewares = [
 	startTime,
 	endTime,
 	multiDay,
-	allDay,
 	wpRequest,
 ];
 
