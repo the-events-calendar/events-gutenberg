c01aec5e9e13941101e1f0c8f9b0e88a1bdd34dd
mitogh <mitogh@gmail.com>
2018-08-02 16:06:29 -0500

    Update selectors for datetime

--- src/modules/data/blocks/datetime/selectors.js
+++ src/modules/data/blocks/datetime/selectors.js
@@ -39,3 +39,8 @@ export const getTimeZone = createSelector(
 	[ datetimeSelector ],
 	( values ) => values.timezone,
 );
+
+export const getNaturalLanguageLabel = createSelector(
+	[ datetimeSelector ],
+	( values ) => values.naturalLanguage,
+);
