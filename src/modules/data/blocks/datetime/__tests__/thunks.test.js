f6e9113713860f08c7baaa9eb7e08a8e67a5c935
Paul <kim.paul.ms@gmail.com>
2018-08-06 17:46:18 +0800

    add thunk tests for set start time

--- src/modules/data/blocks/datetime/__tests__/thunks.test.js
+++ src/modules/data/blocks/datetime/__tests__/thunks.test.js
@@ -13,9 +13,30 @@ import { thunks } from 'data/blocks/datetime';
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
 
+let initialState = {};
+let store;
+
 describe( '[STORE] - Datetime thunks', () => {
+	beforeEach( () => {
+		initialState = {
+			blocks: {
+				datetime: {
+					start: '2018-06-05 17:00:00',
+					end: '2018-06-05 17:30:00',
+					dateTimeSeparator: ' @ ',
+					timeRangeSeparator: ' - ',
+					allDay: false,
+					multiDay: false,
+					timezone: 'UTC',
+				}
+			}
+		};
+
+		store = mockStore( initialState );
+	} );
+
 	test( 'Initial set of state', () => {
-		const store = mockStore( {} );
+		store = mockStore( {} );
 
 		const attributes = {
 			start: 'June 5, 2018 5:00 pm',
@@ -36,5 +57,43 @@ describe( '[STORE] - Datetime thunks', () => {
 		expect( get ).toHaveBeenCalled();
 		expect( get ).toHaveBeenCalledTimes( 2 );
 		expect( store.getActions() ).toMatchSnapshot();
+	} );
+
+	test( 'Set start time when All Day is not set', () => {
+		const attributes = {
+			start: '2018-06-05 17:00:00',
+			end: '2018-06-05 17:30:00',
+			seconds: 7200,
+			isAllDay: false,
+		};
+
+		store.dispatch( thunks.setStartTime( attributes ) );
+
+		expect( store.getActions() ).toMatchSnapshot();
+	} );
+
+	test( 'Set start time when All Day is set', () => {
+		const attributes = {
+			start: '2018-06-05 17:00:00',
+			end: '2018-06-05 17:30:00',
+			seconds: 0,
+			isAllDay: true,
+		};
+
+		store.dispatch( thunks.setStartTime( attributes ) );
+
+		expect( store.getActions() ).toMatchSnapshot();
+	} );
+
+	test( 'Set end time', () => {
+
+	} );
+
+	test( 'Set dates from date picker', () => {
+
+	} );
+
+	test( 'Set multi day', () => {
+
 	} );
 } );
