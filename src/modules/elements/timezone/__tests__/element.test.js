44449b287654987c6837264fcc661635940bd580
mitogh <mitogh@gmail.com>
2018-08-06 22:34:25 -0500

    Add tests for the timezone element

---
+++ src/modules/elements/timezone/__tests__/element.test.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import { TimeZone } from 'elements';
+
+describe( 'EditLink element', () => {
+	it( 'Should render the component', () => {
+		const component = renderer.create( <TimeZone /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'Should render the value on the input', () => {
+		const component = renderer.create( <TimeZone value="Modern Tribe" /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'Should render the placeholder on the input', () => {
+		const component = renderer.create( <TimeZone placeholder="Love this tribe ! " /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'Should add the class name into the component', () => {
+		const component = renderer.create( <TimeZone className="my-custom-class-name" /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'Should trigger the onChange event', () => {
+		const onChange = jest.fn();
+		const component = mount( <TimeZone value="Modern Tribe" onChange={ onChange } /> );
+		component.find( 'input' ).simulate( 'change' );
+		expect( onChange ).toHaveBeenCalled();
+		expect( onChange ).toHaveBeenCalledWith( 'Modern Tribe' );
+	} );
+} );
