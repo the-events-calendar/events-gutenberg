db3d247b5c8894b4d3021d906d4971f8f3b86f0a
Neal Fennimore <hi@neal.codes>
2018-08-06 18:01:42 -0700

    Update tribe common externals

--- webpack/externals/tribe/common.js
+++ webpack/externals/tribe/common.js
@@ -1,6 +1,6 @@
-const pickBy = require( 'lodash/pickBy' );
-
-// Little messy but no reason to recalculate common entries when it's already been done
-const config = require( '../../../plugins/common/webpack.config.js' );
-
-module.exports = pickBy( config.externals, ( v, k ) => /@tribe\/common/.test( k ) );
+module.exports = {
+	'@moderntribe/common': {
+		var: 'tribe.common',
+		root: [ 'tribe', 'common' ],
+	},
+};
