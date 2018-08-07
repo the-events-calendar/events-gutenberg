05f2f973c086d91626d05d4ae573045826536363
mitogh <mitogh@gmail.com>
2018-08-06 22:34:12 -0500

    Setup enzyme

--- jest.setup.js
+++ jest.setup.js
@@ -4,6 +4,8 @@
 import moment from 'moment-timezone';
 import React from 'react';
 import $ from 'jquery';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 global.jQuery = $;
 global.$ = $;
@@ -12,3 +14,5 @@ global.wp = {
 };
 
 moment.tz.setDefault( 'America/Mexico_City' );
+
+Enzyme.configure( { adapter: new Adapter() } );
