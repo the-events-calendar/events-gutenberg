c1cdb0da90234535b214241039435288e1735666
Neal Fennimore <hi@neal.codes>
2018-08-06 21:35:16 -0700

    Update externals to resolve common directory names

--- webpack/externals/tribe/common.js
+++ webpack/externals/tribe/common.js
@@ -1,6 +1,21 @@
-module.exports = {
-	'@moderntribe/common': {
-		var: 'tribe.common',
-		root: [ 'tribe', 'common' ],
-	},
-};
+const { getDirectoryNames } = require( '../../utils/directories' );
+const { resolve } = require( 'path' );
+
+const directories = getDirectoryNames(
+	resolve( __dirname, '../../../plugins/common/src/modules' )
+);
+
+const generateExternals = ( entries ) => (
+	entries.reduce(
+		( result, entry ) => {
+			result[ `@moderntribe/common/${ entry }` ] = {
+				var: `tribe.common.${ entry }`,
+				root: [ 'tribe', 'common', entry ],
+			};
+			return result;
+		},
+		{}
+	)
+);
+
+module.exports = generateExternals( directories );
