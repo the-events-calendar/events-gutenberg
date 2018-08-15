2cb1853d8966e3b6431543d8f49f8d41fbde49b2
mitogh <mitogh@gmail.com>
2018-08-14 23:09:33 -0500

    Update plugin types to use the utils

--- plugins/common/src/modules/data/reducers/plugins/types.js
+++ plugins/common/src/modules/data/reducers/plugins/types.js
@@ -1,6 +1,9 @@
-export const PREFIX = '@@MT/COMMON';
+/**
+ * Internal dependencies
+ */
+import { withPrefix } from '@moderntribe/common/data/utils';
 
-export const ADD_PLUGIN = `${ PREFIX }/ADD_PLUGIN`;
-export const REMOVE_PLUGIN = `${ PREFIX }/REMOVE_PLUGIN`;
-export const ACTIVATE_PLUGIN = `${ PREFIX }/ACTIVATE_PLUGIN`;
-export const DEACTIVATE_PLUGIN = `${ PREFIX }/DEACTIVATE_PLUGIN`;
+export const ADD_PLUGIN = withPrefix( 'ADD_PLUGIN' );
+export const REMOVE_PLUGIN = withPrefix( 'REMOVE_PLUGIN' );
+export const ACTIVATE_PLUGIN = withPrefix( 'ACTIVATE_PLUGIN' );
+export const DEACTIVATE_PLUGIN = withPrefix( 'DEACTIVATE_PLUGIN' );
