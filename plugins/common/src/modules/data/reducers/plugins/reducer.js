b7029c0327a9164fe5c9af86d81109734f164826
mitogh <mitogh@gmail.com>
2018-08-16 13:31:39 -0500

    Remove activation / deactivation of plugins reducer

--- plugins/common/src/modules/data/reducers/plugins/reducer.js
+++ plugins/common/src/modules/data/reducers/plugins/reducer.js
@@ -11,15 +11,9 @@ import { types } from '@moderntribe/common/data/reducers/plugins';
 export default ( state = {}, action ) => {
 	switch ( action.type ) {
 		case types.ADD_PLUGIN:
-		case types.ACTIVATE_PLUGIN:
 			return {
 				...state,
 				[ action.payload.name ]: true,
-			};
-		case types.DEACTIVATE_PLUGIN:
-			return {
-				...state,
-				[ action.payload.name ]: false,
 			};
 		case types.REMOVE_PLUGIN:
 			return omit( state, action.payload.name );
