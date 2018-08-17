8001ba5e12cb9e11b9dcff175df10faee59d9a98
Juanfra Aldasoro <juan@nicethemes.com>
2018-08-17 16:50:10 -0300

    Add indentation

--- src/views/blocks/rsvp/form/quantity.php
+++ src/views/blocks/rsvp/form/quantity.php
@@ -9,19 +9,19 @@ $must_login = ! is_user_logged_in() && tribe( 'tickets.rsvp' )->login_required()
 $remaining  = $ticket->remaining();
 ?>
 <div class="number-input">
-<button class="minus"></button>
-<input
-	type="number"
-	name="quantity_<?php echo absint( $ticket->ID ); ?>"
-	class="tribe-tickets-quantity"
-	step="1"
-	min="1"
-	value="0"
-	data-remaining="<?php echo esc_attr( $remaining ); ?>"
-	<?php if ( -1 !== $remaining ) : ?>
-		max="<?php echo esc_attr( $remaining ); ?>"
-	<?php endif; ?>
-	<?php disabled( $must_login ); ?>
-/>
-<button class="plus"></button>
+	<button class="minus"></button>
+		<input
+			type="number"
+			name="quantity_<?php echo absint( $ticket->ID ); ?>"
+			class="tribe-tickets-quantity"
+			step="1"
+			min="1"
+			value="0"
+			data-remaining="<?php echo esc_attr( $remaining ); ?>"
+			<?php if ( -1 !== $remaining ) : ?>
+				max="<?php echo esc_attr( $remaining ); ?>"
+			<?php endif; ?>
+			<?php disabled( $must_login ); ?>
+		/>
+	<button class="plus"></button>
 </div>
