79d5d519fffc58911a2f2e6b1c8b87628ca6c4ef
mitogh <mitogh@gmail.com>
2018-08-17 11:36:45 -0500

    Update reducer to use an array instead of object

--- plugins/common/src/modules/data/reducers/plugins/__tests__/reducer.test.js
+++ plugins/common/src/modules/data/reducers/plugins/__tests__/reducer.test.js
@@ -5,15 +5,20 @@ import reducer, { actions } from '@moderntribe/common/data/reducers/plugins';
 
 describe( 'Plugins reducer', () => {
 	it( 'Should return the default state', () => {
-		expect( reducer( undefined, {} ) ).toEqual( {} );
+		expect( reducer( undefined, {} ) ).toEqual( [] );
 	} );
 
 	it( 'Should add a new plugin as active', () => {
-		expect( reducer( {}, actions.addPlugin( 'events' ) ) ).toMatchSnapshot();
+		expect( reducer( {}, actions.addPlugin( 'events' ) ) ).toEqual( [ 'events' ] );
+	} );
+
+	it( 'Should avoid adding duplicates entries', () => {
+		const state = reducer( {}, actions.addPlugin( 'events' ) );
+		expect( reducer( state, actions.addPlugin( 'events' ) ) ).toEqual( [ 'events' ] );
 	} );
 
 	it( 'Should remove the plugin from the reducer', () => {
 		const state = reducer( {}, actions.addPlugin( 'events' ) );
-		expect( reducer( state, actions.removePlugin( 'events' ) ) ).toMatchSnapshot();
+		expect( reducer( state, actions.removePlugin( 'events' ) ) ).toEqual( [] );
 	} );
 } );
--- plugins/common/src/modules/data/reducers/plugins/reducer.js
+++ plugins/common/src/modules/data/reducers/plugins/reducer.js
@@ -1,22 +1,19 @@
 /**
  * External dependencies
  */
-import { omit } from 'lodash';
+import { uniq } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { types } from '@moderntribe/common/data/reducers/plugins';
 
-export default ( state = {}, action ) => {
+export default ( state = [], action ) => {
 	switch ( action.type ) {
 		case types.ADD_PLUGIN:
-			return {
-				...state,
-				[ action.payload.name ]: true,
-			};
+			return uniq( [ ...state, action.payload.name ] );
 		case types.REMOVE_PLUGIN:
-			return omit( state, action.payload.name );
+			return [ ...state ].filter( ( pluginName ) => pluginName !== action.payload.name );
 		default:
 			return state;
 	}
