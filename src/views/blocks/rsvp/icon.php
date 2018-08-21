886284923b0d5dc58dd41c6ef5d3060d86f0fb5b
Juanfra Aldasoro <juan@nicethemes.com>
2018-08-21 20:08:48 +0200

    Use SVG file content

--- src/views/blocks/rsvp/icon.php
+++ src/views/blocks/rsvp/icon.php
@@ -7,6 +7,6 @@
  */
 ?>
 <div class="tribe-block__rsvp__icon">
-	<img src="<?php echo tribe( 'gutenberg' )->plugin_url . 'src/resources/icons/rsvp.svg'; ?>" />
+	<?php echo file_get_contents( tribe( 'gutenberg' )->plugin_url . 'src/resources/icons/rsvp.svg' ); ?>
 	<?php esc_html_e( 'RSVP', 'events-gutenberg' ) ?>
 </div>
