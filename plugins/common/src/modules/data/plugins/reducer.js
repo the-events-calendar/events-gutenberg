8e139825064e5a09c23c1976ef51fcf98bddb9a5
Neal Fennimore <hi@neal.codes>
2018-08-22 08:31:32 -0700

    Revert plugin reducer changes

--- plugins/common/src/modules/data/plugins/reducer.js
+++ plugins/common/src/modules/data/plugins/reducer.js
@@ -1,20 +1,19 @@
 /**
+ * External dependencies
+ */
+import { uniq } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { types } from '@moderntribe/common/data/plugins';
 
-export default ( state = {}, action ) => {
+export default ( state = [], action ) => {
 	switch ( action.type ) {
 		case types.ADD_PLUGIN:
-			return {
-				...state,
-				[ action.payload.name ]: true,
-			};
+			return uniq( [ ...state, action.payload.name ] );
 		case types.REMOVE_PLUGIN:
-			return {
-				...state,
-				[ action.payload.name ]: false,
-			};
+			return [ ...state ].filter( ( pluginName ) => pluginName !== action.payload.name );
 		default:
 			return state;
 	}
