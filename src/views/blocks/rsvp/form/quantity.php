5400717ff7030e4ac4474f4a4145a35b194e6ddf
Juanfra Aldasoro <juan@nicethemes.com>
2018-08-22 18:18:50 +0200

    Add tpl for the quantity input.

--- src/views/blocks/rsvp/form/quantity.php
+++ src/views/blocks/rsvp/form/quantity.php
@@ -5,23 +5,11 @@
  * @version TBD
  *
  */
-$must_login = ! is_user_logged_in() && tribe( 'tickets.rsvp' )->login_required();
-$remaining  = $ticket->remaining();
 ?>
 <div class="tribe-block__rsvp__number-input">
 	<?php $this->template( 'blocks/rsvp/form/quantity-minus' ); ?>
-		<input
-			type="number"
-			name="quantity_<?php echo absint( $ticket->ID ); ?>"
-			class="tribe-tickets-quantity"
-			step="1"
-			min="1"
-			value="0"
-			data-remaining="<?php echo esc_attr( $remaining ); ?>"
-			<?php if ( -1 !== $remaining ) : ?>
-				max="<?php echo esc_attr( $remaining ); ?>"
-			<?php endif; ?>
-			<?php disabled( $must_login ); ?>
-		/>
+
+	<?php $this->template( 'blocks/rsvp/form/quantity-input', array( 'ticket' => $ticket ) ); ?>
+
 	<?php $this->template( 'blocks/rsvp/form/quantity-plus' ); ?>
 </div>
