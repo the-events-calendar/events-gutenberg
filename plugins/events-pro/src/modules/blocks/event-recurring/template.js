179458b4bd44360046a5fa6354b9563fdb1fe6e6
Neal Fennimore <hi@neal.codes>
2018-08-24 14:19:38 -0700

    Consolidate event pro blocks

--- plugins/events-pro/src/modules/blocks/event-recurring/template.js
+++ plugins/events-pro/src/modules/blocks/event-recurring/template.js
@@ -8,16 +8,30 @@ import PropTypes from 'prop-types';
  * Internal dependencies
  */
 import RecurringForm from '@moderntribe/events-pro/elements/recurring-form/element';
+import ExceptionForm from '@moderntribe/events-pro/elements/exception-form/element';
+import RecurringAddField from '@moderntribe/events-pro/elements/recurring-add-field/element';
+import ExceptionAddField from '@moderntribe/events-pro/elements/exception-add-field/element';
 
 export default class EventRecurring extends PureComponent {
 	static propTypes = {}
 
 	constructor( props ) {
 		super( props );
+
+		console.warn( props );
 	}
+
 	render() {
 		return (
-			<RecurringForm />
+			<section>
+				<RecurringForm />
+				<ExceptionForm />
+
+				<footer>
+					<RecurringAddField />
+					<ExceptionAddField />
+				</footer>
+			</section>
 		);
 	}
 }
