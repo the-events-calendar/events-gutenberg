762c95f1d95d576eeccee4e648ebb487d4e67137
Neal Fennimore <hi@neal.codes>
2018-08-27 15:16:11 -0700

    Add in event-recurring ui toggle flow

--- plugins/events-pro/src/modules/blocks/event-recurring/container.js
+++ plugins/events-pro/src/modules/blocks/event-recurring/container.js
@@ -17,13 +17,40 @@ import EventRecurringBlock from './template';
 
 const mapStateToProps = state => ( {
 	isRepeatBlockVisible: selectors.isRepeatBlockVisible( state ),
+	isRulePanelVisible: selectors.isRulePanelVisible( state ),
+	isExceptionPanelVisible: selectors.isExceptionPanelVisible( state ),
+	isRulePanelExpanded: selectors.isRulePanelExpanded( state ),
+	isExceptionPanelExpanded: selectors.isExceptionPanelExpanded( state ),
 } );
 
 const mapDispatchToProps = dispatch => ( {
 	toggleRepeatBlocksVisibility: () => dispatch( actions.toggleRepeatBlocksVisibility() ),
+	toggleRulePanelVisibility: () => dispatch( actions.toggleRulePanelVisibility() ),
+	toggleExceptionPanelVisibility: () => dispatch( actions.toggleExceptionPanelVisibility() ),
+	toggleRulePanelExpand: () => dispatch( actions.toggleRulePanelExpand() ),
+	toggleExceptionPanelExpand: () => dispatch( actions.toggleExceptionPanelExpand() ),
+} );
+
+const mergeProps = ( stateProps, dispatchProps, ownProps ) => ( {
+	...stateProps,
+	...dispatchProps,
+	...ownProps,
+	initialRepeatBlockClick: compose(
+		dispatchProps.toggleRulePanelVisibility,
+		dispatchProps.toggleRulePanelExpand,
+		dispatchProps.toggleRepeatBlocksVisibility,
+	),
+	initialRulePanelClick: compose(
+		dispatchProps.toggleRulePanelVisibility,
+		dispatchProps.toggleRulePanelExpand,
+	),
+	initialExceptionPanelClick: compose(
+		dispatchProps.toggleExceptionPanelVisibility,
+		dispatchProps.toggleExceptionPanelExpand,
+	),
 } );
 
 export default compose(
 	withStore(),
-	connect( mapStateToProps, mapDispatchToProps )
+	connect( mapStateToProps, mapDispatchToProps, mergeProps )
 )( EventRecurringBlock );
--- plugins/events-pro/src/modules/elements/index.js
+++ plugins/events-pro/src/modules/elements/index.js
@@ -4,3 +4,5 @@ export { default as ExceptionForm } from './exception-form/element';
 export { default as RecurringAddField } from './recurring-add-field/element';
 export { default as RecurringField } from './recurring-field/element';
 export { default as RecurringForm } from './recurring-form/element';
+export { default as Panel } from './panel/element';
+export { default as PanelHeader } from './panel-header/element';
