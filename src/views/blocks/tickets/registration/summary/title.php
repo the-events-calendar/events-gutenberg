d766ff80808da9782ff4217927145a6150630600
Juanfra Aldasoro <juan@nicethemes.com>
2018-08-30 19:04:55 +0200

    Add heading tag

--- src/views/blocks/tickets/registration/summary/title.php
+++ src/views/blocks/tickets/registration/summary/title.php
@@ -7,7 +7,9 @@
  */
 ?>
 <div class="tribe-block__tickets__registration__title">
-	<header><?php esc_html_e( 'Ticket Registration', 'events-gutenberg' ); ?></header>
+	<header>
+		<h2><?php esc_html_e( 'Ticket Registration', 'events-gutenberg' ); ?></h2>
+	</header>
 </div>
 
 
