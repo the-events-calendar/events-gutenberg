57e97591952cc3c527d6f1b631ac27414e45ffeb
Paul <kim.paul.ms@gmail.com>
2018-09-01 00:51:54 +0800

    add button component

---
+++ plugins/tickets/src/modules/elements/button/element.js
@@ -0,0 +1,37 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const Button = ( {
+	className,
+	isDisabled,
+	label,
+	onClick,
+	type,
+} ) => (
+	<button
+		className={ classnames( 'tribe-tickets__button', className ) }
+		disabled={ isDisabled }
+		type={ type }
+		onClick={ onClick }
+	>
+		{ label }
+	</button>
+);
+
+Button.defaultProps = {
+	type: 'button',
+};
+
+Button.propTypes = {
+	className: PropTypes.string,
+	isDisabled: PropTypes.bool,
+	label: PropTypes.node,
+	onClick: PropTypes.func,
+	type: PropTypes.string,
+};
+
+export default Button;
