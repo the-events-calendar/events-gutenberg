76d6a0996199ff89bc65cc4dae6b6fadddf47deb
mitogh <mitogh@gmail.com>
2018-08-31 14:36:43 -0500

    Add TODO about plugin name

--- plugins/tickets/src/modules/data/index.js
+++ plugins/tickets/src/modules/data/index.js
@@ -8,7 +8,7 @@ import { store } from '@moderntribe/common/store';
 
 export const initStore = () => {
 	const { dispatch, injectReducers } = store;
-
+	// TODO: use `constants` from `recurrence` branch to add the plugin name into the constants.
 	dispatch( actions.addPlugin( 'tickets' ) );
 	injectReducers( { [ 'tickets' ]: reducer } );
 };
