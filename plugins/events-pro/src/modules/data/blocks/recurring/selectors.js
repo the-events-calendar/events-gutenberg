363f51cf9543633666cbb1b2eede532de5446847
Neal Fennimore <hi@neal.codes>
2018-08-31 05:42:14 -0700

    Add in more ui actions

--- plugins/events-pro/src/modules/data/blocks/recurring/selectors.js
+++ plugins/events-pro/src/modules/data/blocks/recurring/selectors.js
@@ -4,4 +4,4 @@
 import { createSelector } from 'reselect';
 import { constants } from '@moderntribe/common/data/plugins';
 
-export const getRecurring = ( state ) => state[ constants.EVENTS_PRO_PLUGIN ].blocks.recurring;
+export const getRules = ( state ) => state[ constants.EVENTS_PRO_PLUGIN ].blocks.recurring;
--- plugins/events-pro/src/modules/data/ui/reducer.js
+++ plugins/events-pro/src/modules/data/ui/reducer.js
@@ -23,20 +23,40 @@ export default ( state = DEFAULT_STATE, action ) => {
 				...state,
 				isRulePanelVisible: ! state.isRulePanelVisible,
 			};
+		case types.HIDE_RULE_PANEL:
+			return {
+				...state,
+				isRulePanelVisible: false,
+			};
 		case types.TOGGLE_EXCEPTION_PANEL_VISIBILITY:
 			return {
 				...state,
 				isExceptionPanelVisible: ! state.isExceptionPanelVisible,
+			};
+		case types.HIDE_EXCEPTION_PANEL:
+			return {
+				...state,
+				isExceptionPanelVisible: false,
 			};
 		case types.TOGGLE_RULE_PANEL_EXPAND:
 			return {
 				...state,
 				isRulePanelExpanded: ! state.isRulePanelExpanded,
 			};
+		case types.EXPAND_RULE_PANEL:
+			return {
+				...state,
+				isRulePanelExpanded: true,
+			};
 		case types.TOGGLE_EXCEPTION_PANEL_EXPAND:
 			return {
 				...state,
 				isExceptionPanelExpanded: ! state.isExceptionPanelExpanded,
+			};
+		case types.EXPAND_EXCEPTION_PANEL:
+			return {
+				...state,
+				isExceptionPanelExpanded: true,
 			};
 		default:
 			return state;
