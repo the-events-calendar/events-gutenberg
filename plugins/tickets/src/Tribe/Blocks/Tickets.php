378e9de38cd2f076b1ca9cad962a0719cfd9a8c0
mitogh <mitogh@gmail.com>
2018-08-31 11:59:58 -0500

    Register block views

---
+++ plugins/tickets/src/Tribe/Blocks/Tickets.php
@@ -0,0 +1,32 @@
+<?php
+class Tribe__Gutenberg__Tickets__Blocks__Tickets
+	extends Tribe__Gutenberg__Events__Blocks__Abstract {
+
+	/**
+	 * Which is the name/slug of this block
+	 *
+	 * @since  0.2.1-alpha
+	 *
+	 * @return string
+	 */
+	public function slug() {
+		return 'event-tickets';
+	}
+
+	/**
+	 * Since we are dealing with a Dynamic type of Block we need a PHP method to render it
+	 *
+	 * @since  0.2.1-alpha
+	 *
+	 * @param  array $attributes
+	 *
+	 * @return string
+	 */
+	public function render( $attributes = array() ) {
+		$args['attributes'] = $this->attributes( $attributes );
+
+		// Add the rendering attributes into global context
+		tribe( 'gutenberg.tickets.template' )->add_template_globals( $args );
+		return tribe( 'gutenberg.tickets.template' )->template( array( 'blocks', $this->slug() ), $args, false );
+	}
+}
