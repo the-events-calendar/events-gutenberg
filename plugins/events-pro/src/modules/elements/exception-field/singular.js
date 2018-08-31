830141013417af6c045fe9fa3858b9131187a1c8
Neal Fennimore <hi@neal.codes>
2018-08-31 10:04:49 -0700

    Add in multi-day exception cases

--- plugins/events-pro/src/modules/elements/exception-field/singular.js
+++ plugins/events-pro/src/modules/elements/exception-field/singular.js
@@ -11,6 +11,9 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import {
+	toMoment,
+} from '@moderntribe/events/editor/utils/moment';
 import { Select } from '@moderntribe/common/components/form';
 import { Row, Label, DatePickerInput } from '@moderntribe/events-pro/elements';
 import { options } from '@moderntribe/events-pro/data/blocks/exception';
@@ -19,6 +22,7 @@ const SingularField = ( {
 	fieldType,
 	multiDay,
 	start,
+	end,
 } ) => {
 	return (
 		<Fragment>
@@ -36,13 +40,27 @@ const SingularField = ( {
 			</Row>
 			<Row>
 				<Label>
-					{ __( 'On', 'events-gutenberg' ) }
+					{
+						multiDay
+							? __( 'From', 'events-gutenberg' )
+							: __( 'On', 'events-gutenberg' )
+					}
 				</Label>
 				<div className="tribe-events-pro-exception-field__time">
 					<DatePickerInput
-						datetime={ start }
+						datetime={ toMoment( start ).add( 1, 'day' ) }
 						// TODO: Add onChange handler
 					/>
+					{
+						multiDay && (
+							<Fragment>
+								<span>{ __( 'to', 'events-gutenberg' ) }</span>
+								<DatePickerInput
+									datetime={ toMoment( start ).add( 2, 'days' ) }
+								/>
+							</Fragment>
+						)
+					}
 					<div className="tribe-events-pro-exception-field__time__multi-day">
 						<ToggleControl
 							label={ __( 'Multi-Day', 'events-gutenberg' ) }
@@ -61,6 +79,7 @@ SingularField.propTypes = {
 	fieldType: PropTypes.string.isRequired,
 	multiDay: PropTypes.bool.isRequired,
 	start: PropTypes.string.isRequired,
+	end: PropTypes.string.isRequired,
 };
 
 export default SingularField;
