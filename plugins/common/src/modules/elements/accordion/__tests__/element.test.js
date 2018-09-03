63e840b82b070c6413dc6a81b5a01720826ed2ce
Paul <kim.paul.ms@gmail.com>
2018-09-03 23:42:40 +0800

    add tests for accordion

---
+++ plugins/common/src/modules/elements/accordion/__tests__/element.test.js
@@ -0,0 +1,49 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+/**
+ * Internal dependencies
+ */
+import { Accordion } from '@moderntribe/common/elements';
+
+let rows;
+
+describe( 'Accordion Element', () => {
+	beforeEach( () => {
+		rows = [
+			{
+				content: "this is a content",
+				contentClassName: "content-class",
+				contentId: "content-id-1",
+				header: "this is header",
+				headerClassName: "header-class",
+				headerId: "header-id-1",
+				isActive: true,
+			},
+		];
+	} );
+
+	it( 'renders null', () => {
+		const component = renderer.create( <Accordion /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'renders an accordion', () => {
+		const component = renderer.create( <Accordion rows={ rows } /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'renders an accordion with wrapper class', () => {
+		const component = renderer.create( <Accordion className="test-class" rows={ rows } /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'renders an accordion with inactive state', () => {
+		rows[0].isActive = false;
+		const component = renderer.create( <Accordion rows={ rows } /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+} );
