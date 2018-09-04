42f376703b4ae2eebf4fd2f0b57977f5314fc84c
Paul <kim.paul.ms@gmail.com>
2018-09-04 20:39:30 +0800

    add tooltip element

---
+++ plugins/common/src/modules/elements/tooltip/element.js
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import { Tooltip } from '@wordpress/components';
+
+const Tooltip = ( {
+	label,
+	text,
+} ) => {
+	const attrs = {
+		'aria-label': text,
+	};
+
+	return (
+		<Tooltip text={ text } position={ position}>
+			{/* @TODO: Fill in with icon */}
+			<Button
+				attrs={ attrs }
+				className={ classnames( 'tribe-editor__tooltip-icon', iconClassName ) }
+				label={ label }
+			/>
+		</Tooltip>
+	)
+};
+
+Tooltip.defaultProps = {
+	position: 'top right',
+};
+
+Tooltip.propTypes = {
+	label: PropTypes.node,
+	position: PropTypes.oneOf( [
+		'top left',
+		'top center',
+		'top right',
+		'bottom left',
+		'bottom center',
+		'bottom right',
+	] ),
+	text: PropTypes.string.isRequired,
+};
