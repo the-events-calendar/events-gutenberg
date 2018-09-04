2f40ad49e2d6e5f2fe76bd07e3c233325dc32529
mitogh <mitogh@gmail.com>
2018-09-04 05:54:33 -0500

    Remove not existing export

--- plugins/common/src/modules/elements/index.js
+++ plugins/common/src/modules/elements/index.js
@@ -1,8 +1,2 @@
-/**
- * Internal dependencies
- */
 export { default as BlockIcon } from '@moderntribe/common/elements/block-icon';
-import * as Icons from '@moderntribe/common/elements/icons/element';
-
-export { Icons };
 export { default as Button } from './button/element';
