25f27630333c719abbe102c2924571e3db794d59
Paul <kim.paul.ms@gmail.com>
2018-09-05 15:19:59 +0800

    add chainable validator creator, add tests

--- plugins/common/src/modules/utils/__tests__/proptypes.test.js
+++ plugins/common/src/modules/utils/__tests__/proptypes.test.js
@@ -1,39 +1,101 @@
 /**
+ * External dependencies
+ */
+import { noop } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import * as proptypes from '@moderntribe/common/utils/proptypes';
 
 describe( 'Tests for proptypes utils', () => {
-	it( 'should not throw an error when provided proper time formatted string', () => {
-		const props = {
-			time: '15:34',
-		};
-		const timeFormat = jest.fn( () => proptypes.timeFormat( props, 'time', 'component' ) );
-		timeFormat();
+	describe( 'createChainableValidator', () => {
+		it( 'should not return an error when prop is undefined and is not required', () => {
+			const props = {};
+			const chainedValidator = jest.fn( proptypes.createChainableValidator( noop ) );
+			chainedValidator( props, 'time', 'component' );
 
-		expect( timeFormat ).toHaveReturned();
-		expect( timeFormat ).toHaveReturnedWith( null );
+			expect( chainedValidator ).toHaveReturned();
+			expect( chainedValidator ).toHaveReturnedWith( null );
+		} );
+
+		it( 'should return an error when prop is undefined and is required', () => {
+			const props = {};
+			const chainedValidator = jest.fn( proptypes.createChainableValidator( noop ).isRequired );
+			chainedValidator( props, 'time', 'component' );
+
+			expect( chainedValidator ).toHaveReturned();
+			expect( chainedValidator ).toHaveReturnedWith( Error( 'The prop `time` is marked as required in `component`, but its value is `undefined`.' ) );
+		} );
+
+		it( 'should return an error when prop is null and is required', () => {
+			const props = {
+				time: null,
+			};
+			const chainedValidator = jest.fn( proptypes.createChainableValidator( noop ).isRequired );
+			chainedValidator( props, 'time', 'component' );
+
+			expect( chainedValidator ).toHaveReturned();
+			expect( chainedValidator ).toHaveReturnedWith( Error( 'The prop `time` is marked as required in `component`, but its value is `null`.' ) );
+		} );
+
+		it( 'should call the validator when prop is provided, not undefined or null, and is not required', () => {
+			const props = {
+				time: '15:34',
+			};
+			const validator = jest.fn( noop );
+			const chainedValidator = jest.fn( proptypes.createChainableValidator( validator ) );
+			chainedValidator( props, 'time', 'component' );
+
+			expect( validator ).toHaveBeenCalled();
+			expect( validator ).toHaveBeenCalledTimes( 1 );
+		} );
+
+		it( 'should call the validator when prop is provided, not undefined or null, and is required', () => {
+			const props = {
+				time: '15:34',
+			};
+			const validator = jest.fn( noop );
+			const chainedValidator = jest.fn( proptypes.createChainableValidator( validator ).isRequired );
+			chainedValidator( props, 'time', 'component' );
+
+			expect( validator ).toHaveBeenCalled();
+			expect( validator ).toHaveBeenCalledTimes( 1 );
+		} );
 	} );
 
-	it( 'should throw an error when not provided a string', () => {
-		const props = {
-			time: true,
-		};
-		const timeFormat = jest.fn( () => proptypes.timeFormat( props, 'time', 'component' ) );
-		timeFormat();
+	describe( 'timeFormat', () => {
+		it( 'should not return an error when provided proper time formatted string', () => {
+			const props = {
+				time: '15:34',
+			};
+			const timeFormat = jest.fn( () => proptypes.timeFormat( props, 'time', 'component' ) );
+			timeFormat();
 
-		expect( timeFormat ).toHaveReturned();
-		expect( timeFormat ).toHaveReturnedWith( Error( "Warning: Invalid prop 'time' of type 'boolean' supplied to 'component', expected 'string'." ) );
-	} );
+			expect( timeFormat ).toHaveReturned();
+			expect( timeFormat ).toHaveReturnedWith( null );
+		} );
 
-	it( 'should throw an error when not provided proper time format', () => {
-		const props = {
-			time: 'random string',
-		};
-		const timeFormat = jest.fn( () => proptypes.timeFormat( props, 'time', 'component' ) );
-		timeFormat();
+		it( 'should return an error when not provided a string', () => {
+			const props = {
+				time: true,
+			};
+			const timeFormat = jest.fn( () => proptypes.timeFormat( props, 'time', 'component' ) );
+			timeFormat();
 
-		expect( timeFormat ).toHaveReturned();
-		expect( timeFormat ).toHaveReturnedWith( Error( "Warning: Invalid format for prop 'time' supplied to 'component', expected time format 'hh:mm'." ) );
+			expect( timeFormat ).toHaveReturned();
+			expect( timeFormat ).toHaveReturnedWith( Error( 'Invalid prop `time` of type `boolean` supplied to `component`, expected `string`.' ) );
+		} );
+
+		it( 'should return an error when not provided proper time format', () => {
+			const props = {
+				time: 'random string',
+			};
+			const timeFormat = jest.fn( () => proptypes.timeFormat( props, 'time', 'component' ) );
+			timeFormat();
+
+			expect( timeFormat ).toHaveReturned();
+			expect( timeFormat ).toHaveReturnedWith( Error( 'Invalid prop `time` shape supplied to `component`, expected `hh:mm`.' ) );
+		} );
 	} );
 } );
--- plugins/common/src/modules/utils/proptypes.js
+++ plugins/common/src/modules/utils/proptypes.js
@@ -1,20 +1,56 @@
 /**
+ * Create chainable validator with isRequired check
+ * @param {function} validator
+ */
+export const createChainableValidator = ( validator ) => {
+	const createChainedValidator = (
+		isRequired,
+		props,
+		propName,
+		componentName,
+	) => {
+		const propValue = props[ propName ];
+
+		if ( propValue == null ) {
+			if ( isRequired ) {
+				if ( propValue === null ) {
+					/* eslint-disable-next-line max-len */
+					return new Error( 'The prop `' + propName + '` is marked as required in `' + componentName + '`, but its value is `null`.' );
+				}
+				/* eslint-disable-next-line max-len */
+				return new Error( 'The prop `' + propName + '` is marked as required in `' + componentName + '`, but its value is `undefined`.' );
+			}
+			return null;
+		} else {
+			return validator( props, propName, componentName );
+		}
+	};
+
+	const chainedValidator = createChainedValidator.bind( null, false );
+	chainedValidator.isRequired = createChainedValidator.bind( null, true );
+
+	return chainedValidator;
+}
+
+/**
  * PropTypes check for type string and time format
  * @param {object} props
  * @param {string} propName
  * @param {string} componentName
  */
 export const timeFormat = ( props, propName, componentName ) => {
-	if ( typeof props[ propName ] !== 'string' ) {
-		const type = typeof props[ propName ];
+	const propValue = props[ propName ];
+
+	if ( typeof propValue !== 'string' ) {
+		const type = typeof propValue;
 		/* eslint-disable-next-line max-len */
-		return new Error( `Warning: Invalid prop '${propName}' of type '${type}' supplied to '${componentName}', expected 'string'.` );
+		return new Error('Invalid prop `' + propName + '` of type `' + type + '` supplied to `' + componentName + '`, expected `string`.' );
 	}
 
 	const timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-	if ( ! timeRegex.test( props[ propName ] ) ) {
+	if ( ! timeRegex.test( propValue ) ) {
 		/* eslint-disable-next-line max-len */
-		return new Error( `Warning: Invalid format for prop '${propName}' supplied to '${componentName}', expected time format 'hh:mm'.` );
+		return new Error('Invalid prop `' + propName + '` shape supplied to `' + componentName + '`, expected `hh:mm`.' );
 	}
 
 	return null;
