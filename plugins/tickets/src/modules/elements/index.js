0b5a50f6b4964a1a0b58a32ecb261f5640527309
Paul <kim.paul.ms@gmail.com>
2018-09-07 02:47:58 +0800

    add label with tooltip element

--- plugins/tickets/src/modules/elements/index.js
+++ plugins/tickets/src/modules/elements/index.js
@@ -1,2 +1,3 @@
 export { default as ActionDashboard } from './action-dashboard/element';
 export { default as DateTimeRangePicker } from './date-time-range-picker/element';
+export { default as LabaelWithTooltip } from './label-with-tooltip/element';
---
+++ plugins/tickets/src/modules/elements/label-with-tooltip/element.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import { Tooltip } from '@moderntribe/common/elements';
+
+const LabelWithTooltip = ( {
+	className,
+	label,
+	tooltipLabel,
+	tooltipText,
+} ) => (
+	<div className={ classNames(
+		'tribe-editor__label-with-tooltip',
+		className,
+	) }>
+		<span className="tribe-editor__label-with-tooltip__label">
+			{ label }
+		</span>
+		<Tooltip label={ tooltipLabel } text={ tooltipText } />
+	</div>
+);
+
+LabelWithTooltip.defaultProps = {
+	label: '',
+};
+
+LabelWithTooltip.propTypes = {
+	className: PropTypes.string,
+	label: PropTypes.node,
+	tooltipLabel: PropTypes.node,
+	tooltipText: PropTypes.string,
+};
+
+export default LabelWithTooltip;
