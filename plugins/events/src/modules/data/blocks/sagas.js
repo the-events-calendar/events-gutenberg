279d6983a49e3e656b6fff06380c93341a13e0d0
Neal Fennimore <hi@neal.codes>
2018-09-07 10:32:19 -0700

    Update sharing block thunk to saga

--- plugins/events/src/modules/data/blocks/sagas.js
+++ plugins/events/src/modules/data/blocks/sagas.js
@@ -10,9 +10,11 @@ import { store } from '@moderntribe/common/store';
 import * as price from '@moderntribe/events/data/blocks/price';
 import * as venue from '@moderntribe/events/data/blocks/venue';
 import * as website from '@moderntribe/events/data/blocks/website';
+import * as sharing from '@moderntribe/events/data/blocks/sharing';
 
 [
 	price.sagas,
 	venue.sagas,
 	website.sagas,
+	sharing.sagas,
 ].forEach( sagas => store.run( sagas ) );
