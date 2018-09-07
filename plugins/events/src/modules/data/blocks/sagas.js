89d198787fce46f19b5fb5cc8adfde841e0251f4
Neal Fennimore <hi@neal.codes>
2018-09-07 09:32:11 -0700

    Update venue thunk initial state to saga

--- plugins/events/src/modules/data/blocks/sagas.js
+++ plugins/events/src/modules/data/blocks/sagas.js
@@ -8,7 +8,9 @@ import { store } from '@moderntribe/common/store';
  * Internal dependencies
  */
 import * as price from '@moderntribe/events/data/blocks/price';
+import * as venue from '@moderntribe/events/data/blocks/venue';
 
 [
 	price.sagas,
+	venue.sagas,
 ].forEach( sagas => store.run( sagas ) );
