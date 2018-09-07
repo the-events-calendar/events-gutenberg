0bd3f89a09e15be885ea9e6854ccbf95e66b0e2a
mitogh <mitogh@gmail.com>
2018-09-06 19:20:32 -0500

    Create a new `<HeaderImage>` component
    Component that allows to fetch / select an existing media from the
    media library

---
+++ plugins/tickets/src/modules/elements/header-image/element.js
@@ -0,0 +1,56 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { __ } from '@wordpress/i18n';
+import { MediaUpload } from '@wordpress/editor';
+import { noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { Button } from '@moderntribe/common/elements';
+import './style.pcss';
+
+export const renderMediaUpload = ( { open } ) => (
+	<Button
+		onClick={ open }
+		className={ [ 'tribe-editor__btn--label', 'tribe-editor__header-image__btn--upload' ] }
+	>
+		{ __( 'Upload Image', 'events-gutenberg' ) }
+	</Button>
+);
+
+const HeaderImage = ( { title, description, buttonLabel, onSelect, mediaId } ) => (
+	<div className="tribe-editor__header-image">
+		{ title && <strong className="tribe-editor__header-image__title">{ title }</strong> }
+		<div className="tribe-editor__header-image__content">
+			<div className="tribe-editor__header-image__description">{ description }</div>
+			<MediaUpload
+				onSelect={ onSelect }
+				type="image"
+				value={ mediaId }
+				render={ renderMediaUpload }
+			/>
+		</div>
+	</div>
+);
+
+HeaderImage.propTypes = {
+	title: PropTypes.string,
+	description: PropTypes.string,
+	mediaId: PropTypes.number,
+	onSelect: PropTypes.func.isRequired,
+};
+
+HeaderImage.defaultProps = {
+	title: __( 'Header Image', 'events-gutenberg' ),
+	description: __(
+		'Select an image from your Media Library to display on emailed tickets. For best results, use a .jpg, .png, or .gif at least 1160px wide.',
+		'events-gutenberg'
+	),
+	onSelect: noop,
+};
+
+export default HeaderImage;
