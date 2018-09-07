81da93d46cfc58ae8786e64e924fb5e87931ace3
Neal Fennimore <hi@neal.codes>
2018-09-07 09:39:27 -0700

    Add in website sagas

--- plugins/events/src/modules/data/blocks/sagas.js
+++ plugins/events/src/modules/data/blocks/sagas.js
@@ -9,8 +9,10 @@ import { store } from '@moderntribe/common/store';
  */
 import * as price from '@moderntribe/events/data/blocks/price';
 import * as venue from '@moderntribe/events/data/blocks/venue';
+import * as website from '@moderntribe/events/data/blocks/website';
 
 [
 	price.sagas,
 	venue.sagas,
+	website.sagas,
 ].forEach( sagas => store.run( sagas ) );
