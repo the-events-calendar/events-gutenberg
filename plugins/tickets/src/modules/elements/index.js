1f529897ef1f27bf98da2bc08128d3a5dfa865db
mitogh <mitogh@gmail.com>
2018-09-07 13:15:27 -0500

    Register the component and styles for the CapacityTable

--- plugins/tickets/src/modules/elements/index.js
+++ plugins/tickets/src/modules/elements/index.js
@@ -2,3 +2,4 @@ export { default as ActionDashboard } from './action-dashboard/element';
 export { default as Availability } from './availability/element';
 export { default as ActionButton } from './action-button/element';
 export { default as NumericLabel } from './numeric-label/element';
+export { default as CapacityTable } from './capacity-table/element';
