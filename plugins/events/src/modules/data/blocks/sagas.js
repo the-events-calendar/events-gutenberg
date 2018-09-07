9773a3ac1ab7c7980e80ddd6351fa0685e41f40f
Neal Fennimore <hi@neal.codes>
2018-09-07 10:37:44 -0700

    Update classic block thunk to saga

--- plugins/events/src/modules/data/blocks/sagas.js
+++ plugins/events/src/modules/data/blocks/sagas.js
@@ -11,10 +11,12 @@ import * as price from '@moderntribe/events/data/blocks/price';
 import * as venue from '@moderntribe/events/data/blocks/venue';
 import * as website from '@moderntribe/events/data/blocks/website';
 import * as sharing from '@moderntribe/events/data/blocks/sharing';
+import * as classic from '@moderntribe/events/data/blocks/classic';
 
 [
 	price.sagas,
 	venue.sagas,
 	website.sagas,
 	sharing.sagas,
+	classic.sagas,
 ].forEach( sagas => store.run( sagas ) );
