4dd1ed5cc6b98f45eb045c1403c18c9e7fecb05c
mitogh <mitogh@gmail.com>
2018-09-10 22:34:44 -0500

    Add ticket template file

---
+++ plugins/tickets/src/modules/blocks/ticket/Template.js
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import { TicketStatus } from '@moderntribe/tickets/elements';
+
+const Ticket = () => ( <TicketStatus /> );
+
+export default Ticket;
