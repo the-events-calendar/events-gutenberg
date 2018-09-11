579b10d20287124f517608d5669cbc404101e685
mitogh <mitogh@gmail.com>
2018-09-11 11:59:28 -0500

    Add placeholder properties

--- plugins/tickets/src/modules/blocks/ticket/Container.js
+++ plugins/tickets/src/modules/blocks/ticket/Container.js
@@ -14,6 +14,11 @@ import { withSaveData, withStore } from '@moderntribe/common/src/modules/hoc';
 
 const mapStateToProps = ( state, props ) => ( {
 	isSelected: props.isSelected,
+	title: 'Balcony',
+	description: 'General admission',
+	price: 125,
+	sold: 71,
+	available: 156,
 } );
 
 export default compose(
