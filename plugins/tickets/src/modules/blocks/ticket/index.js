472229a7b8b13a21d5bcf67b7c432f0655ed5fe4
mitogh <mitogh@gmail.com>
2018-09-10 21:21:07 -0500

    Add single ticket block declaration

---
+++ plugins/tickets/src/modules/blocks/ticket/index.js
@@ -0,0 +1,30 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { InnerBlocks } from '@wordpress/editor';
+
+
+/**
+ * Internal dependencies
+ */
+import { BlockIcon } from '@moderntribe/common/elements';
+
+export default {
+	id: 'event-tickets-ticket',
+	title: __( 'Event Ticket', 'events-gutenberg' ),
+	description: __( 'Entry for ticket', 'events-gutenberg' ),
+	icon: BlockIcon,
+	category: 'tribe-tickets',
+	keywords: [ 'event', 'events-tickets', 'tribe' ],
+
+	parent: [ 'tribe/event-tickets' ],
+
+	supports: {
+		html: false,
+	},
+
+	attributes: {},
+	edit: ( { clientId } ) => <div>Ticket { clientId }</div>,
+	save: () => null,
+};
