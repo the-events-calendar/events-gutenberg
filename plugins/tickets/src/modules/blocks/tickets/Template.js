86a65ee8a70a39edf728eca4ccb2373fdf958ff0
mitogh <mitogh@gmail.com>
2018-09-13 11:43:47 -0500

    Draft component structure

--- plugins/tickets/src/modules/blocks/tickets/Template.js
+++ plugins/tickets/src/modules/blocks/tickets/Template.js
@@ -21,9 +21,9 @@ const disabled = {
 	title: __( 'No Active Tickets', 'events-gutenberg' ),
 	description: __(
 		'The time is curently outside of the ticket sales window. Make adjustments to the start and end date to activate these tickets.',
-		'events-gutenberg'
+		'events-gutenberg',
 	),
-}
+};
 
 const TicketsTemplate = ( props ) => {
 	const {
@@ -40,7 +40,7 @@ const TicketsTemplate = ( props ) => {
 	} = props;
 
 	const availability = isSelected && (
-		<Availability available={ available } total={ total } />
+		<Availability available={ available } total={ total }/>
 	);
 
 	const actionDashboard = isSelected && (
@@ -58,29 +58,68 @@ const TicketsTemplate = ( props ) => {
 			) }
 		>
 			<div className="tribe-editor__tickets-body">
-				<InnerBlocks />
+				<InnerBlocks/>
 				<DisabledTickets title={ disabled.title }>
 					{ disabled.description }
 				</DisabledTickets>
 			</div>
+			<section className="tribe-editor__tickets-form">
+				<div></div>
+				<div className="tribe-editor__tickets-form__container">
+					<div className="tribe-editor__tickets-form__body">
+						<div className="tribe-editor__tickets-form__row">
+							<div className="tribe-editor__tickets-form__labels">
+								<label>Ticket Capacity</label>
+							</div>
+							<div className="tribe-editor__tickets-form__input-group">
+								<select>
+									<option>Share capacity with other tickets</option>
+									<option>Set capacity for this ticket only</option>
+									<option>unlimited</option>
+								</select>
+								<div className="tribe-editor__tickets-form__input-row">
+									<label htmlFor="ticket-available">Number of tickets available</label>
+									<input type="number" id="ticket-available" />
+								</div>
+								<div className="tribe-editor__tickets-form__input-row">
+									<label htmlFor="ticket-limit">(optional) Limit sales of this ticket to:</label>
+									<input type="number" id="ticket-limit"/>
+								</div>
+								<div className="tribe-editor__tickets-form__input-row">
+									<label htmlFor="ticket-capacity">Capacity</label>
+									<input type="number" id="ticket-capacity" />
+								</div>
+							</div>
+						</div>
+						<div className="tribe-editor__tickets-attendee">
+							<span>Attendee Registration</span>
+							<button className="tribe-editor__btn--label">+Add</button>
+						</div>
+					</div>
+					<div className="tribe-editor__tickets-form__footer">
+						<button className="tribe-editor__btn--label">Cancel</button>
+						<button className="tribe-editor__btn--label">Create Ticket</button>
+					</div>
+				</div>
+			</section>
 			{ availability }
 			{ actionDashboard }
 		</div>
 	);
-}
+};
 
 TicketsTemplate.propTypes = {
 	isSelected: PropTypes.bool,
 	footerActions: PropTypes.arrayOf( PropTypes.node ),
 	footerConfirmLabel: PropTypes.string,
 	allowedBlockTypes: PropTypes.arrayOf( PropTypes.string ),
-}
+};
 
 TicketsTemplate.defaultProps = {
 	isSelected: false,
 	footerActions: [],
 	footerConfirmLabel: __( 'Add Tickets', 'events-gutenberg' ),
 	allowedBlockTypes: [],
-}
+};
 
 export default TicketsTemplate;
