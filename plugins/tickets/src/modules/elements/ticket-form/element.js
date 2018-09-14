99ab5c252efdca4cbf10c165451e84942c7358d5
mitogh <mitogh@gmail.com>
2018-09-14 15:29:29 -0500

    Fix linting issues

--- plugins/tickets/src/modules/elements/ticket-form/element.js
+++ plugins/tickets/src/modules/elements/ticket-form/element.js
@@ -71,3 +71,45 @@ TicketForm.defaultProps = {
 };
 
 export default TicketForm;
+
+/*
+		<section className="tribe-editor__tickets-form">
+				<div></div>
+				<div className="tribe-editor__tickets-form__container">
+					<div className="tribe-editor__tickets-form__body">
+						<div className="tribe-editor__tickets-form__row">
+							<div className="tribe-editor__tickets-form__labels">
+								<label>Ticket Capacity</label>
+							</div>
+							<div className="tribe-editor__tickets-form__input-group">
+								<select>
+									<option>Share capacity with other tickets</option>
+									<option>Set capacity for this ticket only</option>
+									<option>unlimited</option>
+								</select>
+								<div className="tribe-editor__tickets-form__input-row">
+									<label htmlFor="ticket-available">Number of tickets available</label>
+									<input type="number" id="ticket-available" />
+								</div>
+								<div className="tribe-editor__tickets-form__input-row">
+									<label htmlFor="ticket-limit">(optional) Limit sales of this ticket to:</label>
+									<input type="number" id="ticket-limit"/>
+								</div>
+								<div className="tribe-editor__tickets-form__input-row">
+									<label htmlFor="ticket-capacity">Capacity</label>
+									<input type="number" id="ticket-capacity" />
+								</div>
+							</div>
+						</div>
+						<div className="tribe-editor__tickets-attendee">
+							<span>Attendee Registration</span>
+							<button className="tribe-editor__btn--label">+Add</button>
+						</div>
+					</div>
+					<div className="tribe-editor__tickets-form__footer">
+						<button className="tribe-editor__btn--label">Cancel</button>
+						<button className="tribe-editor__btn--label">Create Ticket</button>
+					</div>
+				</div>
+			</section>
+ */
