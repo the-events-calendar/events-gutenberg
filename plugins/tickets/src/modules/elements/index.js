aedadf35d45b06f67c2e7aca6494d7eaa7c339de
mitogh <mitogh@gmail.com>
2018-09-14 20:14:09 -0500

    Update ticket form to split into smaller chunks

--- plugins/tickets/src/modules/elements/index.js
+++ plugins/tickets/src/modules/elements/index.js
@@ -9,3 +9,4 @@ export { default as HeaderImage } from './header-image/element';
 export { default as DisabledTickets } from './disabled-tickets/element';
 export { default as TicketStatus } from './ticket-status/element';
 export { default as QuantityBar } from './quantity-bar/element';
+export { default as TicketForm } from './ticket-form/element';
--- plugins/tickets/src/modules/elements/ticket-form/element.js
+++ plugins/tickets/src/modules/elements/ticket-form/element.js
@@ -18,8 +18,9 @@ import {
 } from '@moderntribe/common/elements';
 import { DateTimeRangePicker } from '@moderntribe/tickets/elements';
 
-import TicketFooter from './ticket-footer/template';
-import TicketCapacity from './ticket-capacity';
+import AttendeesRegistration from './attendees-registration/template';
+import Capacity from './capacity/template';
+import Footer from './footer/template';
 import './style.pcss';
 
 const TicketForm = ( props ) => {
@@ -36,13 +37,10 @@ const TicketForm = ( props ) => {
 			<div></div>
 			<div className="tribe-editor__tickets-form__container">
 				<div className="tribe-editor__tickets-form__body">
-					<TicketCapacity label={ ticketCapacityLabel } tooltip={ ticketCapacityToolTip } />
-					<div className="tribe-editor__tickets-attendee">
-						<span>{ attendeeRegistrationLabel }</span>
-						<Button className="tribe-editor__btn--label">{ attendeeButtonLabel }</Button>
-					</div>
+					<Capacity label={ ticketCapacityLabel } tooltip={ ticketCapacityToolTip } />
+					<AttendeesRegistration />
 				</div>
-				<TicketFooter { ...props } />
+				<Footer />
 			</div>
 		</section>
 	);
@@ -51,65 +49,11 @@ const TicketForm = ( props ) => {
 TicketForm.propTypes = {
 	ticketCapacityLabel: PropTypes.string,
 	ticketCapacityToolTip: PropTypes.string,
-	attendeeRegistrationLabel: PropTypes.string,
-	attendeeButtonLabel: PropTypes.string,
-	cancelLabel: PropTypes.string,
-	createLabel: PropTypes.string,
-	onCreate: PropTypes.func,
-	onCancel: PropTypes.func,
 };
 
 TicketForm.defaultProps = {
 	ticketCapacityLabel: __( 'Ticket Capacity', 'events-gutenberg' ),
 	ticketCapacityToolTip: __( 'Ticket capacity', 'events-gutenberg' ),
-	attendeeRegistrationLabel: __( 'Attendee Registration', 'events-gutenberg' ),
-	attendeeButtonLabel: __( '+Add', 'events-gutenberg' ),
-	cancelLabel: __( 'Cancel', 'events-gutenberg' ),
-	createLabel: __( 'Create Ticket', 'events-gutenberg' ),
-	onCreate: noop,
-	onCancel: noop,
 };
 
 export default TicketForm;
-
-/*
-		<section className="tribe-editor__tickets-form">
-				<div></div>
-				<div className="tribe-editor__tickets-form__container">
-					<div className="tribe-editor__tickets-form__body">
-						<div className="tribe-editor__tickets-form__row">
-							<div className="tribe-editor__tickets-form__labels">
-								<label>Ticket Capacity</label>
-							</div>
-							<div className="tribe-editor__tickets-form__input-group">
-								<select>
-									<option>Share capacity with other tickets</option>
-									<option>Set capacity for this ticket only</option>
-									<option>unlimited</option>
-								</select>
-								<div className="tribe-editor__tickets-form__input-row">
-									<label htmlFor="ticket-available">Number of tickets available</label>
-									<input type="number" id="ticket-available" />
-								</div>
-								<div className="tribe-editor__tickets-form__input-row">
-									<label htmlFor="ticket-limit">(optional) Limit sales of this ticket to:</label>
-									<input type="number" id="ticket-limit"/>
-								</div>
-								<div className="tribe-editor__tickets-form__input-row">
-									<label htmlFor="ticket-capacity">Capacity</label>
-									<input type="number" id="ticket-capacity" />
-								</div>
-							</div>
-						</div>
-						<div className="tribe-editor__tickets-attendee">
-							<span>Attendee Registration</span>
-							<button className="tribe-editor__btn--label">+Add</button>
-						</div>
-					</div>
-					<div className="tribe-editor__tickets-form__footer">
-						<button className="tribe-editor__btn--label">Cancel</button>
-						<button className="tribe-editor__btn--label">Create Ticket</button>
-					</div>
-				</div>
-			</section>
- */
