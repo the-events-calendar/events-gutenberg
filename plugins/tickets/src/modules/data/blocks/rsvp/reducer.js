175ac3d13b71be447fc918066fa122346bf4518d
Paul <kim.paul.ms@gmail.com>
2018-09-16 22:39:36 +0800

    fix broken tests

--- plugins/tickets/src/modules/data/blocks/rsvp/reducer.js
+++ plugins/tickets/src/modules/data/blocks/rsvp/reducer.js
@@ -10,9 +10,9 @@ import { types } from '@moderntribe/tickets/data/blocks/rsvp';
 import { moment as momentUtil } from '@moderntribe/common/utils';
 
 export const DEFAULT_STATE = {
-	title: 'July Meetup',
-	description: 'Drinks and food provided',
-	capacity: '30',
+	title: '',
+	description: '',
+	capacity: '',
 	notGoingResponses: false,
 	startDate: momentUtil.toDate( moment() ),
 	endDate: momentUtil.toDate( moment() ),
