28a631019b4b94a56d6113f90753a1c92796989e
mitogh <mitogh@gmail.com>
2018-09-17 17:46:05 -0500

    Update tests

--- plugins/tickets/src/modules/elements/ticket-form/advanced-options/__tests__/template.test.js
+++ plugins/tickets/src/modules/elements/ticket-form/advanced-options/__tests__/template.test.js
@@ -10,7 +10,16 @@ import AdvancedOptions from '../template';
 
 describe( 'AdvancedOptions', () => {
 	test( 'default props of and state of <AdvancedOptions />', () => {
-		const component = renderer.create( <AdvancedOptions /> );
+		const onClick = jest.fn();
+		const component = renderer.create(
+			<AdvancedOptions
+				accordionId="ticketsPlaceholder"
+				contentId="ticketsPlaceholder"
+				headerId="ticketsPlaceholder"
+				isActive={ false }
+				onClick={ onClick }
+			/>
+		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 } );
