dfcad81740fa055163b5b6e8f981d50fd87152f9
mitogh <mitogh@gmail.com>
2018-09-16 21:57:19 -0500

    Update ticket form template

--- plugins/tickets/src/modules/elements/ticket-form/element.js
+++ plugins/tickets/src/modules/elements/ticket-form/element.js
@@ -3,7 +3,6 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import noop from 'lodash/noop';
 
 /**
  * Wordpress dependencies
@@ -13,34 +12,26 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import {
-	Button,
-} from '@moderntribe/common/elements';
-import { DateTimeRangePicker } from '@moderntribe/tickets/elements';
-
+import Header from './header/template';
 import AttendeesRegistration from './attendees-registration/template';
 import Capacity from './capacity/template';
 import Footer from './footer/template';
-import SKU from './sku/template';
+import AdvancedOptions from './advanced-options/template';
 import './style.pcss';
 
 const TicketForm = ( props ) => {
 	const {
 		ticketCapacityLabel,
 		ticketCapacityToolTip,
-		attendeeRegistrationLabel,
-		attendeeButtonLabel,
-		createLabel,
-		cancelLabel,
 	} = props;
 	return (
 		<section className="tribe-editor__tickets-form">
-			<div></div>
+			<Header />
 			<div className="tribe-editor__tickets-form__container">
 				<div className="tribe-editor__tickets-form__body">
 					<Capacity label={ ticketCapacityLabel } tooltip={ ticketCapacityToolTip } />
+					<AdvancedOptions />
 					<AttendeesRegistration />
-					<SKU onChange={ ( v ) => console.log( v ) }/>
 				</div>
 				<Footer />
 			</div>
