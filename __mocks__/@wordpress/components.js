c64de6905e4224e5574ee51a3574087d8b30a878
mitogh <mitogh@gmail.com>
2018-09-16 22:23:16 -0500

    Update mocks for dropdown

--- __mocks__/@wordpress/components.js
+++ __mocks__/@wordpress/components.js
@@ -6,3 +6,4 @@ import { noop } from 'lodash';
 export const withAPIData = () => noop;
 export const Spinner = () => "🏃‍♂️";
 export const Dashicon = ( { className, icon } ) => <span className={ className }>{ icon }</span>;
+export const Dropdown = () => <span>Dropdown</span>;
