ccbec4d8f4909a75bedd33efbc2dd4cfb00e28c4
Paul <kim.paul.ms@gmail.com>
2018-09-17 23:48:34 +0800

    build counter element

--- plugins/common/src/modules/elements/index.js
+++ plugins/common/src/modules/elements/index.js
@@ -2,5 +2,6 @@ export { default as Accordion } from './accordion/element';
 export { default as BlockIcon } from '@moderntribe/common/elements/block-icon';
 export { default as Button } from './button/element';
 export { default as Checkbox } from './checkbox/element';
+export { default as Counter } from './counter/element';
 export { default as TimePicker } from './time-picker/element';
 export { default as Tooltip } from './tooltip/element';
