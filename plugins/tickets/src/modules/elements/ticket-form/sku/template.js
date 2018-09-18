0a00772cb47929180371702879bf51c640b4c01c
mitogh <mitogh@gmail.com>
2018-09-17 20:41:59 -0500

    Add support for tooltip inside of the SKU field

--- plugins/tickets/src/modules/elements/ticket-form/sku/template.js
+++ plugins/tickets/src/modules/elements/ticket-form/sku/template.js
@@ -9,16 +9,23 @@ import noop from 'lodash/noop';
  * Wordpress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Dashicon } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import { sendValue } from '@moderntribe/common/utils/input'
+import { sendValue } from '@moderntribe/common/utils/input';
+import { LabelWithTooltip } from '@moderntribe/tickets/elements';
 
-const SKU = ( { onChange, value, label } ) => (
+const SKU = ( { onChange, value, label, tooltip } ) => (
 	<div className="tribe-editor__tickets-form__row">
 		<div className="tribe-editor__tickets-form__labels">
-			<label htmlFor="ticket-sku">{ label }</label>
+			<LabelWithTooltip
+				id="ticket-sku"
+				label={ label }
+				tooltipText={ tooltip }
+				tooltipLabel={ <Dashicon icon="info-outline" /> }
+			/>
 		</div>
 		<div className="tribe-editor__tickets-form__input-group">
 			<input id="ticket-sku" type="text" value={ value } onChange={ sendValue( onChange ) } />
@@ -28,12 +35,17 @@ const SKU = ( { onChange, value, label } ) => (
 
 SKU.propTypes = {
 	label: PropTypes.string,
+	tooltip: PropTypes.string,
 	onChange: PropTypes.func.isRequired,
 	value: PropTypes.string,
 }
 
 SKU.defaultProps = {
 	label: __( 'Ticket SKU', 'events-gutenberg' ),
+	tooltip: __(
+		'A unique identifying code for each ticket type you\'re selling',
+		'events-gutenberg'
+	),
 	onChange: noop,
 	value: '',
 }
