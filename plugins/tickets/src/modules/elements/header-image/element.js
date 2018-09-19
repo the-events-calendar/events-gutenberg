d0cc3246755490d3e147c00588167a5c1200a6c7
mitogh <mitogh@gmail.com>
2018-09-19 13:08:57 -0500

    Refactor component to render image and add removal button
    - Add removal image button
    - Render provided image from props
    - Split conditional renders into variables to make it more easier to
    visualize

--- plugins/tickets/src/modules/elements/header-image/element.js
+++ plugins/tickets/src/modules/elements/header-image/element.js
@@ -5,7 +5,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
 import { MediaUpload } from '@wordpress/editor';
-import { noop } from 'lodash';
+import noop from 'lodash/noop';
+import partial from 'lodash/partial';
 
 /**
  * Internal dependencies
@@ -22,28 +23,55 @@ export const renderMediaUpload = ( { open } ) => (
 	</Button>
 );
 
-const HeaderImage = ( { title, description, buttonLabel, onSelect, mediaId } ) => (
-	<div className="tribe-editor__header-image">
-		{ title && <h3 className="tribe-editor__header-image__title">{ title }</h3> }
-		<div className="tribe-editor__header-image__content">
-			{ description && (
-				<div className="tribe-editor__header-image__description">{ description }</div>
-			) }
-			<MediaUpload
-				onSelect={ onSelect }
-				type="image"
-				value={ mediaId }
-				render={ renderMediaUpload }
-			/>
+const HeaderImage = ( props ) => {
+	const { title, description, buttonLabel, onSelect, mediaId, image, removeLabel } = props;
+
+	const maybeRenderTitle = title && (
+		<h3 className="tribe-editor__header-image__title">{ title }</h3>
+	);
+
+	const maybeRenderDescription = description && (
+		<div className="tribe-editor__header-image__description">{ description }</div>
+	);
+
+	const maybeRenderImage = ( image && image.url ) && (
+		<figure className="tribe-editor__header-image__display">
+			<img src={ image.url } alt={ title } />
+			<button className="tribe-editor__btn--label" onClick={ partial( onSelect, null ) }>
+				{ removeLabel }
+			</button>
+		</figure>
+	);
+
+	return (
+		<div className="tribe-editor__header-image">
+			{ maybeRenderTitle }
+			<div className="tribe-editor__header-image__content">
+				{ maybeRenderDescription }
+				<MediaUpload
+					onSelect={ onSelect }
+					type="image"
+					value={ mediaId }
+					render={ renderMediaUpload }
+				/>
+			</div>
+			{ maybeRenderImage }
 		</div>
-	</div>
-);
+	);
+};
 
 HeaderImage.propTypes = {
 	title: PropTypes.string,
 	description: PropTypes.string,
 	mediaId: PropTypes.number,
 	onSelect: PropTypes.func.isRequired,
+	removeLabel: PropTypes.string,
+	image: PropTypes.shape( {
+		height: PropTypes.number,
+		width: PropTypes.number,
+		url: PropTypes.string,
+		orientation: PropTypes.string,
+	} ),
 };
 
 HeaderImage.defaultProps = {
@@ -53,6 +81,7 @@ HeaderImage.defaultProps = {
 		'Select an image from your Media Library to display on emailed tickets. For best results, use a .jpg, .png, or .gif at least 1160px wide.',
 		'events-gutenberg'
 	),
+	removeLabel: __( 'Remove', 'events-gutenberg' ),
 	onSelect: noop,
 };
 
