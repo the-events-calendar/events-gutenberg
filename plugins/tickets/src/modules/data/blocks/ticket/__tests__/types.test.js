109888e5e008c94b3d54f513182d0bd0d0b7cc09
mitogh <mitogh@gmail.com>
2018-09-19 13:07:06 -0500

    Add types for the ticket reducer
    The types created here are used inside of header and shared capacity
    fields of the reducer

---
+++ plugins/tickets/src/modules/data/blocks/ticket/__tests__/types.test.js
@@ -0,0 +1,14 @@
+/**
+ * Internal dependencies
+ */
+import { PREFIX_TICKETS_STORE } from '@moderntribe/tickets/data/utils';
+import { types } from '@moderntribe/tickets/data/blocks/ticket';
+
+describe( 'Tickets block types', () => {
+	test( 'Use the prefix on the types', () => {
+		expect( types.SET_TICKET_SHARED_CAPACITY )
+			.toBe( `${PREFIX_TICKETS_STORE}/SET_TICKET_SHARED_CAPACITY` );
+		expect( types.SET_TICKET_HEADER )
+			.toBe( `${PREFIX_TICKETS_STORE}/SET_TICKET_HEADER` );
+	} );
+} );
---
+++ plugins/tickets/src/modules/data/blocks/ticket/types.js
@@ -0,0 +1,7 @@
+/**
+ * Internal dependencies
+ */
+import { PREFIX_TICKETS_STORE } from '@moderntribe/tickets/data/utils';
+
+export const SET_TICKET_HEADER = `${ PREFIX_TICKETS_STORE }/SET_TICKET_HEADER`;
+export const SET_TICKET_SHARED_CAPACITY = `${ PREFIX_TICKETS_STORE }/SET_TICKET_SHARED_CAPACITY`;
