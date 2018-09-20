63c83a4296328896e87fa01f32d212231b97741a
mitogh <mitogh@gmail.com>
2018-09-20 11:23:46 -0500

    Add TODO anotation to keep track of central question

--- plugins/events/src/modules/blocks/event-organizer/template.js
+++ plugins/events/src/modules/blocks/event-organizer/template.js
@@ -131,6 +131,8 @@ class EventOrganizer extends Component {
 
 		removeOrganizerInBlock( clientId, organizer );
 
+		// TODO: make sure this one is provided by the container in mapDispatchToProps, as both
+		// TODO: methods / logic are very similar. See https://github.com/moderntribe/events-gutenberg/pull/259/files#r211099809
 		if ( volatile ) {
 			maybeRemoveEntry( details );
 			removeOrganizerInClassic( organizer );
