beed70e68bb0263955de15948fdaf766e32dc108
mitogh <mitogh@gmail.com>
2018-09-20 18:27:00 -0500

    Remove references to removed components

--- plugins/tickets/src/modules/elements/index.js
+++ plugins/tickets/src/modules/elements/index.js
@@ -1,13 +1,8 @@
 export { default as ActionButton } from './action-button/element';
 export { default as ActionDashboard } from './action-dashboard/element';
-export { default as Availability } from './availability/element';
 export { default as ContainerPanel } from './container-panel/element';
 export { default as DateTimeRangePicker } from './date-time-range-picker/element';
 export { default as LabelWithTooltip } from './label-with-tooltip/element';
 export { default as NumericLabel } from './numeric-label/element';
-export { default as CapacityTable } from './capacity-table/element';
 export { default as InactiveBlock } from './inactive-block/element';
-export { default as TicketStatus } from './ticket-status/element';
-export { default as QuantityBar } from './quantity-bar/element';
 export { default as SettingsDashboard } from './settings-dashboard/element';
-export { default as TicketForm } from './ticket-form/element';
