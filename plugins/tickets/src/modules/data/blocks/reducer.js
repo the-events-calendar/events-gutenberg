8817cd3b155ffd601f4f79472e418e1fcfeaeab4
mitogh <mitogh@gmail.com>
2018-09-19 23:48:49 -0500

    Add ticket reducer inside of the store

--- plugins/tickets/src/modules/data/blocks/reducer.js
+++ plugins/tickets/src/modules/data/blocks/reducer.js
@@ -7,7 +7,9 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import rsvp from './rsvp';
+import ticket from './ticket';
 
 export default combineReducers( {
 	rsvp,
+	ticket,
 } );
