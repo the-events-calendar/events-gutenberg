1aaf46dd5a2199aaa33df1fef7dac6024ceeba35
mitogh <mitogh@gmail.com>
2018-09-20 19:29:42 -0500

    Add missing property into the RSVP action button

--- plugins/tickets/src/modules/blocks/rsvp/rsvp-action-buttons/attendees-action-button/template.js
+++ plugins/tickets/src/modules/blocks/rsvp/rsvp-action-buttons/attendees-action-button/template.js
@@ -16,23 +16,22 @@ import { __ } from '@wordpress/i18n';
 import { ActionButton } from '@moderntribe/tickets/elements';
 import { User as UserIcon } from '@moderntribe/common/src/modules/icons';
 
-const AttendeesActionButton = ( {
-	onClick,
-} ) => (
+const AttendeesActionButton = ( { onClick } ) => (
 	<ActionButton
 		className="tribe-editor__rsvp__action-button tribe-editor__rsvp__action-button--attendees"
+		onClick={ onClick }
 		icon={ <UserIcon /> }
 	>
 		{ __( 'Attendees', 'events-gutenberg' ) }
 	</ActionButton>
 );
 
-AttendeesActionButton.defaultProps = {
-	onClick: noop,
-};
-
 AttendeesActionButton.propTypes = {
 	onClick: PropTypes.func,
+};
+
+AttendeesActionButton.defaultProps = {
+	onClick: noop,
 };
 
 export default AttendeesActionButton;
