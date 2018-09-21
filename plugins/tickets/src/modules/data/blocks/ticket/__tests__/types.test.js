28c5097c84c14993d56ecd3688c0f34450af3f6b
mitogh <mitogh@gmail.com>
2018-09-21 16:50:15 -0500

    Add new type to handle the setup of the settings

--- plugins/tickets/src/modules/data/blocks/ticket/__tests__/types.test.js
+++ plugins/tickets/src/modules/data/blocks/ticket/__tests__/types.test.js
@@ -7,8 +7,10 @@ import { types } from '@moderntribe/tickets/data/blocks/ticket';
 describe( 'Tickets block types', () => {
 	test( 'Use the prefix on the types', () => {
 		expect( types.SET_TICKET_SHARED_CAPACITY )
-			.toBe( `${PREFIX_TICKETS_STORE}/SET_TICKET_SHARED_CAPACITY` );
+			.toBe( `${ PREFIX_TICKETS_STORE }/SET_TICKET_SHARED_CAPACITY` );
 		expect( types.SET_TICKET_HEADER )
-			.toBe( `${PREFIX_TICKETS_STORE}/SET_TICKET_HEADER` );
+			.toBe( `${ PREFIX_TICKETS_STORE }/SET_TICKET_HEADER` );
+		expect( types.SET_TICKET_SETTINGS_OPEN )
+			.toBe( `${ PREFIX_TICKETS_STORE }/SET_TICKET_SETTINGS_OPEN` );
 	} );
 } );
