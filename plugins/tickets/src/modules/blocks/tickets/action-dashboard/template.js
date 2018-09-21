25e3df5a934f66f1968b625580c1d3e7bf6deb59
mitogh <mitogh@gmail.com>
2018-09-21 14:13:17 -0500

    Add action dashboard for the tickets section

---
+++ plugins/tickets/src/modules/blocks/tickets/action-dashboard/template.js
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { ActionDashboard } from '@moderntribe/tickets/elements';
+import {
+	SettingsActionButton,
+	AttendeesActionButton,
+	OrdersActionButton,
+} from '@moderntribe/tickets/blocks/tickets/action-buttons';
+
+const actions = [
+	<SettingsActionButton />,
+	<AttendeesActionButton />,
+	<OrdersActionButton />,
+];
+
+const confirmLabel = ( created ) => (
+	created
+		? __( 'Update Ticket', 'events-gutenberg' )
+		: __( 'Create Ticket', 'events-gutenberg' )
+);
+
+const cancelLabel = __( 'Cancel', 'events-gutenberg' );
+
+const TicketsDashboardAction = ( { created } ) => (
+	<ActionDashboard
+		actions={ actions }
+		cancelLabel={ cancelLabel }
+		confirmLabel={ confirmLabel( created ) }
+	/>
+);
+
+TicketsDashboardAction.propTypes = {
+	created: PropTypes.bool,
+};
+
+export default TicketsDashboardAction;
