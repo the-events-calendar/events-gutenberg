71eb83d9e139ae8f59583d3088be8cd683fb0e5e
mitogh <mitogh@gmail.com>
2018-09-21 14:12:54 -0500

    Remove non required props from the container

--- plugins/tickets/src/modules/blocks/tickets/container.js
+++ plugins/tickets/src/modules/blocks/tickets/container.js
@@ -6,38 +6,16 @@ import { compose } from 'redux';
 import React from 'react';
 
 /**
- * WordPress dependencies
- */
-import { __ } from '@wordpress/i18n';
-
-/**
  * Internal dependencies
  */
 import Template from './template';
 import { withSaveData, withStore } from '@moderntribe/common/src/modules/hoc';
-import { ActionButton } from '@moderntribe/tickets/elements';
 import { selectors, actions } from '@moderntribe/tickets/data/blocks/ticket';
-import {
-	Cog as CogIcon,
-	Tag as TagIcon,
-	User as UserIcon,
-} from '@moderntribe/common/src/modules/icons';
 
 const mapStateToProps = ( state, ownProps ) => ( {
-	isSelected: true,
+	isSelected: ownProps.isSelected,
 	available: 48,
 	total: 166,
-	footerActions: [
-		<ActionButton icon={ <CogIcon /> }>
-			{ __( 'Settings', 'events-gutenberg' ) }
-		</ActionButton>,
-		<ActionButton icon={ <UserIcon /> }>
-			{ __( 'Attendees', 'events-gutenberg' ) }
-		</ActionButton>,
-		<ActionButton icon={ <TagIcon /> }>
-			{ __( 'Orders', 'events-gutenberg' ) }
-		</ActionButton>,
-	],
 	allowedBlockTypes: [ 'tribe/event-tickets', 'tribe/event-tickets-ticket',  'core/image' ],
 	headerImageId: selectors.getImageID( state ),
 	headerImage: selectors.getHeaderSize( state, { size: 'large' } ),
