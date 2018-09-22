9ea981bf3ab0ccb9107a00c07587c24848c49512
mitogh <mitogh@gmail.com>
2018-09-21 21:36:40 -0500

    Update location of the attendees registration element

--- plugins/tickets/src/modules/blocks/tickets/edit-container/content/attendees-registration/template.js
+++ plugins/tickets/src/modules/blocks/tickets/edit-container/content/attendees-registration/template.js
@@ -13,35 +13,30 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { Button } from '@moderntribe/common/elements';
+import { LabelWithModal } from '@moderntribe/common/elements';
 import './style.pcss';
 
-const AttendeesRegistration = ( props ) => {
-	const {
-		attendeeRegistrationLabel,
-		attendeeButtonLabel,
-		attendeeButtonClick,
-	} = props;
-
-	return (
-		<div className="tribe-editor__tickets-attendee">
-			<span>{ attendeeRegistrationLabel }</span>
-			<Button className="tribe-editor__btn--label" onClick={ attendeeButtonClick }>
-				{ attendeeButtonLabel }
-			</Button>
-		</div>
-	);
-};
+/* eslint-disable-next-line max-len */
+const AttendeesRegistration = ( { attendeeRegistrationLabel, attendeeButtonLabel, closeButtonLabel } ) => (
+	<LabelWithModal
+		className="tribe-editor__ticket__attendee-registration"
+		closeButtonLabel={ closeButtonLabel }
+		label={ attendeeRegistrationLabel }
+		modalButtonLabel={ attendeeButtonLabel }
+	/>
+);
 
 AttendeesRegistration.propTypes = {
 	attendeeRegistrationLabel: PropTypes.string,
 	attendeeButtonLabel: PropTypes.string,
+	closeButtonLabel: PropTypes.string,
 	attendeeButtonClick: PropTypes.func,
 };
 
 AttendeesRegistration.defaultProps = {
 	attendeeRegistrationLabel: __( 'Attendee Registration', 'events-gutenberg' ),
 	attendeeButtonLabel: __( '+Add', 'events-gutenberg' ),
+	closeButtonLabel: __( 'close', 'events-gutenberg' ),
 	attendeeButtonClick: noop,
 };
 
