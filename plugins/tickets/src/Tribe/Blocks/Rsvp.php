63b029918851f8f7f97d87f391e30334fc10f880
Juanfra Aldasoro <juan@nicethemes.com>
2018-09-24 17:50:47 -0300

    Add `@todo` for future compatibility changes

--- plugins/tickets/src/Tribe/Blocks/Rsvp.php
+++ plugins/tickets/src/Tribe/Blocks/Rsvp.php
@@ -53,6 +53,7 @@ extends Tribe__Gutenberg__Common__Blocks__Abstract {
 	 * @return string
 	 */
 	public function render( $attributes = array() ) {
+		// @TODO: Be sure we get the post ID from tickets so it can run without TEC
 		$post_id            = tribe( 'gutenberg.events.template' )->get( 'post_id' );
 		$args['attributes'] = $this->attributes( $attributes );
 		$args['tickets']    = $this->get_tickets( $post_id );
