ab776a6189b1579c8b8fef03dc093fa108ab500f
mitogh <mitogh@gmail.com>
2018-09-24 15:37:12 -0500

    Update location of the tickets creation form

--- plugins/tickets/src/modules/blocks/ticket/edit-container/template.js
+++ plugins/tickets/src/modules/blocks/ticket/edit-container/template.js
@@ -4,22 +4,17 @@
 import React, { Fragment } from 'react';
 
 /**
- * WordPress dependencies
- */
-import { __ } from '@wordpress/i18n';
-
-/**
  * Internal dependencies
  */
 import { ContainerPanel } from '@moderntribe/tickets/elements';
 import { LAYOUT } from '@moderntribe/tickets/elements/container-panel/element';
-import StatusIcons from '../../ticket/status-icon/element';
+import StatusIcons from '@moderntribe/tickets/blocks/ticket/status-icon/element';
 import TicketHeader from './header/template';
 import TicketContent from './content/template';
 
 const TicketEditContainer = () => (
 	<ContainerPanel
-		className="tribe-editor__ticket-container"
+		className="tribe-editor__edit-ticket-container"
 		layout={ LAYOUT.ticket }
 		icon={ <StatusIcons expires={ true } /> }
 		header={ <TicketHeader /> }
