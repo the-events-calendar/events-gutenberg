861ba56b4c9710bf0e71afbcf821243ac6cbd62e
mitogh <mitogh@gmail.com>
2018-09-25 05:53:43 -0500

    Update UI ticket reducer to keep track of selection

--- plugins/tickets/src/modules/data/blocks/ticket/reducers/ui.js
+++ plugins/tickets/src/modules/data/blocks/ticket/reducers/ui.js
@@ -1,12 +1,14 @@
 /**
  * Internal dependencies
  */
-import { types } from '@moderntribe/tickets/data/blocks/ticket';
+import * as types from './../types';
 
 export const DEFAULT_STATE = {
 	sharedCapacity: '',
 	header: null,
 	isSettingsOpen: false,
+	isParentBlockSelected: false,
+	isChildBlockSelected: false,
 };
 
 export default ( state = DEFAULT_STATE, action ) => {
@@ -16,7 +18,7 @@ export default ( state = DEFAULT_STATE, action ) => {
 				...state,
 				header: action.payload.header,
 			};
-		case types.SET_TICKET_SHARED_CAPACITY:
+		case types.SET_TICKET_TOTAL_SHARED_CAPACITY:
 			return {
 				...state,
 				sharedCapacity: action.payload.sharedCapacity,
@@ -25,6 +27,16 @@ export default ( state = DEFAULT_STATE, action ) => {
 			return {
 				...state,
 				isSettingsOpen: action.payload.isSettingsOpen,
+			};
+		case types.SET_PARENT_BLOCK_SELECTED:
+			return {
+				...state,
+				isParentBlockSelected: action.payload.selected,
+			};
+		case types.SET_CHILD_BLOCK_SELECTED:
+			return {
+				...state,
+				isChildBlockSelected: action.payload.selected,
 			};
 		default:
 			return state;
