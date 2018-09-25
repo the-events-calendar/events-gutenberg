dd5a3c633f4db1c2ee9079aae50fbe6cafb46d2d
mitogh <mitogh@gmail.com>
2018-09-25 05:50:45 -0500

    use life cycle events to register a block

--- plugins/tickets/src/modules/blocks/ticket/edit-container/template.js
+++ plugins/tickets/src/modules/blocks/ticket/edit-container/template.js
@@ -1,25 +1,49 @@
 /**
  * External dependencies
  */
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import noop from 'lodash/noop';
 
 /**
  * Internal dependencies
  */
 import { ContainerPanel } from '@moderntribe/tickets/elements';
 import { LAYOUT } from '@moderntribe/tickets/elements/container-panel/element';
-import StatusIcons from '@moderntribe/tickets/blocks/ticket/status-icon/element';
-import TicketHeader from './header/template';
+import StatusIcons from '@moderntribe/tickets/blocks/ticket/display-container/status-icon/element';
+import TicketHeader from './header/container';
 import TicketContent from './content/template';
 
-const TicketEditContainer = () => (
-	<ContainerPanel
-		className="tribe-editor__edit-ticket-container"
-		layout={ LAYOUT.ticket }
-		icon={ <StatusIcons expires={ true } /> }
-		header={ <TicketHeader /> }
-		content={ <TicketContent /> }
-	/>
-);
+class TicketEditContainer extends PureComponent {
+	static propTypes = {
+		blockId: PropTypes.string.isRequired,
+		register: PropTypes.func.isRequired,
+		unregister: PropTypes.func.isRequired,
+	};
+
+	componentDidMount() {
+		const { register } = this.props;
+		register();
+	}
+
+	componentWillUnmount() {
+		const { unregister } = this.props;
+		unregister();
+	}
+
+	render() {
+		const { blockId } = this.props;
+
+		return (
+			<ContainerPanel
+				className="tribe-editor__edit-ticket-container"
+				layout={ LAYOUT.ticket }
+				icon={ <StatusIcons expires={ true } /> }
+				header={ <TicketHeader blockId={ blockId } /> }
+				content={ <TicketContent blockId={ blockId } /> }
+			/>
+		);
+	}
+}
 
 export default TicketEditContainer;
