d8c3ad48ddc123a9fd2bc5f21ba0e9c360caa746
Paul <kim.paul.ms@gmail.com>
2018-09-27 02:50:51 +0800

    add keys for post meta

--- plugins/tickets/src/modules/data/__tests__/utils.test.js
+++ plugins/tickets/src/modules/data/__tests__/utils.test.js
@@ -1,10 +1,44 @@
 /**
  * Internal dependencies
  */
-import { PREFIX_TICKETS_STORE } from '@moderntribe/tickets/data/utils';
+import * as utils from '@moderntribe/tickets/data/utils';
 
 describe( 'Tickets prefix', () => {
 	it( 'should follow prefix convention', () => {
-		expect( PREFIX_TICKETS_STORE ).toBe( '@@MT/TICKETS' );
+		expect( utils.PREFIX_TICKETS_STORE ).toBe( '@@MT/TICKETS' );
+	} );
+} );
+
+describe( 'Tickets keys', () => {
+	test( 'rsvp for event', () => {
+		expect( utils.KEY_RSVP_FOR_EVENT ).toBe( '_tribe_rsvp_for_event' );
+	} );
+
+	test( 'ticket show description', () => {
+		expect( utils.KEY_TICKET_SHOW_DESCRIPTION ).toBe( '_tribe_ticket_show_description' );
+	} );
+
+	test( 'price', () => {
+		expect( utils.KEY_PRICE ).toBe( '_price' );
+	} );
+
+	test( 'ticket capacity', () => {
+		expect( utils.KEY_TICKET_CAPACITY ).toBe( '_tribe_ticket_capacity' );
+	} );
+
+	test( 'ticket start date', () => {
+		expect( utils.KEY_TICKET_START_DATE ).toBe( '_ticket_start_date' );
+	} );
+
+	test( 'ticket end date', () => {
+		expect( utils.KEY_TICKET_END_DATE ).toBe( '_ticket_end_date' );
+	} );
+
+	test( 'ticket show not going', () => {
+		expect( utils.KEY_TICKET_SHOW_NOT_GOING ).toBe( '_tribe_ticket_show_not_going' );
+	} );
+
+	test( 'ticket header', () => {
+		expect( utils.KEY_TICKET_HEADER ).toBe( '_tribe_ticket_header' );
 	} );
 } );
