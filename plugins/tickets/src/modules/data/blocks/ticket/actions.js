3678a5abd1ba1488c7c8583ff0462e290c989e4f
mitogh <mitogh@gmail.com>
2018-09-25 21:37:26 -0500

    Update actions to set values into the reducers for child tickets blocks

--- plugins/tickets/src/modules/data/blocks/ticket/actions.js
+++ plugins/tickets/src/modules/data/blocks/ticket/actions.js
@@ -10,8 +10,8 @@ export const setHeader = ( header ) => ( {
 	},
 } );
 
-export const setSharedCapacity = ( sharedCapacity ) => ( {
-	type: types.SET_TICKET_SHARED_CAPACITY,
+export const setTotalSharedCapacity = ( sharedCapacity ) => ( {
+	type: types.SET_TICKET_TOTAL_SHARED_CAPACITY,
 	payload: {
 		sharedCapacity,
 	},
@@ -26,3 +26,149 @@ export const setSettingsOpen = ( isSettingsOpen ) => ( {
 
 export const openSettings = () => setSettingsOpen( true );
 export const closeSettings = () => setSettingsOpen( false );
+
+export const setParentBlockSelected = ( selected ) => ( {
+	type: types.SET_PARENT_BLOCK_SELECTED,
+	payload: {
+		selected,
+	},
+} );
+
+export const setChildBlockSelected = ( selected ) => ( {
+	type: types.SET_CHILD_BLOCK_SELECTED,
+	payload: {
+		selected,
+	},
+} );
+
+export const setActiveChildBlockId = ( activeChildBlockId ) => ( {
+	type: types.SET_ACTIVE_CHILD_BLOCK_ID,
+	payload: {
+		activeChildBlockId,
+	},
+} );
+
+// individual ticket actions
+export const registerTicketBlock = ( blockId ) => ( {
+	type: types.SET_TICKET_BLOCK_ID,
+	payload: {
+		blockId,
+	},
+} );
+
+export const removeTicketBlock = ( blockId ) => ( {
+	type: types.REMOVE_TICKET_BLOCK,
+	payload: {
+		blockId,
+	},
+} );
+
+export const setTitle = ( blockId, title ) => ( {
+	type: types.SET_TICKET_TITLE,
+	payload: {
+		blockId,
+		title,
+	},
+} );
+
+export const setDescription = ( blockId, description ) => ( {
+	type: types.SET_TICKET_DESCRIPTION,
+	payload: {
+		blockId,
+		description,
+	},
+} );
+
+export const setPrice = ( blockId, price ) => ( {
+	type: types.SET_TICKET_PRICE,
+	payload: {
+		blockId,
+		price,
+	},
+} );
+
+export const setSKU = ( blockId, SKU ) => ( {
+	type: types.SET_TICKET_SKU,
+	payload: {
+		blockId,
+		SKU,
+	},
+} );
+
+export const setStartDate = ( blockId, startDate ) => ( {
+	type: types.SET_TICKET_START_DATE,
+	payload: {
+		blockId,
+		startDate,
+	},
+} );
+
+export const setStartTime = ( blockId, startTime ) => ( {
+	type: types.SET_TICKET_START_TIME,
+	payload: {
+		blockId,
+		startTime,
+	},
+} );
+
+export const setEndDate = ( blockId, endDate ) => ( {
+	type: types.SET_TICKET_END_DATE,
+	payload: {
+		blockId,
+		endDate,
+	},
+} );
+
+export const setEndTime = ( blockId, endTime ) => ( {
+	type: types.SET_TICKET_END_TIME,
+	payload: {
+		blockId,
+		endTime,
+	},
+} );
+
+export const setCapacityType = ( blockId, capacityType ) => ( {
+	type: types.SET_TICKET_CAPACITY_TYPE,
+	payload: {
+		blockId,
+		capacityType,
+	},
+} );
+
+export const setCapacity = ( blockId, capacity ) => ( {
+	type: types.SET_TICKET_CAPACITY,
+	payload: {
+		blockId,
+		capacity,
+	},
+} );
+
+export const createNewTicket = ( blockId ) => ( {
+	type: types.SET_CREATE_NEW_TICKET,
+	payload: {
+		blockId,
+	},
+} );
+
+export const updateTicket = ( blockId ) => ( {
+	type: types.SET_UPDATE_TICKET,
+	payload: {
+		blockId,
+	},
+} );
+
+export const setTicketIsEditing = ( blockId, isEditing ) => ( {
+	type: types.SET_TICKET_IS_EDITING,
+	payload: {
+		blockId,
+		isEditing,
+	},
+} );
+
+export const setTicketPostId = ( blockId, postId ) => ( {
+	type: types.SET_TICKET_POST_ID,
+	payload: {
+		blockId,
+		postId,
+	},
+} );
