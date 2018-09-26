2bf9bfdefe96e8fd7e9d7492acfd1fe0e6489935
mitogh <mitogh@gmail.com>
2018-09-25 21:43:38 -0500

    Removal of non used props for TicketImage

--- plugins/tickets/src/modules/blocks/tickets/settings/template.js
+++ plugins/tickets/src/modules/blocks/tickets/settings/template.js
@@ -14,8 +14,8 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { SettingsDashboard } from '@moderntribe/tickets/elements';
-import { ImageUpload } from '@moderntribe/common/elements';
 import CapacityTable from '@moderntribe/tickets/blocks/tickets/capacity-table/container';
+import TicketImage from './../header-image/container';
 
 const TicketsSettingsDashboard = ( { onCloseClick, content } ) => (
 	<SettingsDashboard
@@ -30,22 +30,12 @@ TicketsSettingsDashboard.propTypes = {
 	content: PropTypes.node,
 };
 
-const imageUploadProps = {
-	title: __( 'Ticket Header Image', 'events-gutenberg' ),
-	description: __(
-		/* eslint-disable-next-line max-len */
-		'Select an image from your Media Library to display on emailed tickets. For best results, use a .jpg, .png, or .gif at least 1160px wide.',
-		'events-gutenberg'
-	),
-	buttonLabel: __( 'Upload Image', 'events-gutenberg' ),
-};
-
 TicketsSettingsDashboard.defaultProps = {
 	onCloseClick: noop,
 	content: (
 		<Fragment>
 			<CapacityTable />
-			<ImageUpload { ...imageUploadProps } />
+			<TicketImage />
 		</Fragment>
 	),
 }
