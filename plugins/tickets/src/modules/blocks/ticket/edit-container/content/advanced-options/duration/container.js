ffbe3338a1139a10b96aa9e3c957da16a8cf79a9
mitogh <mitogh@gmail.com>
2018-09-26 11:18:17 -0500

    Update the duration of the dates to set real values

--- plugins/tickets/src/modules/blocks/ticket/edit-container/content/advanced-options/duration/container.js
+++ plugins/tickets/src/modules/blocks/ticket/edit-container/content/advanced-options/duration/container.js
@@ -3,6 +3,7 @@
  */
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import moment from 'moment';
 
 /**
  * Internal dependencies
@@ -10,35 +11,47 @@ import { compose } from 'redux';
 import Template from './template';
 import { withStore } from '@moderntribe/common/src/modules/hoc';
 import { selectors, actions } from '@moderntribe/tickets/data/blocks/ticket';
+import {
+	moment as momentUtil,
+	time as timeUtil,
+} from '@moderntribe/common/utils';
 
-const mapStateToProps = ( state, ownProps ) => ( {} );
+const mapStateToProps = ( state, ownProps ) => ( {
+	fromDate: selectors.getTicketStartDate( state, ownProps ),
+	fromTime: selectors.getTicketStartTime( state, ownProps ),
+	isSameDay: false,
+	toDate: selectors.getTicketEndDate( state, ownProps ),
+	toTime: selectors.getTicketEndTime( state, ownProps ),
+} );
 
 const mapDispatchToProps = ( dispatch, ownProps ) => ( {
 	onFromDateChange: ( date ) => {
 		const { blockId } = ownProps;
-		dispatch( actions.setStartDate( blockId, date ) );
+		dispatch( actions.setStartDate( blockId, momentUtil.toDate( moment( date ) ) ) );
 	},
 	onFromTimePickerChange: ( value ) => {
 		const { blockId } = ownProps;
 		dispatch( actions.setStartTime( blockId, value ) );
 	},
-	onFromTimePickerClick: ( value ) => {
-		/**
-		 * @todo convert seconds into timestamp
-		 */
+	onFromTimePickerClick: ( value, onClose ) => {
+		const { blockId } = ownProps;
+		const endTime = timeUtil.fromSeconds( value, timeUtil.TIME_FORMAT_HH_MM );
+		dispatch( actions.setStartTime( blockId, endTime ) );
+		onClose();
 	},
 	onToDateChange: ( date ) => {
 		const { blockId } = ownProps;
-		dispatch( actions.setEndTime( blockId, date ) );
+		dispatch( actions.setEndDate( blockId, momentUtil.toDate( moment( date ) ) ) );
 	},
 	onToTimePickerChange: ( value ) => {
 		const { blockId } = ownProps;
-		dispatch( actions.setStartTime( blockId, value ) );
+		dispatch( actions.setEndTime( blockId, value ) );
 	},
-	onToTimePickerClick: ( value ) => {
-		/**
-		 * @todo convert seconds into timestamp
-		 */
+	onToTimePickerClick: ( value, onClose ) => {
+		const { blockId } = ownProps;
+		const endTime = timeUtil.fromSeconds( value, timeUtil.TIME_FORMAT_HH_MM );
+		dispatch( actions.setEndTime( blockId, endTime ) );
+		onClose();
 	},
 } );
 
