9aa46a90e21aff90fb53a473ceb804e6e50dc5ee
mitogh <mitogh@gmail.com>
2018-09-25 21:35:27 -0500

    Set parent properties into the TicketsActionDashboard

--- plugins/tickets/src/modules/blocks/tickets/dashboard/template.js
+++ plugins/tickets/src/modules/blocks/tickets/dashboard/template.js
@@ -7,16 +7,30 @@ import PropTypes from 'prop-types';
 /**
  * Internal dependencies
  */
-import TicketsActionDashboard from '@moderntribe/tickets/blocks/tickets/action-dashboard/template';
+import TicketsActionDashboard from '@moderntribe/tickets/blocks/tickets/action-dashboard/container';
 import TicketsSettingsDashboard from '@moderntribe/tickets/blocks/tickets/settings/container';
 
-const TicketsDashboard = ( { isSelected, isSettingsOpen } ) => ( isSelected && (
-	isSettingsOpen ? <TicketsSettingsDashboard /> : <TicketsActionDashboard />
+const TicketsDashboard = ( { isSelected, isSettingsOpen, activeBlockId, isEditing, clientId } ) => ( isSelected && (
+	isSettingsOpen
+		? <TicketsSettingsDashboard />
+		: (
+			<TicketsActionDashboard
+				activeBlockId={ activeBlockId }
+				isEditing={ isEditing }
+				clientId={ clientId }
+			/>
+		)
 ) );
 
 TicketsDashboard.propTypes = {
 	isSelected: PropTypes.bool.isRequired,
+	isEditing: PropTypes.bool,
 	isSettingsOpen: PropTypes.bool.isRequired,
 };
+
+TicketsDashboard.defaultProps = {
+	isSelected: false,
+	isEditing: false,
+}
 
 export default TicketsDashboard;
