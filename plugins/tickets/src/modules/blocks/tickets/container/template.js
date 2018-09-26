d472b1f3df92e5c637d139ba0f09e9bedfa59f00
mitogh <mitogh@gmail.com>
2018-09-25 21:45:18 -0500

    Render InactiveBlock when there are no tickets available

--- plugins/tickets/src/modules/blocks/tickets/container/template.js
+++ plugins/tickets/src/modules/blocks/tickets/container/template.js
@@ -8,23 +8,37 @@ import PropTypes from 'prop-types';
  * Wordpress dependencies
  */
 import { InnerBlocks } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import Availability from '../availability/element';
+import { InactiveBlock } from '@moderntribe/tickets/elements';
+import { LAYOUT } from '@moderntribe/tickets/elements/inactive-block/element';
+import StatusIcon from '@moderntribe/tickets/blocks/ticket/display-container/status-icon/element';
 
-const TicketContainer = ( { isSelected } ) => (
+const TicketContainer = ( { isSelected, isEditing, total, sold, tickets } ) => (
 	<div className="tribe-editor__ticket-container">
 		<div className="tribe-editor__tickets-body">
-			<InnerBlocks />
+			<InnerBlocks allowedBlocks={ [ 'tribe/event-tickets-ticket' ] } />
 		</div>
-		{ isSelected && <Availability available={ 48 } total={ 168 } /> }
+		{ tickets.length === 0 && (
+			<InactiveBlock
+				layout={ LAYOUT.ticket }
+				title={ __( 'No Active Tickets', 'events-gutenberg' ) }
+				icon={ <StatusIcon disabled={ true } expires={ true } /> }
+			/>
+		) }
+		{ isSelected && ! isEditing && <Availability available={ total - sold } total={ total } /> }
 	</div>
 );
 
 TicketContainer.propTypes = {
 	isSelected: PropTypes.bool,
+	isEditing: PropTypes.bool,
+	total: PropTypes.number,
+	sold: PropTypes.number,
 };
 
 export default TicketContainer;
