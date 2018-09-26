f9468d58b8268d4f2c5c7bdd8a0d8fb20a6828ee
mitogh <mitogh@gmail.com>
2018-09-25 21:29:10 -0500

    Propagate the blockId properties into the edit container

--- plugins/tickets/src/modules/blocks/ticket/edit-container/content/template.js
+++ plugins/tickets/src/modules/blocks/ticket/edit-container/content/template.js
@@ -2,22 +2,27 @@
  * External dependencies
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
  */
-import Capacity from './capacity/template';
+import Capacity from './capacity/container';
 import AdvancedOptions from './advanced-options/template';
 import AttendeesRegistration from './attendees-registration/template';
 
 import './style.pcss';
 
-const TicketEditContent = () => (
+const TicketEditContent = ( { blockId } ) => (
 	<div className="tribe-editor__ticket-container__content">
-		<Capacity />
-		<AdvancedOptions />
-		<AttendeesRegistration />
+		<Capacity blockId={ blockId } />
+		<AdvancedOptions blockId={ blockId } />
+		<AttendeesRegistration blockId={ blockId } />
 	</div>
 );
+
+TicketEditContent.propTypes = {
+	blockId: PropTypes.string.isRequired,
+};
 
 export default TicketEditContent;
