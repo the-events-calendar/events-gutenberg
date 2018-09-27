818f3e387317f7faa196cdabb61da46e96a107b5
mitogh <mitogh@gmail.com>
2018-09-27 16:11:32 -0500

    Add properties in order to set the values into the attributes
    Save the child block inner content as well

--- plugins/tickets/src/modules/blocks/tickets/container.js
+++ plugins/tickets/src/modules/blocks/tickets/container.js
@@ -12,16 +12,23 @@ import Template from './template';
 import { withSaveData, withStore } from '@moderntribe/common/src/modules/hoc';
 import { selectors, actions } from '@moderntribe/tickets/data/blocks/ticket';
 
+// Header property is casted into a string as endpoint accepts a string as a value
 const mapStateToProps = ( state, ownProps ) => ( {
 	isSelected: ownProps.isSelected,
 	isBlockSelected: selectors.getParentOrChildSelected( state ),
 	isEditing: selectors.hasActiveBlockId( state ),
 	clientId: ownProps.clientId,
+	header: `${ selectors.getImageId( state ) }`,
+	sharedCapacity: selectors.getSharedCapacity( state ),
+	isLoading: selectors.isParentBlockLoading( state ),
 } );
 
 const mapDispatchToProps = ( dispatch, ownProps ) => ( {
 	setIsSelected( selected ) {
 		dispatch( actions.setParentBlockSelected( selected ) );
+	},
+	setInitialState: ( props ) => {
+		dispatch( actions.setInitialState( props ) );
 	},
 } );
 
