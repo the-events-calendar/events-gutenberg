e647830dc6a9c71a307fde1d65fbff75d6f4ef59
Juanfra Aldasoro <juan@nicethemes.com>
2018-09-27 17:43:21 -0300

    Format `@todo`

--- plugins/tickets/src/Tribe/Blocks/Tickets.php
+++ plugins/tickets/src/Tribe/Blocks/Tickets.php
@@ -60,7 +60,9 @@ extends Tribe__Gutenberg__Common__Blocks__Abstract {
 	 * @return string
 	 */
 	public function render( $attributes = array() ) {
-		// @todo: We'll need to get the post id from ET later, so support it as standalone
+		/**
+		 * @todo: We'll need to get the post id from ET later, so support it as standalone
+		 */
 		$post_id            = tribe( 'gutenberg.events.template' )->get( 'post_id' );
 		$args['attributes'] = $this->attributes( $attributes );
 		$args['tickets']    = $this->get_tickets( $post_id );
