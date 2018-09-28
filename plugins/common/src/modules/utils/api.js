a2acf276f1c2716e92b990bd9d3dd05c2f72ca59
mitogh <mitogh@gmail.com>
2018-09-28 11:24:02 -0500

    Add utils to serialize an object value

--- plugins/common/src/modules/utils/api.js
+++ plugins/common/src/modules/utils/api.js
@@ -1,4 +1,6 @@
 import { config } from '@moderntribe/common/utils/globals';
+import isPlainObject from 'lodash/isPlainObject';
+import flattenDeep from 'lodash/flattenDeep';
 
 /**
  * Returns a URL that is part of the WP-JSON endpoint associated with the site where the
@@ -15,4 +17,40 @@ export const endpointUrl = ( path = '', namespace = 'wp/v2' ) => {
 	const namespaces = rest.namespaces || {};
 	const core = namespaces.core || namespace;
 	return `${ url }${ core }${ path }`;
+};
+
+export const adminAjax = () => {
+	const tribe = config();
+	const adminUrl = tribe.admin_url || '';
+	return `${ adminUrl }admin-ajax.php`;
+};
+
+/**
+ * Encode the components from an object into an array
+ *
+ * @param {Object} obj The object to serialize
+ * @returns {Array} An array with string on it
+ */
+export const encode = ( obj = {} ) => {
+	return flattenDeep( Object
+		.keys( obj )
+		.map( ( key ) => {
+			const item = obj[ key ];
+			if ( isPlainObject( item ) ) {
+				const children = encode( item ).join();
+				const name = decodeURIComponent( `${ key }=[]` );
+				return `${ name }=${ children }`;
+			}
+			return `${ key }=${ encodeURIComponent( item ) }`;
+		} ) );
+};
+
+/**
+ * Serialize an object into a string that is encoded to be sent into a request
+ *
+ * @param {Object} obj Object to be serializable
+ * @returns {string} A string with the serializable value
+ */
+export const serialize = ( obj = {} ) => {
+	return encode( obj ).join( '&' );
 };
