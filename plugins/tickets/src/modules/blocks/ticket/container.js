2b7f35912815567c767aae153640d7aa18908e93
mitogh <mitogh@gmail.com>
2018-09-28 11:25:15 -0500

    Pass props into the ticket container

--- plugins/tickets/src/modules/blocks/ticket/container.js
+++ plugins/tickets/src/modules/blocks/ticket/container.js
@@ -3,6 +3,7 @@
  */
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import get from 'lodash/get';
 
 /**
  * Internal dependencies
@@ -11,13 +12,23 @@ import Template from './template';
 
 import { withSaveData, withStore } from '@moderntribe/common/src/modules/hoc';
 import { actions, selectors } from '@moderntribe/tickets/data/blocks/ticket';
+import { config } from '@moderntribe/common/src/modules/utils/globals';
 
-const mapStateToProps = ( state, ownProps ) => ( {
-	isBlockSelected: selectors.getParentOrChildSelected( state ),
-	isEditing: selectors.getTicketEditing( state, { blockId: ownProps.clientId } ),
-	blockId: ownProps.clientId,
-	isSelected: ownProps.isSelected,
-} );
+const mapStateToProps = ( state, ownProps ) => {
+	const props = { blockId: ownProps.clientId };
+	return {
+		isBlockSelected: selectors.getParentOrChildSelected( state ),
+		isEditing: selectors.getTicketEditing( state, props ),
+		blockId: ownProps.clientId,
+		isSelected: ownProps.isSelected,
+		hasBeenCreated: false,
+		title: selectors.getTicketTitle( state, props ),
+		description: selectors.getTicketDescription( state, props ),
+		price: selectors.getTicketPrice( state, props ),
+		SKU: selectors.getTicketSKU( state, props ),
+		dateIsPristine: ! selectors.getTicketExpires( state, props ),
+	};
+};
 
 const mapDispatchToProps = ( dispatch, ownProps ) => ( {
 	setIsSelected( selected ) {
@@ -31,6 +42,8 @@ const mapDispatchToProps = ( dispatch, ownProps ) => ( {
 		const { clientId } = ownProps;
 		dispatch( actions.removeTicketBlock( clientId ) );
 	},
+	setInitialState( props ) {
+	}
 } );
 
 export default compose(
