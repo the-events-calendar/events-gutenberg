8db91f510a6f609af2d9fdcc049ec97b7747b885
mitogh <mitogh@gmail.com>
2018-09-28 22:59:48 -0500

    Removal of props on action buttons

--- plugins/tickets/src/modules/blocks/tickets/action-buttons/orders/template.js
+++ plugins/tickets/src/modules/blocks/tickets/action-buttons/orders/template.js
@@ -15,24 +15,19 @@ import { __ } from '@wordpress/i18n';
  */
 import { ActionButton } from '@moderntribe/tickets/elements';
 import { Tag as TagIcon } from '@moderntribe/common/src/modules/icons';
-import SettingsActionButton from '../settings/template';
 
-const OrdersActionButton = ( { label, onClick, icon } ) => (
-	<ActionButton icon={ icon } onClick={ onClick }>
-		{ label }
+const OrdersActionButton = ( { onClick } ) => (
+	<ActionButton icon={ <TagIcon /> } onClick={ onClick }>
+		{ __( 'Orders', 'events-gutenberg' ) }
 	</ActionButton>
 );
 
 OrdersActionButton.propTypes = {
 	onClick: PropTypes.func,
-	label: PropTypes.string,
-	icon: PropTypes.node,
 };
 
 OrdersActionButton.defaultProps = {
 	onClick: noop,
-	label: __( 'Orders', 'events-gutenberg' ),
-	icon: <TagIcon />,
 };
 
 export default OrdersActionButton;
