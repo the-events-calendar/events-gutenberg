982a50ad33bfe1e5f5a5382ba7a4c8247e15442c
mitogh <mitogh@gmail.com>
2018-09-30 13:55:50 -0500

    Removal of non used code

--- plugins/common/src/modules/utils/__tests__/api.test.js
+++ plugins/common/src/modules/utils/__tests__/api.test.js
@@ -1,9 +1,7 @@
 import * as api from '../api';
 
-
 jest.mock( '@moderntribe/common/utils/globals', () => ( {
 	config: () => ( {
-		admin_url: 'https://localhost/wp-admin/',
 		rest: {
 			url: 'https://localhost/wp-json/',
 		},
@@ -24,48 +22,6 @@ describe( 'api', () => {
 		test( 'Custom namespace', () => {
 			expect( api.endpointUrl( '/events/10', 'tribe/v1' ) )
 				.toBe( 'https://localhost/wp-json/tribe/v1/events/10' );
-		} );
-	} );
-
-	describe( 'adminAjax', () => {
-		test( 'Ajax url', () => {
-			expect( api.adminAjax() ).toBe( 'https://localhost/wp-admin/admin-ajax.php' );
-		} );
-	} );
-
-	describe( 'encode', () => {
-		test( 'empty object', () => {
-			expect( api.encode() ).toEqual( [] );
-		} );
-
-		test( 'regular object', () => {
-			const input = {
-				name: 'Modern Tribe',
-				rol: 'agency',
-			}
-			expect( api.encode( input ) )
-				.toEqual( [ 'name=Modern%20Tribe', 'rol=agency' ] );
-		} );
-
-		test( 'nested object', () => {
-			const input = {
-				name: 'Modern Tribe',
-				rol: 'agency',
-				teams: {
-					products: {
-						members: 10,
-					},
-					support: {
-						members: 20,
-					},
-					agency: {
-						members: 30,
-					},
-				},
-			}
-			expect( api.encode( input ) )
-				.toEqual( [ 'name=Modern%20Tribe', 'rol=agency' ] );
-			expect( input );
 		} );
 	} );
 } );
