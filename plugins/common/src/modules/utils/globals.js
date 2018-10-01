5fa3516867ccdf067ae5543cd53a78ade9a0c698
mitogh <mitogh@gmail.com>
2018-09-30 19:49:37 -0500

    Add utils on globals methods and add tests for it

--- plugins/common/src/modules/utils/globals.js
+++ plugins/common/src/modules/utils/globals.js
@@ -3,6 +3,8 @@ export const google = () => get( 'google' );
 export const mapsAPI = () => get( 'tribe_blocks_editor_google_maps_api' );
 export const settings = () => get( 'tribe_blocks_editor_settings' );
 export const config = () => get( 'tribe_js_config', {} );
+export const rest = () => config().rest || {};
+export const restNonce = () => rest().nonce || {};
 export const list = () => ( {
 	countries: get( 'tribe_data_countries' ),
 	us_states: get( 'tribe_data_us_states' ),
