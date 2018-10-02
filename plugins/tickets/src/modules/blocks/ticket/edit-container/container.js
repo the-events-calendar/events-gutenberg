1cfa629f047031d2b43a1eb6feade4c5705e041a
mitogh <mitogh@gmail.com>
2018-10-02 17:50:41 -0500

    Select disabled tickets from the props

--- plugins/tickets/src/modules/blocks/ticket/edit-container/container.js
+++ plugins/tickets/src/modules/blocks/ticket/edit-container/container.js
@@ -14,6 +14,7 @@ import { selectors } from '@moderntribe/tickets/data/blocks/ticket';
 
 const mapStateToProps = ( state, ownProps ) => ( {
 	expires: selectors.getTicketExpires( state, ownProps ),
+	isTicketDisabled: selectors.isTicketDisabled( state, ownProps ),
 } );
 
 export default compose(
