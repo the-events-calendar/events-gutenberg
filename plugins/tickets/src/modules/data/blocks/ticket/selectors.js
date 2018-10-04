0cefb9187704eddb0692926bbc0b2ffffaa22033
mitogh <mitogh@gmail.com>
2018-10-03 22:39:08 -0500

    Remove remove the need to call `NaN`

--- plugins/tickets/src/modules/data/blocks/ticket/selectors.js
+++ plugins/tickets/src/modules/data/blocks/ticket/selectors.js
@@ -139,20 +139,28 @@ export const getTicketsSharedCapacity = createSelector(
 	[ getSharedTickets ],
 	( tickets ) => {
 		return tickets.reduce( ( total, ticket ) => {
-			const capacity = parseInt( ticket.capacity, 10 );
-			return total + ( isNaN( capacity ) ? 0 : capacity );
+			const capacity = parseInt( ticket.capacity, 10 ) || 0;
+			return total + capacity;
 		}, 0 );
 	},
 );
 
 export const getTotalSold = createSelector(
-	[ getTicketsIds, getTicketsObject ],
-	( ids, tickets ) => {
-		return ids.reduce( ( total, id ) => {
-			const item = tickets[ id ];
-			const sold = parseInt( item.sold, 10 );
-			const value = isNaN( sold ) ? 0 : sold;
-			return total + value;
+	[ getTicketsArray ],
+	( tickets ) => {
+		return tickets.reduce( ( total, ticket ) => {
+			const sold = parseInt( ticket.sold, 10 ) || 0;
+			return total + sold;
+		}, 0 );
+	},
+);
+
+export const getTotalAvailable = createSelector(
+	[ getTicketsArray ],
+	( tickets ) => {
+		return tickets.reduce( ( total, ticket ) => {
+			const sold = parseInt( ticket.available, 10 ) || 0;
+			return total + sold;
 		}, 0 );
 	},
 );
