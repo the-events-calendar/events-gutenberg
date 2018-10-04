5b9230a1f8b43e560f5142f5b021a69b779fe28a
mitogh <mitogh@gmail.com>
2018-10-04 11:33:27 -0500

    Send `href` property into order component
    - render component only if `href` is provided

--- plugins/tickets/src/modules/blocks/tickets/action-buttons/orders/container.js
+++ plugins/tickets/src/modules/blocks/tickets/action-buttons/orders/container.js
@@ -14,19 +14,24 @@ import { select } from '@wordpress/data';
  * Internal dependencies
  */
 import AttendeesActionButton from './template';
-
+import { selectors } from '@moderntribe/tickets/data/blocks/ticket';
+import { TICKET_ORDERS_PAGE_SLUG } from '@moderntribe/tickets/data/utils';
 import { withStore } from '@moderntribe/common/hoc';
 
-const mapStateToProps = () => {
+const mapStateToProps = ( state ) => {
 	const adminURL = config().admin_url || '';
 	const postType = select( 'core/editor' ).getCurrentPostType();
 	const postId = select( 'core/editor' ).getCurrentPostId();
+	const provider = selectors.getSelectedProvider( state );
+	const page = TICKET_ORDERS_PAGE_SLUG[ provider ];
 
 	/**
 	 * @todo revise page= for other providers
 	 */
 	return {
-		href: `${ adminURL }edit.php?post_type=${ postType }&page=tpp-orders&event_id=${ postId }`,
+		href: page
+			? `${ adminURL }edit.php?post_type=${ postType }&page=${ page }&event_id=${ postId }`
+			: '',
 	};
 };
 
