98384261d57ceb922b761b662f1d14c44a094e8f
mitogh <mitogh@gmail.com>
2018-10-08 20:34:56 -0500

    Create container for the Human Readable input
    Connect template with the store using the container as HOC to connect with the store

---
+++ plugins/events/src/modules/blocks/event-datetime/human-readable-input/container.js
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+/**
+ * Internal dependencies
+ */
+import {
+	actions as dateTimeActions,
+	selectors as dateTimeSelectors,
+} from '@moderntribe/events/data/blocks/datetime';
+import { withStore, withSaveData } from '@moderntribe/common/hoc';
+import HumanReadableInput from './template';
+
+const mapStateToProps = ( state ) => ( {
+	naturalLanguageLabel: dateTimeSelectors.getNaturalLanguageLabel( state ),
+} );
+
+const mapDispatchToProps = ( dispatch ) => ( {
+	onChange: ( event ) => dispatch( dateTimeActions.setNaturalLanguageLabel( event.target.value ) ),
+} );
+
+export default compose(
+	withStore(),
+	connect( mapStateToProps, mapDispatchToProps ),
+	withSaveData(),
+)( HumanReadableInput );
