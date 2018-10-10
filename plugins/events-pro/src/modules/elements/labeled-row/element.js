e34b861cf11e92a390862e2824d059ce9ca2fd58
Paul <kim.paul.ms@gmail.com>
2018-10-11 01:29:47 +0800

    create labeled row component

---
+++ plugins/events-pro/src/modules/elements/labeled-row/element.js
@@ -0,0 +1,31 @@
+/**
+ * External Dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import Label from '@moderntribe/events-pro/elements/label/element';
+import Row from '@moderntribe/events-pro/elements/row/element';
+
+const LabeledRow = ( { children, className, label } ) => (
+	<Row className={ classNames( 'tribe-editor__labeled-row', className ) }>
+		<Label className="tribe-editor__labeled-row__label">
+			{ label }
+		</Label>
+		<div className="tribe-editor__labeled-row__content">
+			{ children }
+		</div>
+	</Row>
+);
+
+LabeledRow.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string,
+	label: PropTypes.node,
+};
+
+export default LabeledRow;
