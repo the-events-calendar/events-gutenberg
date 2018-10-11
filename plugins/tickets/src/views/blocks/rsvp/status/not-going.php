8769de197a0aa621b275bcaf2787cda0a196a5ff
mitogh <mitogh@gmail.com>
2018-10-10 19:14:04 -0500

    Update field to be a boolean and update views to render conditionally

--- plugins/tickets/src/views/blocks/rsvp/status/not-going.php
+++ plugins/tickets/src/views/blocks/rsvp/status/not-going.php
@@ -5,6 +5,20 @@
  * @version 0.3.0-alpha
  *
  */
+
+/**
+ * @todo: Create a hook for the get_ticket method in order to set dynamic or custom properties into
+ * the instance variable so we can set a new one called $ticket->show_not_going.
+ *
+ * For now we need to access directly the value of the meta field in order to render this field.
+ */
+$show_not_going = tribe_is_truthy(
+	get_post_meta( $ticket->ID, '_tribe_ticket_show_not_going', true )
+);
+
+if ( ! $show_not_going ) {
+    return;
+}
 ?>
 <span>
 	<button class="tribe-block__rsvp__status-button tribe-block__rsvp__status-button--not-going">
