9d37281f1b6e181831e83cbc7318bff9dbb6edad
Neal Fennimore <hi@neal.codes>
2018-10-14 18:59:35 -0400

    Update exceptions to edit and remove by index

--- plugins/events-pro/src/modules/elements/exception-field/element.js
+++ plugins/events-pro/src/modules/elements/exception-field/element.js
@@ -17,10 +17,11 @@ export default class ExceptionField extends PureComponent {
 	static propTypes = {
 		onRemoveClick: PropTypes.func.isRequired,
 		id: PropTypes.string.isRequired,
+		index: PropTypes.number.isRequired,
 		type: PropTypes.string.isRequired,
 	}
 
-	handleRemove = () => this.props.onRemoveClick( this.props.id )
+	handleRemove = () => this.props.onRemoveClick( this.props.index )
 
 	renderFieldType = () => {
 		switch ( this.props.type ) {
