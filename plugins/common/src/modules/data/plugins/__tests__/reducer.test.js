865a7b5880aef5fc3d6ea171373e76e97bb98d14
mitogh <mitogh@gmail.com>
2018-10-15 16:33:41 -0500

    Revert plugin reducer changes

--- plugins/common/src/modules/data/plugins/__tests__/reducer.test.js
+++ plugins/common/src/modules/data/plugins/__tests__/reducer.test.js
@@ -5,16 +5,21 @@ import reducer, { actions } from '@moderntribe/common/data/plugins';
 
 describe( 'Plugins reducer', () => {
 	it( 'Should return the default state', () => {
-		expect( reducer( undefined, {} ) ).toEqual( {} );
+		expect( reducer( undefined, {} ) ).toEqual( [] );
 	} );
 
 	it( 'Should add a new plugin as active', () => {
-		expect( reducer( {}, actions.addPlugin( 'events' ) ) ).toEqual( { events: true } );
+		expect( reducer( {}, actions.addPlugin( 'events' ) ) ).toEqual( [ 'events' ] );
+	} );
+
+	it( 'Should avoid adding duplicates entries', () => {
+		const state = reducer( {}, actions.addPlugin( 'events' ) );
+		expect( reducer( state, actions.addPlugin( 'events' ) ) ).toEqual( [ 'events' ] );
 	} );
 
 	it( 'Should remove the plugin from the reducer', () => {
 		const state = reducer( {}, actions.addPlugin( 'events' ) );
-		expect( state ).toEqual( { events: true } );
-		expect( reducer( state, actions.removePlugin( 'events' ) ) ).toEqual( { events: false } );
+		expect( state ).toEqual( [ 'events' ] );
+		expect( reducer( state, actions.removePlugin( 'events' ) ) ).toEqual( [] );
 	} );
 } );
