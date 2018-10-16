c6a4254eda6a0d566fe273a143d283c141efa2cd
mitogh <mitogh@gmail.com>
2018-10-16 12:20:41 -0500

    Add placeholder element for the Additional Fields

---
+++ plugins/events-pro/src/modules/blocks/additional-fields/elements/placeholder/__tests__/element.test.js
@@ -0,0 +1,17 @@
+import Placeholder from '../element';
+
+describe( '<Placeholder> component', () => {
+	test( 'Default behavior', () => {
+		const component = renderer.create(
+			<Placeholder>Custom Text</Placeholder>
+		);
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	test( 'Custom Class name attached', () => {
+		const component = renderer.create(
+			<Placeholder className='custom-class-name'>Custom Text</Placeholder>
+		);
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+} );
--- plugins/events-pro/src/modules/blocks/index.js
+++ plugins/events-pro/src/modules/blocks/index.js
@@ -1,6 +1,21 @@
 /**
- * Internal dependencies
+ * External Dependencies
  */
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Internal Dependencies
+ */
+import additionalFields from '@moderntribe/events-pro/blocks/additional-fields';
 import { initStore } from '@moderntribe/events-pro/data';
+
+const blocks = [
+	additionalFields,
+];
+
+blocks.forEach( block => {
+	const blockName = `tribe/${ block.id }`;
+	registerBlockType( blockName, block );
+} );
 
 initStore();
