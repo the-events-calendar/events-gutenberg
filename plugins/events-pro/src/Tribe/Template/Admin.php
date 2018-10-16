efdcf4c85b5ff63e12b64cdb4ee248b09b4aafd8
mitogh <mitogh@gmail.com>
2018-10-16 10:21:28 -0500

    Simplify path for views

--- plugins/events-pro/src/Tribe/Template/Admin.php
+++ plugins/events-pro/src/Tribe/Template/Admin.php
@@ -12,9 +12,9 @@ class Tribe__Gutenberg__Events_Pro__Template__Admin extends Tribe__Template {
 	 * @since TBD
 	 */
 	public function __construct() {
-		$this->set_template_origin( tribe( 'gutenberg' ) );
+		$this->set_template_origin( tribe( 'gutenberg.events-pro.plugin' ) );
 		// todo: update to the plugins directory only
-		$this->set_template_folder( 'plugins/events-pro/src/admin-view' );
+		$this->set_template_folder( 'src/admin-view' );
 		
 		// Configures this templating class extract variables
 		$this->set_template_context_extract( true );
