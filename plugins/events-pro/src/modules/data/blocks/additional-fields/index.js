b462c52fd842dad27a934345cbf50adde3bb6384
mitogh <hello@crisoforo.com>
2018-10-19 17:49:12 -0500

    Remove references to non existing files

--- plugins/events-pro/src/modules/data/blocks/additional-fields/index.js
+++ plugins/events-pro/src/modules/data/blocks/additional-fields/index.js
@@ -5,7 +5,6 @@ import reducer from './reducers';
 import * as types from './types';
 import * as actions from './actions';
 import * as selectors from './selectors';
-import * as utils from './utils';
 
 export default reducer;
-export { types, actions, selectors, utils };
+export { types, actions, selectors };
