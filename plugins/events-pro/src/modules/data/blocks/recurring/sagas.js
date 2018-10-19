b172336fcce18b98ce23d45e91867bc6ee680ab0
Neal Fennimore <hi@neal.codes>
2018-10-19 17:34:37 -0400

    Add in recurring saga flows. Update FromTimeRangePicker to only use actions

--- plugins/events-pro/src/modules/data/blocks/recurring/sagas.js
+++ plugins/events-pro/src/modules/data/blocks/recurring/sagas.js
@@ -1,14 +1,15 @@
 /**
  * External dependencies
  */
-import { takeEvery, put, select } from 'redux-saga/effects';
+import { takeEvery, put, select, call, all } from 'redux-saga/effects';
+import { keys } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import * as recurring from '@moderntribe/events-pro/data/blocks/recurring';
 import * as ui from '@moderntribe/events-pro/data/ui';
-import { moment as momentUtil } from '@moderntribe/common/utils';
+import { moment as momentUtil, time } from '@moderntribe/common/utils';
 import { selectors as datetime } from '@moderntribe/events/data/blocks/datetime';
 
 export function* handleRuleRemoval() {
@@ -53,7 +54,90 @@ export function* handleRuleAddition() {
 	} ) );
 }
 
+export function* handleRuleEdit( action ) {
+	// Prevent rule syncs from looping
+	if ( action.sync ) {
+		return;
+	}
+
+	const fieldKeys = yield call( keys, action.payload );
+
+	for ( let i = 0; i < fieldKeys.length; i++ ) {
+		const fieldKey = fieldKeys[ i ];
+
+		switch ( fieldKey ) {
+			case recurring.constants.KEY_START_TIME:
+			case recurring.constants.KEY_END_TIME:
+				yield call( handleTimeChange, action, fieldKey );
+				break;
+
+			case recurring.constants.KEY_MULTI_DAY:
+				yield call( handleMultiDayChange, action, fieldKey );
+				break;
+
+			default:
+				break;
+		}
+	}
+}
+
+export function* handleTimeChange( action, key ) {
+	const payloadTime = action.payload[ key ];
+	const isAllDay = payloadTime === 'all-day';
+
+	if ( isAllDay ) {
+		yield put(
+			recurring.actions.syncRule( action.index, {
+				all_day: isAllDay,
+				start_time: '00:00:00',
+				end_time: '23:59:00',
+			} )
+		);
+	} else {
+		yield put(
+			recurring.actions.syncRule( action.index, {
+				all_day: isAllDay,
+				[ key ]: time.fromSeconds( payloadTime, time.TIME_FORMAT_HH_MM ),
+			} )
+		);
+	}
+}
+
+export function* handleMultiDayChange( action, key ) {
+	const isMultiDay = action.payload[ key ];
+
+	if ( ! isMultiDay ) {
+		const startTime = yield select( recurring.selectors.getStartTime, action );
+		const endTime = yield select( recurring.selectors.getEndTime, action );
+
+		let startTimeSeconds = time.toSeconds( startTime, time.TIME_FORMAT_HH_MM );
+		let endTimeSeconds = time.toSeconds( endTime, time.TIME_FORMAT_HH_MM );
+
+		// If end time is earlier than start time, fix time
+		if ( endTimeSeconds <= startTimeSeconds ) {
+			// If there is less than half an hour left in the day, roll back one hour
+			if ( startTimeSeconds + time.HALF_HOUR_IN_SECONDS >= time.DAY_IN_SECONDS ) {
+				startTimeSeconds -= time.HOUR_IN_SECONDS;
+			}
+
+			endTimeSeconds = startTimeSeconds + time.HALF_HOUR_IN_SECONDS;
+
+			yield put(
+				recurring.actions.syncRule( action.index, {
+					[ recurring.constants.KEY_START_TIME ]: (
+						time.fromSeconds( startTimeSeconds, time.TIME_FORMAT_HH_MM )
+					),
+					[ recurring.constants.KEY_END_TIME ]: (
+						time.fromSeconds( endTimeSeconds, time.TIME_FORMAT_HH_MM )
+					),
+				} )
+			);
+		}
+	}
+}
+
 export default function* watchers() {
 	yield takeEvery( [ recurring.types.REMOVE_RULE ], handleRuleRemoval );
 	yield takeEvery( [ recurring.types.ADD_RULE_FIELD ], handleRuleAddition );
+	yield takeEvery( [ recurring.types.EDIT_RULE ], handleRuleEdit );
 }
