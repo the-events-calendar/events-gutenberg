6811e96ebca1b50f63b57dc66dd6440e677225ef
mitogh <hello@crisoforo.com>
2018-10-18 22:45:34 -0500

    Export Field from elements

--- plugins/events-pro/src/modules/blocks/additional-fields/elements/index.js
+++ plugins/events-pro/src/modules/blocks/additional-fields/elements/index.js
@@ -4,3 +4,4 @@
 export { default as Settings } from './settings/container';
 export { default as Preview } from './preview/element';
 export { default as EditContainer } from './edit-container/element';
+export { default as Field } from './field-container/element';
