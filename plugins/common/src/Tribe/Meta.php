d8d5d275a4cd3eea0285ccfe0822106e14dc8b25
mitogh <hello@crisoforo.com>
2018-10-22 23:11:22 -0500

    Abstract repeated logic into an interface an parent class

---
+++ plugins/common/src/Tribe/Meta.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * Class Tribe__Gutenberg__Common__Meta
+ *
+ * @since TBD
+ */
+abstract class Tribe__Gutenberg__Common__Meta
+	implements Tribe__Gutenberg__Common__Meta_Interface {
+	
+	/**
+	 * Default definition for an attribute of type text
+	 *
+	 * @since 0.2.4-alpha
+	 *
+	 * @return array
+	 */
+	protected function text() {
+		return array(
+			'auth_callback' => array( $this, 'auth_callback' ),
+			'sanitize_callback' => 'sanitize_text_field',
+			'type' => 'string',
+			'single' => true,
+			'show_in_rest' => true,
+		);
+	}
+	
+	/**
+	 * Default definition for an attribute of type text
+	 *
+	 * @since 0.3.0-alpha
+	 *
+	 * @return array
+	 */
+	private function numeric() {
+		return array(
+			'auth_callback' => array( $this, 'auth_callback' ),
+			'sanitize_callback' => 'absint',
+			'type' => 'number',
+			'single' => true,
+			'show_in_rest' => true,
+		);
+	}
+	
+	/***
+	 * Default definition for an attribute of type boolean
+	 *
+	 * @since 0.2.4-alpha
+	 *
+	 * @return array
+	 */
+	protected function boolean() {
+		return array(
+			'auth_callback' => array( $this, 'auth_callback' ),
+			'sanitize_callback' => array( $this, 'sanitize_boolean' ),
+			'type' => 'boolean',
+			'single' => true,
+			'show_in_rest' => true,
+		);
+	}
+	
+	/**
+	 * Register a numeric type of array
+	 *
+	 * @since 0.2.4-alpha
+	 *
+	 * @return array
+	 */
+	protected function numeric_array() {
+		return array(
+			'description' => __( 'Array block', 'events-gutenberg' ),
+			'auth_callback' => array( $this, 'auth_callback' ),
+			'sanitize_callback' => array( $this, 'sanitize_numeric_array' ),
+			'type' => 'number',
+			'single' => false,
+			'show_in_rest' => true,
+		);
+	}
+	
+	/**
+	 * Verify if the current user can edit or not this Post
+	 *
+	 * @since  0.1.1-alpha
+	 *
+	 * @param bool   $allowed  Whether the user can add the post meta. Default false.
+	 * @param string $meta_key The meta key.
+	 * @param int    $post_id  Post ID.
+	 * @param int    $user_id  User ID.
+	 * @param string $cap      Capability name.
+	 * @param array  $caps     User capabilities.
+	 *
+	 * @return boolean
+	 */
+	public function auth_callback( $allowed, $meta_key, $post_id, $user_id, $cap, $caps ) {
+		$post = get_post( $post_id );
+		$post_type_obj = get_post_type_object( $post->post_type );
+		$current_user_can = current_user_can( $post_type_obj->cap->edit_post, $post_id );
+		
+		return $current_user_can;
+	}
+	
+	/**
+	 * Checks and sanitize a given value to a numeric array or a numeric string
+	 *
+	 * @since  0.1.0-alpha
+	 *
+	 * @param  mixed $value Check agains this value
+	 *
+	 * @return array|bool|int
+	 */
+	public function sanitize_numeric_array( $value ) {
+		if ( is_array( $value ) ) {
+			return wp_parse_id_list( $value );
+		} elseif ( is_numeric( $value ) ) {
+			return absint( $value );
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Make sure sanitization on boolean does not triggered warnings when multiple values are passed
+	 * to the function
+	 *
+	 * @since 0.2.4-alpha
+	 * @param $value
+	 *
+	 * @return bool
+	 */
+	public function sanitize_boolean( $value ) {
+		return boolval( $value );
+	}
+	
+	/**
+	 * Sanitize strings allowing the usage of white spaces before or after the separators, as
+	 * - sanitize_text_field removes any whitespace
+	 *
+	 * @since 0.2.4-alpha
+	 * @param $value
+	 *
+	 * @return mixed
+	 */
+	public function sanitize_separator( $value ) {
+		return filter_var( $value, FILTER_SANITIZE_STRING );
+	}
+}
