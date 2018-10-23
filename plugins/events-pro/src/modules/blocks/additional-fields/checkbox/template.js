f94e2684701b40f2213f9b6a5866e077d6663b96
mitogh <hello@crisoforo.com>
2018-10-22 21:40:59 -0500

    Connect `CheckboxField` into the store
    And simplify component signature

--- plugins/events-pro/src/modules/blocks/additional-fields/checkbox/template.js
+++ plugins/events-pro/src/modules/blocks/additional-fields/checkbox/template.js
@@ -9,49 +9,46 @@ import PropTypes from 'prop-types';
  */
 import { normalize } from '@moderntribe/common/utils/string';
 import { Field } from '@moderntribe/events-pro/blocks/additional-fields/elements';
-import Settings from './settings';
+import Settings from './settings/container';
 
-const CheckboxInput = ( { options, onChange } ) => {
-	return (
-		<fieldset className="tribe-editor__additional-fields__edit--horizontal-fields">
-			{ options.map( ( option, index ) => {
-				const { label = '', value = '', isChecked = false } = option;
-				const name = normalize( label );
-				const id = `name-${ index + 1 }`;
-				return (
-					<Fragment key={ id }>
-						<input
-							type="checkbox"
-							id={ id }
-							name={ name }
-							value={ value }
-							checked={ isChecked }
-							onChange={ onChange }
-						/>
-						<label htmlFor={ id }>{ label }</label>
-					</Fragment>
-				);
-			} ) }
-		</fieldset>
-	);
-};
+const CheckboxInput = ( { options, onChange } ) => (
+	<fieldset className="tribe-editor__additional-fields__edit--horizontal-fields">
+		{ options.map( ( option, index ) => {
+			const { label = '', value = '', isChecked = false } = option;
+			const name = normalize( label );
+			const id = `name-${ index + 1 }`;
+			return (
+				<Fragment key={ id }>
+					<input
+						type="checkbox"
+						id={ id }
+						name={ name }
+						value={ value }
+						checked={ isChecked }
+						onChange={ onChange }
+					/>
+					<label htmlFor={ id }>{ label }</label>
+				</Fragment>
+			);
+		} ) }
+	</fieldset>
+);
 
-const CheckboxField = ( { name, value, options, onInputChange, ...rest } ) => (
+const CheckboxField = ( { name, output, options, onInputChange, isSelected } ) => (
 	<Field
-		name={ name }
+		id={ name }
 		input={ <CheckboxInput onChange={ onInputChange } options={ options } /> }
-		output={ value }
-		settings={ <Settings name={ name } /> }
-		{ ...rest }
+		output={ output }
+		settings={ <Settings id={ name } /> }
+		isSelected={ isSelected }
 	/>
 );
 
 CheckboxField.propTypes = {
 	name: PropTypes.string.isRequired,
-	isPristine: PropTypes.bool,
+	output: PropTypes.string,
 	isSelected: PropTypes.bool,
 	onInputChange: PropTypes.func,
-	value: PropTypes.string,
 	options: PropTypes.arrayOf(
 		PropTypes.shape( {
 			value: PropTypes.string,
@@ -62,16 +59,7 @@ CheckboxField.propTypes = {
 };
 
 CheckboxField.defaultProps = {
-	isPristine: true,
 	isSelected: false,
-	/**
-	 * @todo remove placeholder options with data from the store
-	 */
-	options: [
-		{ value: 'chocolate', label: 'Chocolate', isChecked: false },
-		{ value: 'strawberry', label: 'Strawberry', isChecked: false },
-		{ value: 'vanilla', label: 'Vanilla', isChecked: true },
-	],
 };
 
 export default CheckboxField;
