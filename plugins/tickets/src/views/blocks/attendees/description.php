3f25515773accc28c5273a7db337e847c5ffb8fe
Juanfra Aldasoro <juanfraa@gmail.com>
2018-10-23 10:36:56 +0200

    Move message to its own variable

--- plugins/tickets/src/views/blocks/attendees/description.php
+++ plugins/tickets/src/views/blocks/attendees/description.php
@@ -7,5 +7,6 @@
  */
 $post_id         = $this->get( 'post_id' );
 $attendees_total = count( $attendees );
+$message         = _n( 'One person is attending %2$s', '%d people are attending %s', $attendees_total, 'events-gutenberg' );
 ?>
-<p><?php echo esc_html( sprintf( _n( 'One person is attending %2$s', '%d people are attending %s', $attendees_total, 'events-gutenberg' ), $attendees_total, get_the_title( $post_id ) ) ); ?></p>
+<p><?php echo esc_html( sprintf( $message, $attendees_total, get_the_title( $post_id ) ) ); ?></p>
