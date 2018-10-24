943d6791fa7a09f09e99341978d8aec977645192
mitogh <hello@crisoforo.com>
2018-10-23 19:44:15 -0500

    Use wrapper for the text input

--- plugins/events-pro/src/modules/blocks/additional-fields/text/template.js
+++ plugins/events-pro/src/modules/blocks/additional-fields/text/template.js
@@ -8,11 +8,12 @@ import PropTypes from 'prop-types';
  * Internal dependencies
  */
 import { Field } from '@moderntribe/events-pro/blocks/additional-fields/elements';
+import { Input } from '@moderntribe/common/elements';
 
 const TextField = ( { isSelected, value, onInputChange, name } ) => (
 	<Field
 		id={ name }
-		input={ <input type="text" value={ value } onChange={ onInputChange } /> }
+		input={ <Input type="text" value={ value } onChange={ onInputChange } /> }
 		output={ value }
 		isSelected={ isSelected }
 	/>
