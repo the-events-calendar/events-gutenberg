6f29ea81aacdaa2d2aa2e1f42e5d86e9fb0ded04
mitogh <hello@crisoforo.com>
2018-10-23 19:44:30 -0500

    Use wrapper for checkbox

--- plugins/events-pro/src/modules/blocks/additional-fields/checkbox/template.js
+++ plugins/events-pro/src/modules/blocks/additional-fields/checkbox/template.js
@@ -9,7 +9,9 @@ import PropTypes from 'prop-types';
  */
 import { normalize } from '@moderntribe/common/utils/string';
 import { Field } from '@moderntribe/events-pro/blocks/additional-fields/elements';
+import { Checkbox } from '@moderntribe/common/elements';
 import Settings from './settings/container';
+import './style.pcss';
 
 const CheckboxInput = ( { options, onChange } ) => (
 	<fieldset className="tribe-editor__additional-fields__edit--horizontal-fields">
@@ -18,17 +20,15 @@ const CheckboxInput = ( { options, onChange } ) => (
 			const name = normalize( label );
 			const id = `name-${ index + 1 }`;
 			return (
-				<Fragment key={ id }>
-					<input
-						type="checkbox"
-						id={ id }
-						name={ name }
-						value={ value }
-						checked={ isChecked }
-						onChange={ onChange }
-					/>
-					<label htmlFor={ id }>{ label }</label>
-				</Fragment>
+				<Checkbox
+					id={ id }
+					checked={ isChecked }
+					onChange={ onChange }
+					name={ name }
+					value={ value }
+					label={ label }
+					className={ 'tribe-editor__additional-fields__field--checkbox' }
+				/>
 			);
 		} ) }
 	</fieldset>
