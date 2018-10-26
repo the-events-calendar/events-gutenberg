273322d8a817b0c26a916cb30290fa5b21206b66
Neal Fennimore <hi@neal.codes>
2018-10-26 12:36:18 -0400

    Add more saga tests

--- plugins/events-pro/src/modules/data/shared/__tests__/sagas.spec.js
+++ plugins/events-pro/src/modules/data/shared/__tests__/sagas.spec.js
@@ -13,12 +13,16 @@ import { selectors as datetime } from '@moderntribe/events/data/blocks/datetime'
 import * as sagas from '../sagas';
 
 describe( 'Shared recurrence sagas', () => {
-	let actions, args;
+	let actions, selectors, args;
 
 	beforeEach( () => {
 		actions = {
-			sync: jest.fn(),
-			add: jest.fn(),
+			sync: jest.fn( obj => obj ),
+			add: jest.fn( obj => obj ),
+		};
+		selectors = {
+			getStartTimeNoSeconds: jest.fn( () => '12:00' ),
+			getEndTimeNoSeconds: jest.fn( () => '15:00' ),
 		};
 		args = {
 			start_date: '2018-01-01 12:00:00',
@@ -30,8 +34,20 @@ describe( 'Shared recurrence sagas', () => {
 	} );
 
 	describe( 'handleAddition', () => {
+		const _tempDate = global.Date;
+		beforeAll( () => {
+			global.Date = class _Date {
+				constructor( date ) {
+					return date;
+				}
+			};
+		} );
+
+		afterAll( () => {
+			global.Date = _tempDate;
+		} );
 		it( 'should add rule', () => {
-			const gen = sagas.handleAddition();
+			const gen = sagas.handleAddition( { actions } );
 			expect( gen.next().value ).toEqual(
 				select( datetime.getStart )
 			);
@@ -47,48 +63,186 @@ describe( 'Shared recurrence sagas', () => {
 			expect( gen.next( args.multi_day ).value ).toEqual(
 				select( datetime.getTimeZone )
 			);
+
 			expect( gen.next( args.timezone ).value ).toEqual(
 				call( momentUtil.toMoment, args.start_date )
 			);
-			expect( gen.next().value ).toEqual(
+			expect( gen.next( args.start_date ).value ).toEqual(
 				call( momentUtil.toMoment, args.end_date )
+			);
+
+			expect( gen.next( args.end_date ).value ).toEqual(
+				call( momentUtil.toDatabaseDate, args.start_date )
+			);
+			expect( gen.next( args.start_date ).value ).toEqual(
+				call( momentUtil.toDatabaseTime, args.start_date )
+			);
+
+			expect( gen.next( args.start_date ).value ).toEqual(
+				call( momentUtil.toDatabaseDate, args.end_date )
+			);
+			expect( gen.next( args.end_date ).value ).toEqual(
+				call( momentUtil.toDatabaseTime, args.end_date )
+			);
+
+			expect( gen.next( args.end_date ).value ).toEqual(
+				call( momentUtil.toDate, args.start_date )
+			);
+			expect( gen.next( args.start_date ).value ).toEqual(
+				call( momentUtil.toDate, args.end_date )
+			);
+
+			expect( gen.next().value ).toEqual(
+				put(
+					actions.add( {
+						[ constants.KEY_TYPE ]: recurringConstants.SINGLE,
+						[ constants.KEY_ALL_DAY ]: args.all_day,
+						[ constants.KEY_MULTI_DAY ]: args.multi_day,
+						[ constants.KEY_START_DATE ]: args.start_date,
+						[ constants.KEY_START_DATE_INPUT ]: args.start_date,
+						[ constants.KEY_START_DATE_OBJ ]: new Date( '2018-01-01T12:00:00.000Z' ),
+						[ constants.KEY_START_TIME ]: args.start_date,
+						[ constants.KEY_END_DATE ]: args.end_date,
+						[ constants.KEY_END_DATE_INPUT ]: undefined,
+						[ constants.KEY_END_DATE_OBJ ]: new Date( NaN ),
+						[ constants.KEY_END_TIME ]: args.end_date,
+						[ constants.KEY_BETWEEN ]: 1,
+						[ constants.KEY_LIMIT_TYPE ]: recurringConstants.COUNT,
+						[ constants.KEY_LIMIT ]: 7,
+						[ constants.KEY_LIMIT_DATE_INPUT ]: undefined,
+						[ constants.KEY_LIMIT_DATE_OBJ ]: new Date( NaN ),
+						[ constants.KEY_DAYS ]: [],
+						[ constants.KEY_WEEK ]: recurringConstants.FIRST,
+						[ constants.KEY_DAY ]: 1,
+						[ constants.KEY_MONTH ]: [],
+						[ constants.KEY_TIMEZONE ]: args.timezone,
+						[ constants.KEY_MULTI_DAY_SPAN ]: recurringConstants.NEXT_DAY,
+					} )
+				)
 			);
 		} );
 	} );
 
-	describe( 'handleExceptionRemoval', () => {
-		it( 'should not hide exception panel', () => {
-			const gen = sagas.handleExceptionRemoval();
+	describe( 'handleTimeChange', () => {
+		it( 'should handle time change', () => {
+			const gen = sagas.handleTimeChange( { actions }, { payload: {
+				[ constants.KEY_END_TIME ]: '12:00',
+			}, index: 0 }, constants.KEY_END_TIME );
+
 			expect( gen.next().value ).toEqual(
-				select( exception.selectors.getExceptions )
+				put( actions.sync( 0, {
+					[ constants.KEY_ALL_DAY ]: false,
+					[ constants.KEY_END_TIME ]: '12:00:00',
+				} ) )
 			);
-			expect( gen.next( [{ id: 'uniqid' }] ).done ).toEqual( true );
+
+			expect( gen.next().done ).toEqual( true );
 		} );
-		it( 'should hide exception panel', () => {
-			const gen = sagas.handleExceptionRemoval();
+		it( 'should handle time change when all-day', () => {
+			const gen = sagas.handleTimeChange( { actions }, { payload: {
+				[ constants.KEY_END_TIME ]: 'all-day',
+			}, index: 0 }, constants.KEY_END_TIME );
+
 			expect( gen.next().value ).toEqual(
-				select( exception.selectors.getExceptions )
+				put( actions.sync( 0, {
+					[ constants.KEY_ALL_DAY ]: true,
+					[ constants.KEY_START_TIME ]: '00:00:00',
+					[ constants.KEY_END_TIME ]: '23:59:59',
+				} ) )
 			);
-			expect( gen.next( [] ).value ).toEqual(
-				put( ui.actions.hideExceptionPanel() )
-			);
+
 			expect( gen.next().done ).toEqual( true );
 		} );
 	} );
-	describe( 'handleExceptionAddition', () => {
-		it( 'should add exception to store', () => {
-			const gen = sagas.handleExceptionAddition();
+
+	describe( 'handleMultiDayChange', () => {
+		it( 'should do nothing', () => {
+			const gen = sagas.handleMultiDayChange( { actions, selectors }, {
+				payload: {
+					[ constants.KEY_MULTI_DAY ]: true,
+				},
+				index: 0,
+			}, constants.KEY_MULTI_DAY );
+
+			gen.next().value;
+
+			expect( gen.next().done ).toEqual( true );
+		} );
+		it( 'should adjust time when start and end time are the same', () => {
+			const startTime = '12:00';
+			const endTime = '12:00';
+			const action = {
+				payload: {
+					[ constants.KEY_MULTI_DAY ]: false,
+				},
+				index: 0,
+			};
+
+			const gen = sagas.handleMultiDayChange( { actions, selectors }, action, constants.KEY_MULTI_DAY );
+
 			expect( gen.next().value ).toEqual(
-				select( datetime.datetimeSelector )
+				select( selectors.getStartTimeNoSeconds, action )
 			);
-			const payload = { start: 'start' };
-			expect( gen.next( payload ).value ).toEqual(
-				put( exception.actions.addException( {
-					type: recurring.constants.SINGLE,
-					...payload,
+
+			expect( gen.next( startTime ).value ).toEqual(
+				select( selectors.getEndTimeNoSeconds, action )
+			);
+			expect( gen.next( endTime ).value ).toEqual(
+				call( time.toSeconds, startTime, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next( 43200 ).value ).toEqual(
+				call( time.toSeconds, endTime, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next( 43200 ).value ).toEqual(
+				call( time.fromSeconds, 43200, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next( startTime ).value ).toEqual(
+				call( time.fromSeconds, 45000, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next( '13:00' ).value ).toEqual(
+				put( actions.sync( 0, {
+					[ constants.KEY_START_TIME ]: `$12:00:00`,
+					[ constants.KEY_END_TIME ]: `13:00:00`,
 				} ) )
 			);
-			expect( gen.next().done ).toEqual( true );
+		} );
+		it( 'should roll back start time when exceeds day time', () => {
+			const startTime = '23:59';
+			const endTime = '23:59';
+			const action = {
+				payload: {
+					[ constants.KEY_MULTI_DAY ]: false,
+				},
+				index: 0,
+			};
+
+			const gen = sagas.handleMultiDayChange( { actions, selectors }, action, constants.KEY_MULTI_DAY );
+
+			expect( gen.next().value ).toEqual(
+				select( selectors.getStartTimeNoSeconds, action )
+			);
+
+			expect( gen.next( startTime ).value ).toEqual(
+				select( selectors.getEndTimeNoSeconds, action )
+			);
+			expect( gen.next( endTime ).value ).toEqual(
+				call( time.toSeconds, startTime, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next( 86340 ).value ).toEqual(
+				call( time.toSeconds, endTime, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next( 86340 ).value ).toEqual(
+				call( time.fromSeconds, 82740, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next( startTime ).value ).toEqual(
+				call( time.fromSeconds, 84540, time.TIME_FORMAT_HH_MM )
+			);
+			expect( gen.next().value ).toEqual(
+				put( actions.sync( 0, {
+					[ constants.KEY_START_TIME ]: `12:00:00`,
+					[ constants.KEY_END_TIME ]: `13:00:00`,
+				} ) )
+			);
 		} );
 	} );
 } );
