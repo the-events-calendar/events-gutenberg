fa18e1389928fb02329363e56c0dd5c5b1f641e3
Paul <kim.paul.ms@gmail.com>
2018-10-29 22:57:50 +0800

    add tests for ui reducer

--- plugins/events-pro/src/modules/data/ui/__tests__/reducer.test.js
+++ plugins/events-pro/src/modules/data/ui/__tests__/reducer.test.js
@@ -10,7 +10,47 @@ describe( '[STORE] - UI reducer', () => {
 	} );
 
 	it( 'Should toggle repeat panel', () => {
-		const current = reducer( { isRepeatBlockVisible: false }, actions.toggleRepeatBlocksVisibility() );
+		const current = reducer( DEFAULT_STATE, actions.toggleRepeatBlocksVisibility() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should toggle rule panel', () => {
+		const current = reducer( DEFAULT_STATE, actions.toggleRulePanelVisibility() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should hide rule panel', () => {
+		const current = reducer( { ...DEFAULT_STATE, isRulePanelVisible: true }, actions.hideRulePanel() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should toggle exception panel', () => {
+		const current = reducer( DEFAULT_STATE, actions.toggleExceptionPanelVisibility() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should hide exception panel', () => {
+		const current = reducer( { ...DEFAULT_STATE, isExceptionPanelVisible: true }, actions.hideExceptionPanel() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should toggle rule panel expand', () => {
+		const current = reducer( DEFAULT_STATE, actions.toggleRulePanelExpand() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should expand rule panel', () => {
+		const current = reducer( DEFAULT_STATE, actions.expandRulePanel() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should toggle exception panel expand', () => {
+		const current = reducer( DEFAULT_STATE, actions.toggleExceptionPanelExpand() );
+		expect( current ).toMatchSnapshot();
+	} );
+
+	it( 'Should expand exception panel', () => {
+		const current = reducer( DEFAULT_STATE, actions.expandExceptionPanel() );
 		expect( current ).toMatchSnapshot();
 	} );
 } );
