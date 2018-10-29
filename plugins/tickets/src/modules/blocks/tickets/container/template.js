730e45d3c6a3eec40a5ef5b2ac6b6e3e85a3479d
Neal Fennimore <hi@neal.codes>
2018-10-29 14:50:37 -0400

    Escape comma

--- plugins/tickets/src/modules/blocks/tickets/container/template.js
+++ plugins/tickets/src/modules/blocks/tickets/container/template.js
@@ -37,7 +37,7 @@ const TicketContainer = ( props ) => {
 			: __( 'There is no ecommerce available', 'events-gutenberg' ),
 		description: hasProviders
 			? __( 'Edit this block to create your first ticket.', 'events-gutenberg' )
-			: __( 'To create tickets, you'll need to enable an ecommerce solution.', 'events-gutenberg' ),
+			: __( 'To create tickets, you\'ll need to enable an ecommerce solution.', 'events-gutenberg' ),
 	};
 
 	return (
@@ -48,7 +48,7 @@ const TicketContainer = ( props ) => {
 			{ tickets.length === 0 && (
 				<InactiveBlock
 					layout={ LAYOUT.ticket }
-					title={ messages.title  }
+					title={ messages.title }
 					description={ messages.description }
 					icon={ <StatusIcon disabled={ true } /> }
 				/>
@@ -58,7 +58,7 @@ const TicketContainer = ( props ) => {
 			) }
 		</div>
 	);
-}
+};
 
 TicketContainer.propTypes = {
 	isSelected: PropTypes.bool,
