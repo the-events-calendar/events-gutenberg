be186ed00e1da64add78276280f63bcbb91be1f4
Paul <kim.paul.ms@gmail.com>
2018-10-29 22:25:05 +0800

    add tests for ui actions

--- plugins/events-pro/src/modules/data/ui/__tests__/actions.test.js
+++ plugins/events-pro/src/modules/data/ui/__tests__/actions.test.js
@@ -4,7 +4,39 @@
 import { actions } from '@moderntribe/events-pro/data/ui';
 
 describe( '[STORE] - UI actions', () => {
-	it( 'Should toggle the repeat block', () => {
+	test( 'toggleRepeatBlocksVisibility', () => {
 		expect( actions.toggleRepeatBlocksVisibility() ).toMatchSnapshot();
+	} );
+
+	test( 'toggleRulePanelVisibility', () => {
+		expect( actions.toggleRulePanelVisibility() ).toMatchSnapshot();
+	} );
+
+	test( 'hideRulePanel', () => {
+		expect( actions.hideRulePanel() ).toMatchSnapshot();
+	} );
+
+	test( 'toggleExceptionPanelVisibility', () => {
+		expect( actions.toggleExceptionPanelVisibility() ).toMatchSnapshot();
+	} );
+
+	test( 'toggleRulePanelExpand', () => {
+		expect( actions.toggleRulePanelExpand() ).toMatchSnapshot();
+	} );
+
+	test( 'expandRulePanel', () => {
+		expect( actions.expandRulePanel() ).toMatchSnapshot();
+	} );
+
+	test( 'hideExceptionPanel', () => {
+		expect( actions.hideExceptionPanel() ).toMatchSnapshot();
+	} );
+
+	test( 'expandExceptionPanel', () => {
+		expect( actions.expandExceptionPanel() ).toMatchSnapshot();
+	} );
+
+	test( 'toggleExceptionPanelExpand', () => {
+		expect( actions.toggleExceptionPanelExpand() ).toMatchSnapshot();
 	} );
 } );
