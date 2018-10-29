f8941622c55d594b5cf14f2df11ee8629bb69b09
Paul <kim.paul.ms@gmail.com>
2018-10-29 22:43:18 +0800

    add tests for ui selectors

--- plugins/events-pro/src/modules/data/ui/__tests__/selectors.test.js
+++ plugins/events-pro/src/modules/data/ui/__tests__/selectors.test.js
@@ -15,7 +15,23 @@ describe( '[STORE] - UI Selectors', () => {
 		expect( selectors.getUI( state ) ).toEqual( DEFAULT_STATE );
 	} );
 
-	it( 'Should select the repeat block visible state', () => {
+	it( 'Should select the is repeat block visible state', () => {
 		expect( selectors.isRepeatBlockVisible( state ) ).toEqual( DEFAULT_STATE.isRepeatBlockVisible );
+	} );
+
+	it( 'Should select the is rule panel visibile state', () => {
+		expect( selectors.isRulePanelVisible( state ) ).toEqual( DEFAULT_STATE.isRulePanelVisible );
+	} );
+
+	it( 'Should select the is exception panel visible state', () => {
+		expect( selectors.isExceptionPanelVisible( state ) ).toEqual( DEFAULT_STATE.isExceptionPanelVisible );
+	} );
+
+	it( 'Should select the is rule panel expanded state', () => {
+		expect( selectors.isRulePanelExpanded( state ) ).toEqual( DEFAULT_STATE.isRulePanelExpanded );
+	} );
+
+	it( 'Should select the is exception panel expanded state', () => {
+		expect( selectors.isExceptionPanelExpanded( state ) ).toEqual( DEFAULT_STATE.isExceptionPanelExpanded );
 	} );
 } );
