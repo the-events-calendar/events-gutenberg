778f0ba2fe80f8d16adca7e3edeeef78a1903c24
Neal Fennimore <hi@neal.codes>
2018-10-30 15:19:33 -0400

    Add in Clipboard icon

--- plugins/events/src/modules/blocks/event-datetime/index.js
+++ plugins/events/src/modules/blocks/event-datetime/index.js
@@ -12,7 +12,6 @@ import { get } from 'lodash';
  * Internal dependencies
  */
 import EventDateTime from './container';
-import { BlockIcon } from '@moderntribe/common/elements';
 import { Clipboard } from '@moderntribe/common/icons';
 import { date, globals } from '@moderntribe/common/utils';
 
@@ -29,7 +28,7 @@ export default {
 		'Define the date, time, and duration for your event.',
 		'events-gutenberg'
 	),
-	icon: ()=><BlockIcon icon={Clipboard}/>,
+	icon: <div><Clipboard /></div>,
 	category: 'tribe-events',
 	keywords: [ 'event', 'events-gutenberg', 'tribe' ],
 
