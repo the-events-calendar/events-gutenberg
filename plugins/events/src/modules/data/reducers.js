a0a5445f48a877521810dcff917300882ec31b36
Neal Fennimore <hi@neal.codes>
2018-11-02 08:47:57 -0400

    Fix forms reducer and selector

--- plugins/events/src/modules/data/reducers.js
+++ plugins/events/src/modules/data/reducers.js
@@ -10,13 +10,11 @@ import blocks from './blocks';
 import ui from './ui';
 import search from './search';
 import details from './details';
-import forms from './forms';
 
 export default combineReducers( {
 	blocks,
 	ui,
 	search,
 	details,
-	forms,
 } );
 
