78013b70dd403ff61637f69c13b7e8e6359e0328
Neal Fennimore <hi@neal.codes>
2018-11-05 16:55:14 -0500

    Fix common using own externals

--- plugins/common/webpack.config.js
+++ plugins/common/webpack.config.js
@@ -4,18 +4,25 @@
 const { resolve } = require( 'path' );
 const merge = require( 'webpack-merge' );
 const common = require( '../../webpack/common/webpack.config' );
+const wpExternals = require( '../../webpack/externals/wp.js' );
 const { getDirectoryNames } = require( '../../webpack/utils/directories' );
 const { generateEntries } = require( '../../webpack/entry/tribe' );
 
 const directoryNames = getDirectoryNames( resolve( __dirname, './src/modules' ) );
 const PLUGIN_SCOPE = 'common';
 
-const config = merge( common, {
-	entry: generateEntries( __dirname, directoryNames ),
-	output: {
-		path: __dirname,
-		library: [ 'tribe', PLUGIN_SCOPE, '[name]' ],
-	},
-} );
+const config = merge.strategy( {
+	externals: 'replace',
+} )(
+	common,
+	{
+		externals: [ wpExternals ], // Only use WP externals
+		entry: generateEntries( __dirname, directoryNames ),
+		output: {
+			path: __dirname,
+			library: [ 'tribe', PLUGIN_SCOPE, '[name]' ],
+		},
+	}
+);
 
 module.exports = config;
