6fdf0386c3bb1ff957ded28e99404e4425897146
Paul <kim.paul.ms@gmail.com>
2018-11-06 14:30:10 +0800

    update constants

--- plugins/tickets/src/modules/data/blocks/ticket/__tests__/constants.test.js
+++ plugins/tickets/src/modules/data/blocks/ticket/__tests__/constants.test.js
@@ -5,15 +5,15 @@ import { constants } from '@moderntribe/tickets/data/blocks/ticket';
 
 describe( 'Ticket Constants', () => {
 	test( 'tc', () => {
-		expect( constants.TC ).toEqual( 'tribe_commerce' );
+		expect( constants.TC ).toEqual( 'tribe-commerce' );
 	} );
 
 	test( 'edd', () => {
-		expect( constants.EDD ).toEqual( 'easy_digital_downloads' );
+		expect( constants.EDD ).toEqual( 'edd' );
 	} );
 
 	test( 'woo', () => {
-		expect( constants.WOO ).toEqual( 'wooCommerce' );
+		expect( constants.WOO ).toEqual( 'woo' );
 	} );
 
 	test( 'tc class', () => {
@@ -29,19 +29,19 @@ describe( 'Ticket Constants', () => {
 	} );
 
 	test( 'provider class to constant mapping', () => {
-		expect( constants.PROVIDER_CLASS_TO_CONSTANT_MAPPING )
+		expect( constants.PROVIDER_CLASS_TO_PROVIDER_MAPPING )
 			.toEqual( {
-				Tribe__Tickets__Commerce__PayPal__Main: 'tribe_commerce',
-				Tribe__Tickets_Plus__Commerce__EDD__Main: 'easy_digital_downloads',
-				Tribe__Tickets_Plus__Commerce__WooCommerce__Main: 'wooCommerce',
+				Tribe__Tickets__Commerce__PayPal__Main: 'tribe-commerce',
+				Tribe__Tickets_Plus__Commerce__EDD__Main: 'edd',
+				Tribe__Tickets_Plus__Commerce__WooCommerce__Main: 'woo',
 			} );
 	} );
 
 	test( 'provider types', () => {
 		expect( constants.PROVIDER_TYPES ).toEqual( [
-			'tribe_commerce',
-			'easy_digital_downloads',
-			'wooCommerce',
+			'tribe-commerce',
+			'edd',
+			'woo',
 		] );
 	} );
 } );
--- plugins/tickets/src/modules/data/blocks/ticket/constants.js
+++ plugins/tickets/src/modules/data/blocks/ticket/constants.js
@@ -1,12 +1,12 @@
-export const TC = 'tribe_commerce';
-export const EDD = 'easy_digital_downloads';
-export const WOO = 'wooCommerce';
+export const TC = 'tribe-commerce';
+export const EDD = 'edd';
+export const WOO = 'woo';
 
 export const TC_CLASS = 'Tribe__Tickets__Commerce__PayPal__Main';
 export const EDD_CLASS = 'Tribe__Tickets_Plus__Commerce__EDD__Main';
 export const WOO_CLASS = 'Tribe__Tickets_Plus__Commerce__WooCommerce__Main';
 
-export const PROVIDER_CLASS_TO_CONSTANT_MAPPING = {
+export const PROVIDER_CLASS_TO_PROVIDER_MAPPING = {
 	[ TC_CLASS ]: TC,
 	[ EDD_CLASS ]: EDD,
 	[ WOO_CLASS ]: WOO,
