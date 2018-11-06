7e327ec1fca7bc0dd4c8e93def8e636b5333a6ac
Neal Fennimore <hi@neal.codes>
2018-11-06 08:11:43 -0500

    Revert "WIP"
    This reverts commit 2c9bb81ad8abdb043c6fc659ab6eaa43346907e0.

--- plugins/common/webpack.config.js
+++ plugins/common/webpack.config.js
@@ -5,40 +5,22 @@ const { resolve } = require( 'path' );
 const merge = require( 'webpack-merge' );
 const common = require( '../../webpack/common/webpack.config' );
 const wpExternals = require( '../../webpack/externals/wp.js' );
-const vendor = require( '../../webpack/externals/vendor.js' );
 const { getDirectoryNames } = require( '../../webpack/utils/directories' );
 const { generateEntries } = require( '../../webpack/entry/tribe' );
 
 const directoryNames = getDirectoryNames( resolve( __dirname, './src/modules' ) );
 const PLUGIN_SCOPE = 'common';
 
-const vendorEntries = Object.keys( vendor );
-const vendorChunk = new RegExp( `(${ vendorEntries.join( '|' ) })` );
-
 const config = merge.strategy( {
 	externals: 'replace',
 } )(
 	common,
 	{
-		externals: { ...wpExternals, ...vendor },
-		entry: {
-			...generateEntries( __dirname, directoryNames ),
-			vendor: Object.keys( vendor ),
-		},
+		externals: [ wpExternals ], // Only use WP externals
+		entry: generateEntries( __dirname, directoryNames ),
 		output: {
 			path: __dirname,
 			library: [ 'tribe', PLUGIN_SCOPE, '[name]' ],
-		},
-		optimization: {
-			splitChunks: {
-				cacheGroups: {
-					vendor: {
-						test: vendorChunk,
-						name: 'vendor',
-						chunks: 'all',
-					},
-				},
-			},
 		},
 	}
 );
--- webpack/externals/index.js
+++ webpack/externals/index.js
@@ -1,9 +1,7 @@
 const wpExternals = require( './wp' );
-const vendor = require( './vendor' );
 const tribeCommon = require( './tribe/common' );
 
 module.exports = {
 	...wpExternals,
-	...vendor,
 	...tribeCommon,
 };
