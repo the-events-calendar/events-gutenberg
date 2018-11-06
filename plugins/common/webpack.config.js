2c9bb81ad8abdb043c6fc659ab6eaa43346907e0
Neal Fennimore <hi@neal.codes>
2018-11-06 07:52:34 -0500

    WIP

--- plugins/common/webpack.config.js
+++ plugins/common/webpack.config.js
@@ -5,22 +5,40 @@ const { resolve } = require( 'path' );
 const merge = require( 'webpack-merge' );
 const common = require( '../../webpack/common/webpack.config' );
 const wpExternals = require( '../../webpack/externals/wp.js' );
+const vendor = require( '../../webpack/externals/vendor.js' );
 const { getDirectoryNames } = require( '../../webpack/utils/directories' );
 const { generateEntries } = require( '../../webpack/entry/tribe' );
 
 const directoryNames = getDirectoryNames( resolve( __dirname, './src/modules' ) );
 const PLUGIN_SCOPE = 'common';
 
+const vendorEntries = Object.keys( vendor );
+const vendorChunk = new RegExp( `(${ vendorEntries.join( '|' ) })` );
+
 const config = merge.strategy( {
 	externals: 'replace',
 } )(
 	common,
 	{
-		externals: [ wpExternals ], // Only use WP externals
-		entry: generateEntries( __dirname, directoryNames ),
+		externals: { ...wpExternals, ...vendor },
+		entry: {
+			...generateEntries( __dirname, directoryNames ),
+			vendor: Object.keys( vendor ),
+		},
 		output: {
 			path: __dirname,
 			library: [ 'tribe', PLUGIN_SCOPE, '[name]' ],
+		},
+		optimization: {
+			splitChunks: {
+				cacheGroups: {
+					vendor: {
+						test: vendorChunk,
+						name: 'vendor',
+						chunks: 'all',
+					},
+				},
+			},
 		},
 	}
 );
---
+++ webpack/externals/vendor.js
@@ -0,0 +1,11 @@
+const vendor = {
+	'chrono-node': 'ChronoNode',
+	'he': 'he',
+	'lodash': 'lodash',
+	'moment': 'moment',
+	'react-dom': 'ReactDOM',
+	'react': 'React',
+	'redux': 'redux',
+};
+
+module.exports = vendor;
