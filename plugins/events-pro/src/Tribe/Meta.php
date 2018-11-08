43b5c5ae3bcea72038760d11a1582d14c44f791f
mitogh <hello@crisoforo.com>
2018-11-07 22:22:08 -0600

    Remove recurrence keys if `_EventRecurrence` is deleted
    Remove the keys associated with the event:
    
    - `_tribe_blocks_recurrence_exclusions`
    - `_tribe_blocks_recurrence_rules`
    
    If the event removes the `_EventRecurrence` meta key as is no longer a
    recurrent event.
    
    Removes the recurrence meta box from the editor as well if
    `classic-editor` is set.

--- plugins/events-pro/src/Tribe/Meta.php
+++ plugins/events-pro/src/Tribe/Meta.php
@@ -18,6 +18,8 @@ class Tribe__Gutenberg__Events_Pro__Meta {
 		register_meta( 'post', '_tribe_blocks_recurrence_exclusions', tribe( 'gutenberg.events.meta' )->text() );
 		
 		add_filter( 'get_post_metadata', array( $this, 'filter_going_fields' ), 15, 4 );
+		add_action( 'deleted_post_meta', array( $this, 'remove_recurrence_meta' ), 10, 3  );
+		add_filter( 'tribe_pro_show_recurrence_meta_box', array( $this, 'remove_recurrence_classic_meta' ) );
 	}
 	
 	/**
@@ -82,5 +84,40 @@ class Tribe__Gutenberg__Events_Pro__Meta {
 		$query = "SELECT meta_value FROM $wpdb->postmeta WHERE post_id = %d AND meta_key = %s";
 		
 		return $wpdb->get_var( $wpdb->prepare( $query, $post_id, $meta_key ) );
+	}
+	
+	/**
+	 * Removes the meta keys that maps into the classic editor when the `_EventRecurrence` is
+	 * removed.
+	 *
+	 * @since TBD
+	 *
+	 * @param $meta_id
+	 * @param $object_id
+	 * @param $meta_key
+	 */
+	public function remove_recurrence_meta( $meta_id, $object_id, $meta_key ) {
+		if ( '_EventRecurrence' !== $meta_key ) {
+			return;
+		}
+		delete_post_meta( $object_id, '_tribe_blocks_recurrence_rules' );
+		delete_post_meta( $object_id, '_tribe_blocks_recurrence_exclusions' );
+	}
+	
+	/**
+	 * Remove the recurrence meta box if classic-editor is set
+	 *
+	 * @since TBD
+	 *
+	 * @param $show_meta
+	 *
+	 * @return bool
+	 */
+	public function remove_recurrence_classic_meta( $show_meta ) {
+		$is_classic_editor = tribe_get_request_var('classic-editor', null );
+		if ( $is_classic_editor === null ) {
+			return false;
+		}
+		return $show_meta;
 	}
 }
