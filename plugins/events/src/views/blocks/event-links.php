58f2fd103b71dbe9fc5ca6e802e3abf6505143b0
Paul <kim.paul.ms@gmail.com>
2018-11-09 00:14:43 +0800

    fix indentation from spaces to tabs

--- plugins/events/src/views/blocks/event-links.php
+++ plugins/events/src/views/blocks/event-links.php
@@ -18,30 +18,30 @@ $should_render = $has_google_cal || $has_ical;
 remove_filter( 'the_content', 'do_blocks', 9 );
 ?>
 <?php if ( $should_render ) : ?>
-    <div class="tribe-block tribe-block__events-link">
+	<div class="tribe-block tribe-block__events-link">
 		<?php if ( $has_google_cal ) : ?>
-            <div class="tribe-block__btn--link tribe-block__events-gcal">
-                <a
-                    href="<?php echo Tribe__Events__Main::instance()->esc_gcal_url( tribe_get_gcal_link() ); ?>"
-                    title="<?php esc_attr_e( 'Add to Google Calendar', 'events-gutenberg' ); ?>"
-                >
-                    <img src="<?php echo tribe( 'gutenberg' )->extension_url . 'src/modules/icons/link.svg'; ?>" />
-                    <?php echo esc_html( $this->attr( 'googleCalendarLabel' ) ) ?>
-                </a>
-            </div>
+			<div class="tribe-block__btn--link tribe-block__events-gcal">
+				<a
+					href="<?php echo Tribe__Events__Main::instance()->esc_gcal_url( tribe_get_gcal_link() ); ?>"
+					title="<?php esc_attr_e( 'Add to Google Calendar', 'events-gutenberg' ); ?>"
+				>
+					<img src="<?php echo tribe( 'gutenberg' )->extension_url . 'src/modules/icons/link.svg'; ?>" />
+					<?php echo esc_html( $this->attr( 'googleCalendarLabel' ) ) ?>
+				</a>
+			</div>
 		<?php endif; ?>
 		<?php if ( $has_ical ) : ?>
-            <div class="tribe-block__btn--link tribe-block__-events-ical">
-                <a
-                    href="<?php echo esc_url( tribe_get_single_ical_link() ); ?>"
-                    title="<?php esc_attr_e( 'Download .ics file', 'events-gutenberg' ); ?>"
-                >
-                    <img src="<?php echo tribe( 'gutenberg' )->extension_url . 'src/modules/icons/link.svg'; ?>" />
-                    <?php echo esc_html( $this->attr( 'iCalLabel' ) ) ?>
-                </a>
-            </div>
+			<div class="tribe-block__btn--link tribe-block__-events-ical">
+				<a
+					href="<?php echo esc_url( tribe_get_single_ical_link() ); ?>"
+					title="<?php esc_attr_e( 'Download .ics file', 'events-gutenberg' ); ?>"
+				>
+					<img src="<?php echo tribe( 'gutenberg' )->extension_url . 'src/modules/icons/link.svg'; ?>" />
+					<?php echo esc_html( $this->attr( 'iCalLabel' ) ) ?>
+				</a>
+			</div>
 		<?php endif; ?>
-    </div>
+	</div>
 <?php endif; ?>
 
 <?php add_filter( 'the_content', 'do_blocks', 9 );
